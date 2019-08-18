a4f78546a93dbb2992954b563bf4a40870370ee1
Jannik <jannik@outlook.com>
2019-08-18 12:37:19 +0200

    Add: Carto Maps

--- resources/views/includes/statusMap.blade.php
+++ resources/views/includes/statusMap.blade.php
@@ -1,20 +1,14 @@
-<div id="map-{{$status->id}}" style="width: 100%; height: 200px; overflow: hidden;"></div>
+<div id="map-{{$status->id}}" style="width: 100%; height: 210px; overflow: hidden;"></div>
 
 <script>
 (function() {
     var map = L.map('map-{{$status->id}}');
     
-    // All of this does not work:
-    // 
-    // var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    //     subdomains: 'abcd',
-    //     maxZoom: 19
-    // }).addTo(map);
-    // var OpenRailwayMap = L.tileLayer('https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', {
-    //     maxZoom: 19,
-    //     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://www.OpenRailwayMap.org">OpenRailwayMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    // }).addTo(map);
+    var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    }).addTo(map);
 
     const lnglats = {{$status->trainCheckin->getMapLines()}};
     const latlngs = lnglats.map(([a,b]) => [b,a]);
