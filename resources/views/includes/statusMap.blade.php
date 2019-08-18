503b79e575bf2c190ebf2415930e3344e4a5a25b
Jannik <jannik@outlook.com>
2019-08-18 12:37:49 +0200

    Disable Zoom and Dragging. The map is just a static image at this point.

--- resources/views/includes/statusMap.blade.php
+++ resources/views/includes/statusMap.blade.php
@@ -2,7 +2,7 @@
 
 <script>
 (function() {
-    var map = L.map('map-{{$status->id}}');
+    var map = L.map('map-{{$status->id}}', {zoomControl: false});
     
     var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -15,5 +15,10 @@
     var polyline = L.polyline(latlngs).setStyle({color: "rgb(192, 57, 43)", weight: 5}).addTo(map);
 
     map.fitBounds(polyline.getBounds());
+
+    map.dragging.disable();
+    map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
 })();
 </script>
