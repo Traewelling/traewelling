f4f94dd8f7d75867f79b0ec53372e2aaad293dc2
Jannik <jannik@outlook.com>
2019-08-31 17:06:18 +0200

    Add: Absatz über neue Features, minor style changes

--- public/js/app.js
+++ public/js/app.js
@@ -49488,8 +49488,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/herrlevin_/Dev/trwl/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/herrlevin_/Dev/trwl/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /c/laragon/www/trwl/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /c/laragon/www/trwl/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,9 +7,11 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-8">
+            <div class="col-lg-8" id="about-page">
                 <h1>About us</h1>
                 <blockquote class="blockquote"><p>Träwelling ist ein kostenloser Check-in Service, mit dem du deinen Freunden mitteilen kannst, wo du gerade mit der Bahn unterwegs bist. Kurz gesagt: Man kann in Züge einchecken und bekommt dafür Punkte.</p></blockquote>
+
+                <h2>F.A.Q. <small class="text-muted">H&auml;ufig gestellte Fragen</small></h2>
 
                 <h3>Wer entwickelt Träwelling?</h3>
                 <p class="lead">@<a href="https://twitter.com/herrlevin_">HerrLevin_</a> hat einen großen Teil der ersten Version (seit 2013), der mobilen Version (seit 2014) und der neuen Version (seit 2019) entwickelt. Unterstützt wird er von @<a href="https://twitter.com/confuzd_">confuzd_</a>, @<a href="https://twitter.com/aledjones">aledjones</a>, @<a href="https://twitter.com/der_Karl">der_Karl</a> und @<a href="https://twitter.com/quantatheist">quantatheist</a>.
@@ -17,10 +19,11 @@
                 <h3>Woher kommt der Name?</h3>
                 <p class="lead">Der Name ist eine Anspielung auf das allseits bekannte <i>"Senk ju for träwelling wis Deutsche Bahn"</i>, was man eigentlich in fast jedem Fernverkehrszug der Deutschen Bahn gehört haben sollte.<br />Auf die Idee kam der nette @<a href="https://twitter.com/mrpelz">mrpelz</a>.</p>
 
-                <h2>F.A.Q. <small class="text-muted">H&auml;ufig gestellte Fragen</small></h2>
-                
                 <h3>Warum wird mein Zug nicht aufgelistet?</h3>
                 <p class="lead">Wir verwenden eine Schnittstelle der Deutschen Bahn, bei der nicht alle Angebote direkt dargestellt werden. Leider können wir da auch nicht viel dran tun, wenn dein Zug nicht dabei ist.</p>
+
+                <h3>Da fehlt etwas! Wieso wurde dieses Feature entfernt?</h3>
+                <p class="lead">In dieser Version von Träwelling fangen wir einmal ganz von neu an &ndash; und dabei kann es sein, dass wenig-verwendete Funktionen noch nicht implementiert sind. <strong>Übrigens:</strong> Wenn Du ein Feature vorschlagen möchtest, schreib einfach eine E-Mail an <a href="mailto:traewelling@herrlev.in">traewelling@herrlev.in</a>.</p>
 
                 <h3>Wie werden Punkte berechnet?</h3>
                 <p class="lead">
