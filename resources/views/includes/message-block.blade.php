6340c1f1d5e114934dc29465c53fd81a554189b4
Jannik <jannik@outlook.com>
2019-09-05 19:54:27 +0200

    Fix #52

--- resources/views/includes/message-block.blade.php
+++ resources/views/includes/message-block.blade.php
@@ -1,25 +1,29 @@
 @if ($errors->any())
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @foreach ($errors->all() as $error)
-                <div class="alert my-3 alert-danger" role="alert">
-                    {!! $error !!}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                @foreach ($errors->all() as $error)
+                    <div class="alert my-3 alert-danger" role="alert">
+                        {!! $error !!}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endif
 @if(Session::has('message'))
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="alert my-3 alert-info" role="alert">
-                {{Session::get('message')}}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="alert my-3 alert-info" role="alert">
+                    {{Session::get('message')}}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
