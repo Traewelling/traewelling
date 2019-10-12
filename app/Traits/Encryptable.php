b02932c4edcb5bddaec8a9b086f6719f0c0a7342
Levin Herr <gitlab@ca.herrlev.in>
2019-10-12 23:43:57 +0200

    - added key-encryption as stated in #9 - removed --uhr translations

---
+++ app/Traits/Encryptable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Crypt;
+trait Encryptable
+{
+    /**
+     * If the attribute is in the encryptable array
+     * then decrypt it.
+     *
+     * @param  $key
+     *
+     * @return $value
+     */
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+        if (in_array($key, $this->encryptable) && $value !== '') {
+            $value = decrypt($value);
+        }
+        return $value;
+    }
+    /**
+     * If the attribute is in the encryptable array
+     * then encrypt it.
+     *
+     * @param $key
+     * @param $value
+     */
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encryptable)) {
+            $value = encrypt($value);
+        }
+        return parent::setAttribute($key, $value);
+    }
+    /**
+     * When need to make sure that we iterate through
+     * all the keys.
+     *
+     * @return array
+     */
+    public function attributesToArray()
+    {
+        $attributes = parent::attributesToArray();
+        foreach ($this->encryptable as $key) {
+            if (isset($attributes[$key])) {
+                $attributes[$key] = decrypt($attributes[$key]);
+            }
+        }
+        return $attributes;
+    }
+}
