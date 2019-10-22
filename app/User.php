71b6489459390e5998e26ae05a6f9ef404086336
Levin Herr <gitlab@ca.herrlev.in>
2019-10-22 23:40:24 +0200

    added reset-task in console-kernel for every friday 3:14 (b/c pi. hehe) added check when deleting statuses before removing points from profile

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'username', 'name', 'email', 'password'
+        'username', 'name', 'avatar', 'email', 'password'
     ];
 
     /**
