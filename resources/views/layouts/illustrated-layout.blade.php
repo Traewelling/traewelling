f8e11d3c8ba3612da7ea7213ec3a0a3bebf7d61f
Jannik <jannik@outlook.com>
2019-11-17 16:11:09 +0100

    Track welcome page and count errorcodes

--- resources/views/layouts/illustrated-layout.blade.php
+++ resources/views/layouts/illustrated-layout.blade.php
@@ -449,6 +449,23 @@
                 background-position: center;
             }
         </style>
+
+        <!-- Matomo -->
+        <script type="text/javascript">
+            var _paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackEvent', 'Error', 'Hit', '@yield("code") @yield("title")']);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+                var u="//matomo.trwl.pw/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
+        <!-- End Matomo Code -->
     </head>
     <body class="antialiased font-sans img-bg">
         <div class="md:flex min-h-screen">
