5219d40bfbfd59b3083f0cd2c44745249fa2bdcf
Jannik <jannik@outlook.com>
2019-11-21 18:14:47 +0100

    Remove matomo until we find a way to opt-in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,25 +44,6 @@
     <style>
         /* Moved to resources/sass/site.scss */
     </style>
-    <!-- Matomo -->
-    <script type="text/javascript">
-        var _paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        @php($user = Auth::user())
-        @if($user != null)
-        _paq.push(['setUserId', '{{$user->username}}']);
-        @endif
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u="//matomo.trwl.pw/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-    </script>
-    <!-- End Matomo Code -->
 </head>
 <body>
     <div id="app">
