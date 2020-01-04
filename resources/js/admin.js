519bcafda57c19e695817173f3e00bdd06622336
Jannik <jannik@outlook.com>
2020-01-04 12:17:26 +0100

    Use the adminlte package from npm instead of a copied one, webpack it all

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,6 +1,6 @@
 require("jquery");
-require('./admin-lte');
-const Chart = require('chart.js');
+require("admin-lte");
+const Chart = require("chart.js");
 
 window.addEventListener("load", () => {
     require("./components/usageBoard");
