4cc4e625648527c5d165dc110ff5ea46a984027d
Levin Herr <levin@herrlevin.de>
2020-03-02 19:19:52 +0100

    removed Deppenapostroph

--- resources/lang/de/admin.php
+++ resources/lang/de/admin.php
@@ -4,7 +4,7 @@ return [
     "usage" => "Nutzung",
     "usage-board" => "Nutzungsauswertung",
     "select-range" => "Bereich auswählen",
-    "checkins" => "Check-in's",
+    "checkins" => "Check-ins",
     "registered-users" => "Registrierte Nutzer",
     "hafas-entries-by-type" => "HAFAS-Einträge nach Transport-Typ",
     "hafas-entries-by-polylines" => "HAFAS-Einträge und Anzahl dazugehöriger Polylines",
