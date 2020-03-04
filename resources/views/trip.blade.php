700f5f7f70c8ff74da40d422dab63ec189ee7650
Jannik <jannik@outlook.com>
2020-03-04 21:49:27 +0100

    Make fast-forward-checkin work for trains that terminate earlier than planned.

--- resources/views/trip.blade.php
+++ resources/views/trip.blade.php
@@ -10,7 +10,7 @@
             <div class="card">
                 <div class="card-header" data-linename="{{ $train['linename'] }}" data-startname="{{ $start }}" data-start="{{ request()->start }}" data-tripid="{{ request()->tripID }}">
                     <div class="float-right">
-                        <a href="#" class="train-destinationrow" data-ibnr="{{end($stopovers)['stop']['id']}}" data-stopname="{{end($stopovers)['stop']['name']}}"><i class="fa fa-fast-forward"></i></a>
+                        <a href="#" class="train-destinationrow" data-ibnr="{{$terminalStop['stop']['id']}}" data-stopname="{{$terminalStop['stop']['name']}}"><i class="fa fa-fast-forward"></i></a>
                     </div>
                     @if (file_exists(public_path('img/'.$train['category'].'.svg')))
                         <img class="product-icon" src="{{ asset('img/'.$train['category'].'.svg') }}">
