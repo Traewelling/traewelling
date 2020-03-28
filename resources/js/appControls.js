2dad14de22390db4975a49038e8ccbac8b957715
Jannik <jannik@outlook.com>
2020-03-28 12:13:58 +0100

    Translate the "follow/unfollow" button and fix its classes

--- resources/js/appControls.js
+++ resources/js/appControls.js
@@ -1,7 +1,7 @@
 var statusId = 0;
 var statusBodyElement = null;
 
-$(document).on("click", ".edit", function(event) {
+$(document).on("click", ".edit", function (event) {
     console.log("edit");
     event.preventDefault();
 
@@ -11,7 +11,7 @@ $(document).on("click", ".edit", function(event) {
     $("#edit-modal").modal();
 });
 
-$(document).on("click", "#modal-save", function() {
+$(document).on("click", "#modal-save", function () {
     $.ajax({
         method: "POST",
         url: urlEdit,
@@ -21,29 +21,29 @@ $(document).on("click", "#modal-save", function() {
             businessCheck: $("#business_check:checked").length,
             _token: token
         }
-    }).done(function(msg) {
+    }).done(function (msg) {
         window.location.reload();
     });
 });
 
-$(document).on("click", ".delete", function(event) {
+$(document).on("click", ".delete", function (event) {
     event.preventDefault();
 
     statusId = event.target.parentElement.dataset["statusid"];
     $("#delete-modal").modal();
 });
 
-$(document).on("click", "#modal-delete", function() {
+$(document).on("click", "#modal-delete", function () {
     $.ajax({
         method: "DELETE",
         url: urlDelete,
         data: { statusId: statusId, _token: token }
-    }).done(function(msg) {
+    }).done(function (msg) {
         window.location.replace("/dashboard");
     });
 });
 
-$(document).on("click", ".like", function(event) {
+$(document).on("click", ".like", function (event) {
     statusId = event.target.dataset["statusid"];
 
     var $likecount = document.getElementById("like-count-" + statusId);
@@ -55,7 +55,7 @@ $(document).on("click", ".like", function(event) {
             method: "POST",
             url: urlLike,
             data: { statusId: statusId, _token: token }
-        }).done(function() {
+        }).done(function () {
             event.target.className = "like fas fa-star animated bounceIn";
             $likecount.innerText = ++count;
 
@@ -76,7 +76,7 @@ $(document).on("click", ".like", function(event) {
             method: "POST",
             url: urlDislike,
             data: { statusId: statusId, _token: token }
-        }).done(function() {
+        }).done(function () {
             event.target.className = "like far fa-star";
             $likecount.innerText = --count;
 
@@ -98,7 +98,7 @@ $(document).on("click", ".like", function(event) {
     event.stopPropagation();
 });
 
-$(document).on("click", ".follow", function(event) {
+$(document).on("click", ".follow", function (event) {
     event.preventDefault();
 
     userId = event.target.dataset["userid"];
@@ -108,23 +108,27 @@ $(document).on("click", ".follow", function(event) {
             method: "POST",
             url: urlFollow,
             data: { follow_id: userId, _token: token }
-        }).done(function() {
+        }).done(function () {
             event.target.dataset["following"] = "yes";
-            event.target.innerText = "Unfollow";
+            event.target.classList.add('btn-danger');
+            event.target.classList.remove('btn-primary');
+            event.target.innerText = window.translUnfollow;
         });
     } else {
         $.ajax({
             method: "POST",
             url: urlUnfollow,
             data: { follow_id: userId, _token: token }
-        }).done(function() {
+        }).done(function () {
             event.target.dataset["following"] = "no";
-            event.target.innerText = "Follow";
+            event.target.classList.add('btn-primary');
+            event.target.classList.remove('btn-danger');
+            event.target.innerText = window.translFollow;
         });
     }
 });
 
-$(document).on("click", ".disconnect", function(event) {
+$(document).on("click", ".disconnect", function (event) {
     event.preventDefault();
 
     var provider = event.target.dataset["provider"];
@@ -132,15 +136,15 @@ $(document).on("click", ".disconnect", function(event) {
         method: "POST",
         url: urlDisconnect,
         data: { provider: provider, _token: token },
-        success: function() {
+        success: function () {
             location.reload();
         },
-        error: function(request, status, error) {
+        error: function (request, status, error) {
             bootstrap_alert.danger(request.responseText);
         }
     });
 });
 
-$(document).on("click", "#timepicker-button", function(event) {
+$(document).on("click", "#timepicker-button", function (event) {
     event.preventDefault();
 });
