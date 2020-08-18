0bbe263a0f021ba21b51d34e013824c483e5da85
Kris <git@k118.de>
2020-08-18 23:31:11 +0200

    Declaration of primary key type

--- app/Session.php
+++ app/Session.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
+    protected $keyType = 'string';
+
     public function user() {
         return $this->belongsTo('App\User');
     }
