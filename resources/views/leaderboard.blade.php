45efcbc5ea79ee07782c204ba7dadf1e6ff694ca
Kris <git@k118.de>
2020-08-21 12:39:08 +0200

    Underline username on leaderboard on hover
    I've done this so users can see this is link. Otherwise it's just text.

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card" id="leaderboard">
                     <div class="card-header">
                         {{ __('menu.leaderboard') }}
                     </div>
