7611730515e07124f684ac06cab5bb2186788dc5
Kris <git@k118.de>
2020-09-27 23:04:50 +0200

    Migrate Laravel to v7

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -82,6 +82,13 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
