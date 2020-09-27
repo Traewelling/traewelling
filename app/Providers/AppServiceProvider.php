f704b9c59a27b2fff0b7b030406176746e1b3ad8
Kris <git@k118.de>
2020-09-28 01:29:53 +0200

    [MigrateLarV8] Use Bootstrap Paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Revolution\Socialite\Mastodon\MastodonProvider;
 
@@ -32,5 +33,7 @@ class AppServiceProvider extends ServiceProvider
                 return $socialite->buildProvider(MastodonProvider::class, $config);
             }
         );
+
+        Paginator::useBootstrap();
     }
 }
