2e719720fb3aa651b1d3b1bb134b486b3926c9ae
Kris <git@k118.de>
2020-10-05 00:47:15 +0200

    [APIv0] Add expires_at to login & signup endpoint (#127)
    * [APIv0] Add expires_at date to login/signup response
    
    closes #126
    
    * [APIv0] Check username parameter to be unique
    
    * [APIv0] Use Laravels Hash Facade
    
    * [APIv0] Add expires_at to swagger

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\ResponseController as ResponseController;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
@@ -11,55 +12,58 @@ use App\Models\User;
 class AuthController extends ResponseController
 {
     //create user
-    public function signup(Request $request)
-    {
+    public function signup(Request $request) {
         $validator = Validator::make($request->all(), [
-            'username' => 'required|string|',
-            'name' => 'required|string|',
-            'email' => 'required|string|email|unique:users',
-            'password' => 'required',
+            'username'         => 'required|string|unique:users',
+            'name'             => 'required|string|',
+            'email'            => 'required|string|email|unique:users',
+            'password'         => 'required',
             'confirm_password' => 'required|same:password'
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $this->sendError($validator->errors(), 400);
         }
 
         $input             = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user              = new User($input);
-        $user->save();
-        if($user){
-            $success['token']   =  $user->createToken('token')->accessToken;
-            $success['message'] = "Registration successfull..";
-            return $this->sendResponse($success);
-        } else{
-            $error = "Sorry! Registration is not successfull.";
-            return $this->sendError($error, 401);
+        $input['password'] = Hash::make($input['password']);
+        $user              = User::create($input);
+
+        if ($user) {
+            $userToken = $user->createToken('token');
+            return $this->sendResponse([
+                                           'token'      => $userToken->accessToken,
+                                           'message'    => 'Registration successful.',
+                                           'expires_at' => $userToken->token->expires_at->toIso8601String()
+                                       ]);
         }
 
+        $error = "Sorry! Registration is not successful.";
+        return $this->sendError($error, 401);
     }
 
     //login
-    public function login(Request $request)
-    {
+    public function login(Request $request) {
         $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email',
+            'email'    => 'required|string|email',
             'password' => 'required'
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $this->sendError($validator->errors(), 400);
         }
 
         $credentials = request(['email', 'password']);
-        if(!Auth::attempt($credentials)){
+        if (!Auth::attempt($credentials)) {
             $error = "Unauthorized";
             return $this->sendError($error, 401);
         }
-        $user             = $request->user();
-        $success['token'] = $user->createToken('token')->accessToken;
-        return $this->sendResponse($success);
+        $userToken = $request->user()->createToken('token');
+
+        return $this->sendResponse([
+                                       'token'      => $userToken->accessToken,
+                                       'expires_at' => $userToken->token->expires_at->toIso8601String()
+                                   ]);
     }
 
     //logout
