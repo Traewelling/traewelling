f29d07aebd0394c00b1dbdc7ead8c5a7bae4bb59
Kris <git@k118.de>
2020-10-05 20:10:28 +0200

    Redirect logged in Users to Dashboard (#141)

--- app/Http/Controllers/FrontendStaticController.php
+++ app/Http/Controllers/FrontendStaticController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 
@@ -11,5 +12,12 @@ class FrontendStaticController extends Controller
     public function changeLanguage($lang = null) {
         Session::put('language', $lang);
         return Redirect::back();
+    }
+
+    public function renderLandingPage() {
+        if (Auth::check()) {
+            return \redirect()->route('dashboard');
+        }
+        return view('welcome');
     }
 }
