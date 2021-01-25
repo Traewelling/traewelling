03cfa5bb88ccf67976156140d090143d6e4d8196
Kris <git@k118.de>
2021-01-26 00:06:56 +0100

    Fixed another TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -31,11 +31,16 @@ abstract class TestCase extends BaseTestCase
      * may contain trains starting the next day. If the test runs after midnight it might contain
      * some trains that started the day before.
      *
+     * Trips where the first station is a day before the requestDate can be even one day more earlier.
+     * e.g. Train starts at 01.01. but out request is on the same train which departs on 02.01 at 00:01
+     * at the second station -> in the trip is is still the 01.01.
+     *
      * @return Boolean If all checks were resolved positively. Assertions to be made on the caller
      * side to provide a coherent amount of assertions.
      * @throws \Exception
      */
     public static function isCorrectHafasTrip($hafastrip, $requestDate): bool {
+        $requestDateMinusMinusOneDay = (clone $requestDate)->sub(new \DateInterval('P2D'));
         $requestDateMinusOneDay = (clone $requestDate)->sub(new \DateInterval('P1D'));
         $requestDatePlusOneDay  = (clone $requestDate)->add(new \DateInterval('P1D'));
 
@@ -43,6 +48,7 @@ abstract class TestCase extends BaseTestCase
         $fourPipes = 4 == substr_count($hafastrip->tripId, '|');
 
         $rightDate = in_array(1, [
+            substr_count($hafastrip->tripId, $requestDateMinusMinusOneDay->format(self::$HAFAS_ID_DATE)),
             substr_count($hafastrip->tripId, $requestDateMinusOneDay->format(self::$HAFAS_ID_DATE)),
             substr_count($hafastrip->tripId, $requestDate->format(self::$HAFAS_ID_DATE)),
             substr_count($hafastrip->tripId, $requestDatePlusOneDay->format(self::$HAFAS_ID_DATE))
