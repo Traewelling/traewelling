3e0af75206656087b3bf5f1a194d62436f3bf3d9
Kristian Stöckel <git@k118.de>
2021-01-28 23:56:11 +0100

    Reordered database migration

--- database/migrations/2019_08_09_003814_create_likes_table.php
+++ database/migrations/2019_08_09_003814_create_likes_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateLikesTable extends Migration
 {
@@ -11,14 +11,12 @@ class CreateLikesTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('likes', function (Blueprint $table) {
+    public function up(): void {
+        Schema::create('likes', function(Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
             $table->integer('user_id')->unsigned();
             $table->integer('status_id')->unsigned();
-
+            $table->timestamps();
         });
     }
 
@@ -27,8 +25,7 @@ class CreateLikesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(): void {
         Schema::dropIfExists('likes');
     }
 }
