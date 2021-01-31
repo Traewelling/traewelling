5e35c5d81d88c581b2a8bdf46f32545fe5852c51
Kristian Stöckel <git@k118.de>
2021-01-31 22:55:48 +0100

    Revert

--- resources/js/components/stationboard.js
+++ resources/js/components/stationboard.js
@@ -51,7 +51,6 @@ $(document)
             .data("start");
         var destination = $(this).data("ibnr");
         var stopname = $(this).data("stopname");
-        var arrivalTime = $(this).data("arrivaltime");
         var linename = $(this)
             .parent()
             .parent()
@@ -69,7 +68,6 @@ $(document)
                 modal.find("#input-tripID").val(tripID);
                 modal.find("#input-destination").val(destination);
                 modal.find("#input-start").val(start);
-                modal.find("#input-arrivaltime").val(arrivalTime);
             });
         }
     })
