cd9daaf8351edf3f2d60f372fa42d6454824798b
Kristian Stöckel <git@k118.de>
2021-02-19 14:19:32 +0100

    Improved UI when user have no followers

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -47,6 +47,7 @@ return [
     "create-ics-token-success"    => "The calendar token has been created. You can embed it with the following link to every software which support ICS files: :link",
     "revoke-ics-token-success"    => "The calendar token has been revoked.",
     'follower'                    => [
+        'no-follower'     => 'You don\'t have any followers.',
         'manage'          => 'Manage followers',
         'following-since' => 'Following since',
         'delete-success'  => 'You successfully removed the follower.',
--- resources/views/settings/follower.blade.php
+++ resources/views/settings/follower.blade.php
@@ -10,51 +10,60 @@
                     <div class="card-header">{{ __('menu.settings-myFollower') }}</div>
 
                     <div class="card-body">
-                        <table class="table table-responsive table-striped">
-                            <thead>
-                            <tr>
-                                <th colspan="2">Follower</th>
-                                <th>{{__('settings.follower.following-since')}}</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($followers as $follower)
+
+                        @if($followers->count() == 0)
+                            <b class="text-danger">
+                                <i class="fas fa-users-slash"></i>
+                                {{__('settings.follower.no-follower')}}
+                            </b>
+                        @else
+
+                            <table class="table table-responsive table-striped">
+                                <thead>
                                 <tr>
-                                    <td>
-                                        <div class="image-box pr-0 d-none d-lg-flex">
-                                            <a href="{{ route('account.show', ['username' => $follower->username]) }}">
-                                                <img src="{{ route('account.showProfilePicture', ['username' => $follower->username]) }}"
-                                                     style="height: 3em;"
-                                                />
-                                            </a>
-                                        </div>
-                                    </td>
-                                    <td style="vertical-align: middle">
-                                        <a href="{{route('account.show', ['username' => $follower->username])}}">
-                                            {{$follower->name}}
-                                            @if($follower->name != $follower->username)
-                                                <br/>
-                                                <small>{{'@' . $follower->username}}</small>
-                                            @endif
-                                        </a>
-                                    </td>
-                                    <td style="vertical-align: middle">{{$follower->created_at->diffForHumans()}}</td>
-                                    <td>
-                                        <form method="POST" action="{{route('settings.follower.remove')}}">
-                                            @csrf
-                                            <input type="hidden" name="user_id" value="{{$follower->id}}"/>
-                                            <button type="submit" class="btn btn-sm btn-danger">
-                                                <i class="fas fa-users-slash"></i>
-                                                {{__('settings.follower.delete')}}
-                                            </button>
-                                        </form>
-                                    </td>
+                                    <th colspan="2">Follower</th>
+                                    <th>{{__('settings.follower.following-since')}}</th>
+                                    <th></th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        {{$followers->links()}}
+                                </thead>
+                                <tbody>
+                                @foreach($followers as $follower)
+                                    <tr>
+                                        <td>
+                                            <div class="image-box pr-0 d-none d-lg-flex">
+                                                <a href="{{ route('account.show', ['username' => $follower->username]) }}">
+                                                    <img src="{{ route('account.showProfilePicture', ['username' => $follower->username]) }}"
+                                                         style="height: 3em;"
+                                                    />
+                                                </a>
+                                            </div>
+                                        </td>
+                                        <td style="vertical-align: middle">
+                                            <a href="{{route('account.show', ['username' => $follower->username])}}">
+                                                {{$follower->name}}
+                                                @if($follower->name != $follower->username)
+                                                    <br/>
+                                                    <small>{{'@' . $follower->username}}</small>
+                                                @endif
+                                            </a>
+                                        </td>
+                                        <td style="vertical-align: middle">{{$follower->created_at->diffForHumans()}}</td>
+                                        <td>
+                                            <form method="POST" action="{{route('settings.follower.remove')}}">
+                                                @csrf
+                                                <input type="hidden" name="user_id" value="{{$follower->id}}"/>
+                                                <button type="submit" class="btn btn-sm btn-danger">
+                                                    <i class="fas fa-users-slash"></i>
+                                                    {{__('settings.follower.delete')}}
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            {{$followers->links()}}
+                        @endif
                     </div>
                 </div>
             </div>
