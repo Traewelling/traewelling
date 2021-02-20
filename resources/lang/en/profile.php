355e5cd0cfe23c34c8d8dcdc248209bb98792cf3
Kristian Stöckel <git@k118.de>
2021-02-20 23:26:27 +0100

    Autoformatted all language-files

--- resources/lang/en/profile.php
+++ resources/lang/en/profile.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    "follow" => "Follow",
-    "follow_req" => "Request follow",
-    "last-journeys-of" => "Last journeys of",
-    "points-abbr" => "pts",
-    "settings" => "Settings",
-    "statistics-for" => "Statistics for",
-    "unfollow" => "Unfollow",
-    "private-profile-text" => "This profile is private.",
+    "follow"                           => "Follow",
+    "follow_req"                       => "Request follow",
+    "last-journeys-of"                 => "Last journeys of",
+    "points-abbr"                      => "pts",
+    "settings"                         => "Settings",
+    "statistics-for"                   => "Statistics for",
+    "unfollow"                         => "Unfollow",
+    "private-profile-text"             => "This profile is private.",
     "private-profile-information-text" =>
         "Only approved followers can see the check-ins of @:username. To request access, click Follow."
 ];
