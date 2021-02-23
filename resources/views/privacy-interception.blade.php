372b291b49eea0aad7058051b4675848a6d3faf4
Kristian Stöckel <git@k118.de>
2021-02-23 13:28:47 +0100

    Added aria-labelledby

--- resources/views/privacy-interception.blade.php
+++ resources/views/privacy-interception.blade.php
@@ -46,11 +46,12 @@
                         </form>
 
                         <!-- Das ist das Delete Modal aus den Settings -->
-                        <div class="modal fade" id="deleteUserModal" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal fade" id="deleteUserModal" tabindex="-1" role="dialog"
+                             aria-hidden="true" aria-labelledby="deleteModalTitle">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title">{{__('settings.delete-account')}}</h5>
+                                        <h5 class="modal-title" id="deleteModalTitle">{{__('settings.delete-account')}}</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
