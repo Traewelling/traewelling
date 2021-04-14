028e5be97345d755904b5c09d0ca2e695f09e9f6
Levin Herr <github@17d.me>
2021-04-14 15:12:18 +0200

    en.json sv.json

--- resources/lang/sv/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    "failed"   => "Det här kontot existerar inte eller data har skrivits in felaktigt.",
-    "throttle" => "För många inloggningsförsök. Försök igen om :seconds: sekunder."
-];
