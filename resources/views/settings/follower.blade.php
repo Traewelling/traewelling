013e87ff03fd3e0970c80dae2139ff3213ec169d
Kristian Stöckel <git@k118.de>
2021-04-28 22:24:06 +0200

    Code formatting

--- resources/views/settings/follower.blade.php
+++ resources/views/settings/follower.blade.php
@@ -20,44 +20,44 @@
 
                             <table class="table table-striped table-hover">
                                 <thead>
-                                <tr>
-                                    <th colspan="2">Follower</th>
-                                    <th></th>
-                                </tr>
+                                    <tr>
+                                        <th colspan="2">Follower</th>
+                                        <th></th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($followers as $follower)
-                                    <tr>
-                                        <td>
-                                            <div class="image-box pe-0 d-lg-flex" style="width: 4em; height: 4em;">
-                                                <a href="{{ route('account.show', ['username' => $follower->username]) }}">
-                                                    <img src="{{ route('account.showProfilePicture', ['username' => $follower->username]) }}"
-                                                         style="height: 3em;"
-                                                    />
+                                    @foreach($followers as $follower)
+                                        <tr>
+                                            <td>
+                                                <div class="image-box pe-0 d-lg-flex" style="width: 4em; height: 4em;">
+                                                    <a href="{{ route('account.show', ['username' => $follower->username]) }}">
+                                                        <img src="{{ route('account.showProfilePicture', ['username' => $follower->username]) }}"
+                                                             style="height: 3em;"
+                                                        />
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td style="vertical-align: middle">
+                                                <a href="{{route('account.show', ['username' => $follower->username])}}">
+                                                    {{$follower->name}}
+                                                    @if($follower->name != $follower->username)
+                                                        <br/>
+                                                        <small>{{'@' . $follower->username}}</small>
+                                                    @endif
                                                 </a>
-                                            </div>
-                                        </td>
-                                        <td style="vertical-align: middle">
-                                            <a href="{{route('account.show', ['username' => $follower->username])}}">
-                                                {{$follower->name}}
-                                                @if($follower->name != $follower->username)
-                                                    <br/>
-                                                    <small>{{'@' . $follower->username}}</small>
-                                                @endif
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <form method="POST" action="{{route('settings.follower.remove')}}">
-                                                @csrf
-                                                <input type="hidden" name="user_id" value="{{$follower->id}}"/>
-                                                <button type="submit" class="btn btn-sm btn-danger">
-                                                    <i class="fas fa-users-slash"></i>
-                                                    {{__('settings.follower.delete')}}
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                            </td>
+                                            <td>
+                                                <form method="POST" action="{{route('settings.follower.remove')}}">
+                                                    @csrf
+                                                    <input type="hidden" name="user_id" value="{{$follower->id}}"/>
+                                                    <button type="submit" class="btn btn-sm btn-danger">
+                                                        <i class="fas fa-users-slash"></i>
+                                                        {{__('settings.follower.delete')}}
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                             {{$followers->links()}}
