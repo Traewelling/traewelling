52ebd53e745610306fd325886fa672a7e64353ea
Kris <git@k118.de>
2021-05-11 10:30:20 +0200

    Adjust the CodeStyle guidelines to align constants (#304)

--- app/Enum/HafasTravelType.php
+++ app/Enum/HafasTravelType.php
@@ -6,13 +6,13 @@ namespace App\Enum;
 final class HafasTravelType extends BasicEnum
 {
     public const NATIONAL_EXPRESS = 'nationalExpress';
-    public const NATIONAL = 'national';
-    public const REGIONAL_EXP = 'regionalExp';
-    public const REGIONAL = 'regional';
-    public const SUBURBAN = 'suburban';
-    public const BUS = 'bus';
-    public const FERRY = 'ferry';
-    public const SUBWAY = 'subway';
-    public const TRAM = 'tram';
-    public const TAXI = 'taxi';
+    public const NATIONAL         = 'national';
+    public const REGIONAL_EXP     = 'regionalExp';
+    public const REGIONAL         = 'regional';
+    public const SUBURBAN         = 'suburban';
+    public const BUS              = 'bus';
+    public const FERRY            = 'ferry';
+    public const SUBWAY           = 'subway';
+    public const TRAM             = 'tram';
+    public const TAXI             = 'taxi';
 }
