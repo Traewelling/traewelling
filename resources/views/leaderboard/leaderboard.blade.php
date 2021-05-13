aefe5e1d78540c8e69ae036bef0f4559dcd91992
Kris <git@k118.de>
2021-05-13 10:00:19 +0200

    Make leaderboard wider (#332)
    closes #324

--- resources/views/leaderboard/leaderboard.blade.php
+++ resources/views/leaderboard/leaderboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-7">
+            <div class="col-md-9 col-lg-9">
                 <div class="card" id="leaderboard">
                     <div class="card-header">
                         <a href="{{route('leaderboard.month', ['date' => date('Y-m')])}}" class="float-end">
