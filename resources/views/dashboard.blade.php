ef29aef00cf29de679cc6f6e351d5cbac2f2ecfc
Levin Herr <github@17d.me>
2021-05-19 12:48:49 +0200

    ✨ Filtered out check-ins that are more than 20 minutes in the future (#349)
    * Filtered out check-ins that are more than 20 minutes in the future
    
    * removed unnecessary variable declaration
    
    * Update resources/views/dashboard.blade.php
    
    Co-authored-by: Kris <git@k118.de>
    
    * Update app/Http/Controllers/StatusController.php
    
    Co-authored-by: Kris <git@k118.de>
    
    Co-authored-by: Kris <git@k118.de>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,34 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 col-lg-7">
+                @if($future->count() >= 1)
+                    <div class="accordion accordion-flush" id="accordionFutureCheckIns">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button
+                                        class="accordion-button collapsed"
+                                        type="button"
+                                        data-mdb-toggle="collapse"
+                                        data-mdb-target="#future-check-ins"
+                                        aria-expanded="false"
+                                        aria-controls="future-check-ins"
+                                >
+                                    {{ __('dashboard.future') }}
+                                </button>
+                            </h2>
+                            <div
+                                    id="future-check-ins"
+                                    class="accordion-collapse collapse"
+                                    aria-labelledby="flush-headingOne"
+                                    data-mdb-parent="#accordionFutureCheckIns"
+                            >
+                                <div class="accordion-body">
+                                    @include('includes.statuses', ['statuses' => $future, 'showDates' => false])
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
                 @include('includes.statuses', ['statuses' => $statuses, 'showDates' => true])
             </div>
         </div>
