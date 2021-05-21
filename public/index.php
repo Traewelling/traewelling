b3799036ce6083f479d1547b9f79fe18a85658b0
Kristian Stöckel <git@k118.de>
2021-05-21 18:04:40 +0200

    Code cleanup for better Codacy performance!

--- public/index.php
+++ public/index.php
@@ -57,11 +57,8 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+$kernel   = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = Illuminate\Http\Request::capture());
 
 $response->send();
 
