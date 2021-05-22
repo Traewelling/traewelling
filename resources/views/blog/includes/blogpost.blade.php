ab942373b885bffd30c06a4120cc3c99cc68df17
Kristian Stöckel <git@k118.de>
2021-05-22 16:08:52 +0200

    Added structured data (SEO!)

--- resources/views/blog/includes/blogpost.blade.php
+++ resources/views/blog/includes/blogpost.blade.php
@@ -1,13 +1,15 @@
-<div>
-    <a href="{{ route('blog.show', ['slug' => $blogpost->slug]) }}">
-        <h2 class="font-weight-bold" style="font-size: 1.5em;">{{$blogpost->title}}</h2>
+<div itemscope itemtype="https://schema.org/Article">
+    <a href="{{ route('blog.show', ['slug' => $blogpost->slug]) }}" itemprop="url">
+        <h2 class="font-weight-bold" style="font-size: 1.5em;" itemprop="name">
+            {{$blogpost->title}}
+        </h2>
     </a>
 
     @if($showFull)
-        {!! Markdown::parse($blogpost->body) !!}
+        <div itemprop="articleBody">{!! Markdown::parse($blogpost->body) !!}</div>
     @else
         <div class="row">
-            <div class="col-md-10">{!! Markdown::parse($blogpost->preview) !!}</div>
+            <div class="col-md-10" itemprop="description">{!! Markdown::parse($blogpost->preview) !!}</div>
             <div class="col-md-2">
                 <a class="mt-0 mb-5"
                    href="{{ route('blog.show', ['slug' => $blogpost->slug]) }}">
@@ -19,7 +21,7 @@
 
     <div class="text-muted">
         <p class="float-end">
-            <samp class="pe-2">{{ $blogpost->published_at->format('d.m.Y') }}</samp>
+            <samp class="pe-2" itemprop="datePublished">{{ $blogpost->published_at->format('d.m.Y') }}</samp>
         </p>
         <ul class="list-inline mb-0">
             <li class="list-inline-item">
@@ -28,7 +30,7 @@
                         <i class="fab fa-twitter"></i>
                     </a>
                 @endif
-                {{ $blogpost->author_name }}
+                <div itemprop="author">{{ $blogpost->author_name }}</div>
             </li>
             <li class="list-inline-item">
                 <a href="{{ route('blog.category', ['category' => $blogpost->category]) }}">
