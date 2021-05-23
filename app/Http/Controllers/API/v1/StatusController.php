9f469690307b1a04c7351dc8e1d75ce3a19825a6
Levin Herr <github@17d.me>
2021-05-23 20:52:45 +0200

    statuscontroller fixes

--- app/Http/Controllers/API/v1/StatusController.php
+++ app/Http/Controllers/API/v1/StatusController.php
@@ -23,42 +23,43 @@ class StatusController extends ResponseController
 
     /**
      * Show single status
-     * @param $id
-     * @return StatusResource
+     * @param int $id
+     * @return StatusResource|Response
      */
-    public function show($id): StatusResource|Response {
+    public function show(int $id): StatusResource|Response {
         return new StatusResource(StatusBackend::getStatus($id));
     }
 
     /**
-     * @param $parameters
+     * @param string $parameters
      * @return AnonymousResourceCollection|JsonResponse
      * @todo extract this to backend
      * @todo does this conform to the private checkin-shit?
      */
-    public function getPolyline($parameters) {
+    public function getPolyline(string $parameters): JsonResponse {
         $ids      = explode(',', $parameters, 50);
         $mapLines = Status::whereIn('id', $ids)
                           ->with('trainCheckin.HafasTrip.getPolyLine')
                           ->get()
                           ->reject(function($status) {
-                              return $status->user->userInvisibleToMe === true;
+                              return $status->user->userInvisibleToMe;
                           })
                           ->map(function($status) {
-                              return ["id"               => (int) $status->id,
-                                      "coordinatesArray" => $status->trainCheckin->getMapLines()
+                              return [
+                                  "id"               => (int) $status->id,
+                                  "coordinatesArray" => $status->trainCheckin->getMapLines()
                               ];
                           });
         return $ids ? $this->sendv1Response($mapLines) : $this->sendError("");
     }
 
     /**
-     * @param $parameters
+     * @param string $parameters
      * @return JsonResponse
      */
-    public function getStopovers($parameters) {
+    public function getStopovers(string $parameters): JsonResponse {
         $tripIds = explode(',', $parameters, 50);
-        $trips   = HafasTrip::wherein('id', $tripIds)->get()->mapWithKeys(function($trip) {
+        $trips   = HafasTrip::whereIn('id', $tripIds)->get()->mapWithKeys(function($trip) {
             return [$trip->id => StopoverResource::collection($trip->stopoversNEW)];
         });
         return $this->sendv1Response($trips);
