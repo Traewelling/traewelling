4c9935ca779e611c2f495782385baeb1b06d4a78
Kristian Stöckel <git@k118.de>
2021-05-26 21:07:36 +0200

    Changes

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EventResource extends JsonResource
@@ -9,22 +10,22 @@ class EventResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @return array
      */
     public function toArray($request) {
         return [
-            "id"      => $this->id,
-            "name"    => $this->name,
-            "slug"    => $this->slug,
-            "hashtag" => $this->hashtag,
-            "host"    => $this->host,
-            "url"     => $this->url,
-            "begin"   => $this->begin,
-            "end"     => $this->end,
+            "id"            => $this->id,
+            "name"          => $this->name,
+            "slug"          => $this->slug,
+            "hashtag"       => $this->hashtag,
+            "host"          => $this->host,
+            "url"           => $this->url,
+            "begin"         => $this->begin,
+            "end"           => $this->end,
             "trainDistance" => $this->trainDistance,
             "trainDuration" => $this->trainDuration,
-            "station" => new TrainstationResource($this->station)
+            "station"       => new TrainstationResource($this->station)
         ];
     }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,13 +18,13 @@ class UserResource extends JsonResource
             'id'                => (int) $this->id,
             'displayName'       => (string) $this->name,
             'username'          => (string) $this->username,
-            'trainDistance'    => (float) $this->train_distance,
-            'trainDuration'    => (int) $this->train_duration,
-            'trainSpeed'       => (float) $this->averageSpeed,
+            'trainDistance'     => (float) $this->train_distance,
+            'trainDuration'     => (int) $this->train_duration,
+            'trainSpeed'        => (float) $this->averageSpeed,
             'points'            => (int) $this->points,
-            'twitterUrl'       => $this->twitterUrl ?? null,
-            'mastodonUrl'      => $this->mastodonUrl ?? null,
-            'privateProfile'   => (bool) $this->private_profile,
+            'twitterUrl'        => $this->twitterUrl ?? null,
+            'mastodonUrl'       => $this->mastodonUrl ?? null,
+            'privateProfile'    => (bool) $this->private_profile,
             'userInvisibleToMe' => (bool) $this->userInvisibleToMe
         ];
     }
