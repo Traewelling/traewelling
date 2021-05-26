5b4f2b60e9b0d55d3a98459f405a330c96a59ede
Kristian Stöckel <git@k118.de>
2021-05-26 12:30:40 +0200

    Added "Sorry-Alert"

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -11,6 +11,12 @@
             <div class="col-md-9">
                 <p><a href="{{route('blog.all')}}">{!! __('pagination.back') !!}</a></p>
 
+                @if(substr(app()->getLocale(), 0, 2) != 'de')
+                    <div class="alert alert-primary" role="alert" data-mdb-color="primary">
+                        <i class="fas fa-globe"></i> {{__('localisation.not-available')}}
+                    </div>
+                @endif
+
                 @include('blog.includes.blogpost', ['showFull' => true])
             </div>
         </div>
