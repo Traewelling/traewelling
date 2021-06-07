a96f22e19cb3a5509c0ddfeb630286b2f9153771
Kris <git@k118.de>
2021-06-07 11:51:31 +0200

    🚸 [AdminPanel] Show all events (#396)
    * [AdminPanel] Show all events
    
    * [AdminPanel] Show all events

--- app/Http/Controllers/Backend/Admin/TelegramController.php
+++ app/Http/Controllers/Backend/Admin/TelegramController.php
@@ -9,7 +9,7 @@ abstract class TelegramController extends Controller
 {
     public static function sendAdminMessage(string $message) {
         $client   = new Client(['base_uri' => 'https://api.telegram.org']);
-        $response = $client->get(strtr('/bot:token/sendMessage', [':token' => config('app.telegram.token')]), [
+        $client->get(strtr('/bot:token/sendMessage', [':token' => config('app.telegram.token')]), [
             'query' => [
                 'chat_id'    => config('app.telegram.admin_id'),
                 'text'       => $message,
