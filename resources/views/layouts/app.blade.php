9ef76ce9ab981d0768199930c8b51319b0fa63c1
Kris <git@k118.de>
2021-06-08 21:18:04 +0200

    🚸 Force user to reload styles and scripts if needed (#405)
    * Hardrefresh CSS and JS after new Release automaticly
    
    * Use hash function from laravel mix instead

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,13 @@
         @include('layouts.includes.meta')
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}?1611964800"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
 
         <!-- Fonts -->
         <link href="{{ asset('fonts/Nunito/Nunito.css') }}" rel="stylesheet">
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <link rel="mask-icon" href="{{ asset('images/icons/touch-icon-vector.svg') }}">
         <link rel="shortcut favicon" href="{{ asset('images/icons/favicon.ico') }}">
         <link rel="shortcut icon" sizes="512x512" href="{{ asset('images/icons/logo512.png') }}">
