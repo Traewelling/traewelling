d850053988fdad947b0b5fed3d23555112e2424c
Kristian Stöckel <git@k118.de>
2021-06-08 16:24:11 +0200

    Use hash function from laravel mix instead

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,3 +20,7 @@ mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/admin.scss", "public/css")
     .sass("resources/sass/welcome.scss", "public/css")
     .sourceMaps();
+
+if (mix.inProduction()) {
+    mix.version();
+}
