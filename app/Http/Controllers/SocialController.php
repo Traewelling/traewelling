fda263112d34bfc5fe6f99aba6c9e355fa372906
Levin Herr <github@17d.me>
2021-06-20 13:40:03 +0200

    🚑 Allow invalidation & recreation of mastodon secrets (#442)
    * recreate mastodon secrets, if keys have been invoked
    Prevent mastodon-connection if keys are invalidated.
    
    * $e

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -61,9 +61,26 @@ class SocialController extends Controller
                                                          'client_id'     => $info['client_id'],
                                                          'client_secret' => $info['client_secret'],
                                                      ]);
-                } catch (ClientException $e) {
+                } catch (ClientException) {
                     return redirect()->back()->with('error', __('user.invalid-mastodon', ['domain' => $domain]));
                 }
+            }
+            //If we ever run into a reset of Mastodon AppKeys (#), then this recreates the keys.
+            //Keys have to be set to 0 in the database, since the fields are covered by NOT NULL constraint
+            if ($server->client_id <= 1 || $server->client_secret <= 1) {
+                try {
+                    //create new app
+                    $info = Mastodon::domain($domain)->createApp(config('trwl.mastodon_appname'), config('trwl.mastodon_redirect'), 'write read');
+
+                    //save app info
+                    $server->update([
+                                        'client_id'     => $info['client_id'],
+                                        'client_secret' => $info['client_secret'],
+                                    ]);
+                } catch (ClientException) {
+                    return redirect()->back()->with('error', __('user.invalid-mastodon', ['domain' => $domain]));
+                }
+
             }
 
             //change config
