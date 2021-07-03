37237b3ed45add34a88b9aaf406afce086a389b5
Kristian Stöckel <git@k118.de>
2021-07-03 23:59:37 +0200

    Remove unused HafasTrips

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void {
         $schedule->command('trwl:cleanUpUsers')->daily();
+        $schedule->command('trwl:cleanUpHafasTrips')->daily();
         $schedule->command('trwl:refreshTrips')->everyMinute();
     }
 
