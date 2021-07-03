b88dd919d1d39413ee2d6c7361e1e5a52e3fb088
Kristian Stöckel <git@k118.de>
2021-07-04 01:03:39 +0200

    ...

--- config/trwl.php
+++ config/trwl.php
@@ -21,16 +21,16 @@ return [
 
     'base_points' => [
         'train' => [
-            'tram'            => env('BASIS_POINTS_TRAIN_TRAM', 2),
-            'bus'             => env('BASIS_POINTS_TRAIN_BUS', 2),
-            'subway'          => env('BASIS_POINTS_TRAIN_SUBWAY', 2),
-            'suburban'        => env('BASIS_POINTS_TRAIN_SUBURBAN', 3),
-            'ferry'           => env('BASIS_POINTS_TRAIN_FERRY', 3),
-            'regional'        => env('BASIS_POINTS_TRAIN_REGIONAL', 5),
-            'regionalExp'     => env('BASIS_POINTS_TRAIN_REGIONALEXP', 6),
-            'express'         => env('BASIS_POINTS_TRAIN_EXPRESS', 10),
-            'national'        => env('BASIS_POINTS_TRAIN_NATIONAL', 10),
-            'nationalExpress' => env('BASIS_POINTS_TRAIN_NATIONALEXPRESS', 10),
+            'tram'            => env('BASE_POINTS_TRAIN_TRAM', 2),
+            'bus'             => env('BASE_POINTS_TRAIN_BUS', 2),
+            'subway'          => env('BASE_POINTS_TRAIN_SUBWAY', 2),
+            'suburban'        => env('BASE_POINTS_TRAIN_SUBURBAN', 3),
+            'ferry'           => env('BASE_POINTS_TRAIN_FERRY', 3),
+            'regional'        => env('BASE_POINTS_TRAIN_REGIONAL', 5),
+            'regionalExp'     => env('BASE_POINTS_TRAIN_REGIONALEXP', 6),
+            'express'         => env('BASE_POINTS_TRAIN_EXPRESS', 10),
+            'national'        => env('BASE_POINTS_TRAIN_NATIONAL', 10),
+            'nationalExpress' => env('BASE_POINTS_TRAIN_NATIONALEXPRESS', 10),
         ]
     ]
 ];
