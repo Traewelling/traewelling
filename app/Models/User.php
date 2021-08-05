e3768d92c95a6315500da098ae68da597e086195
Kris <git@k118.de>
2021-08-06 01:04:22 +0200

    ⬆️ Update abraham/twitteroauth to v3 (#466)
    * Update abraham/twitteroauth to v3
    and update other dependencies
    
    * catch
    
    * fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
+use App\Exceptions\NotConnectedException;
+use App\Http\Controllers\Backend\Social\TwitterController;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -146,26 +147,17 @@ class User extends Authenticatable implements MustVerifyEmail
      * @deprecated
      */
     public function getTwitterUrlAttribute(): ?string {
-        $twitterUrl = null;
-        if ($this->socialProfile != null
-            && !empty($this->socialProfile->twitter_token)
-            && !empty($this->socialProfile->twitter_tokenSecret)) {
-            try {
-                $connection = new TwitterOAuth(
-                    config('trwl.twitter_id'),
-                    config('trwl.twitter_secret'),
-                    $this->socialProfile->twitter_token,
-                    $this->socialProfile->twitter_tokenSecret
-                );
-
-                $getInfo    = $connection->get('users/show', ['user_id' => $this->socialProfile->twitter_id]);
-                $twitterUrl = "https://twitter.com/" . $getInfo->screen_name;
-            } catch (Exception $exception) {
-                // The big whale time or $user has removed the api rights but has not told us yet.
-                Log::warning($exception);
-            }
+        try {
+            $connection = TwitterController::getApi($this);
+            $getInfo    = $connection->get('users/show', ['user_id' => $this->socialProfile->twitter_id]);
+            return "https://twitter.com/" . $getInfo->screen_name;
+        } catch (NotConnectedException) {
+            return null;
+        } catch (Exception $exception) {
+            // The big whale time or $user has removed the api rights but has not told us yet.
+            Log::warning($exception);
+            return null;
         }
-        return $twitterUrl;
     }
 
     public function getMastodonUrlAttribute(): ?string {
