d64f2a3fd6ae7fd1fc08c9c2c30afb0ac36d311b
Kris <git@k118.de>
2021-10-02 14:26:22 +0200

    🌐 Add validation translations (#528)

---
+++ resources/lang/sv/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+/**
+ * @copyright https://github.com/Laravel-Lang/lang MIT-License
+ */
+return [
+    'failed'   => 'Dessa uppgifter stämmer inte överens med vårt register.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'För många inloggningsförsök. Var vänlig försök igen om :seconds sekunder.',
+];
