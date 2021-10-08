60daea0b89acad1eed61d59233f626eee66ffee2
Kristian Stöckel <git@k118.de>
2021-10-08 16:13:31 +0200

    Move SocialController

--- app/Http/Controllers/Frontend/Social/SocialController.php
+++ app/Http/Controllers/Frontend/Social/SocialController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Frontend\Social;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
