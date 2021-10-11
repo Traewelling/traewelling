7ab40edd2ecd80b7182b5b540ff5ff57cc0a16b0
Levin herr <github@17d.me>
2021-10-11 09:14:49 +0200

    codacy

--- resources/components/views/Charts.vue
+++ resources/components/views/Charts.vue
@@ -318,20 +318,20 @@ export default {
                     from: this.from,
                     until: this.until,
                     filetype: type
-                }, {responseType: 'blob'})
+                }, {responseType: "blob"})
                 .then((res) => {
-                    const {data, headers} = res
-                    const fileName        = headers['content-disposition'].replace(/\w+;filename=(.*)/, '$1')
-                    const blob            = new Blob([data], {type: headers['content-type']})
-                    let dom               = document.createElement('a')
-                    let url               = window.URL.createObjectURL(blob)
-                    dom.href              = url
-                    dom.download          = decodeURI(fileName)
-                    dom.style.display     = 'none'
-                    document.body.appendChild(dom)
-                    dom.click()
-                    dom.parentNode.removeChild(dom)
-                    window.URL.revokeObjectURL(url)
+                    const {data, headers} = res;
+                    const fileName        = headers['content-disposition'].replace(/\w+;filename=(.*)/, "$1");
+                    const blob            = new Blob([data], {type: headers["content-type"]});
+                    let dom               = document.createElement("a");
+                    let url               = window.URL.createObjectURL(blob);
+                    dom.href              = url;
+                    dom.download          = decodeURI(fileName);
+                    dom.style.display     = "none";
+                    document.body.appendChild(dom);
+                    dom.click();
+                    dom.parentNode.removeChild(dom);
+                    window.URL.revokeObjectURL(url);
                 });
         },
         fetchRecentDays(delta) {
