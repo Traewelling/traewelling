a192f310c4354beafbb215d75f06c757db3de361
Kristian Stöckel <git@k118.de>
2021-10-13 14:20:10 +0200

    codacy

--- app/Http/Controllers/Backend/Social/MastodonController.php
+++ app/Http/Controllers/Backend/Social/MastodonController.php
@@ -8,6 +8,7 @@ use App\Models\MastodonServer;
 use App\Models\SocialLoginProfile;
 use App\Models\User;
 use GuzzleHttp\Exception\ClientException;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Revolution\Mastodon\Facades\Mastodon;
 
 abstract class MastodonController extends Controller
@@ -17,14 +18,14 @@ abstract class MastodonController extends Controller
      * If logged in, the user will have the login-provider added.
      * If a user with corresponding login-provider already exists, it will be returned.
      *
-     * @param \Laravel\Socialite\Contracts\User $socialiteUser
-     * @param MastodonServer                    $server
+     * @param SocialiteUser  $socialiteUser
+     * @param MastodonServer $server
      *
      * @return User model
      */
     public static function getUserFromSocialite(
-        \Laravel\Socialite\Contracts\User $socialiteUser,
-        MastodonServer                    $server
+        SocialiteUser  $socialiteUser,
+        MastodonServer $server
     ): User {
         $socialProfile = SocialLoginProfile::where('mastodon_id', $socialiteUser->id)
                                            ->where('mastodon_server', $server->id)
@@ -80,7 +81,11 @@ abstract class MastodonController extends Controller
      */
     private static function createMastodonServer(string $domain): MastodonServer {
         try {
-            $info = Mastodon::domain($domain)->createApp(config('trwl.mastodon_appname'), config('trwl.mastodon_redirect'), 'write read');
+            $info = Mastodon::domain($domain)->createApp(
+                client_name:   config('trwl.mastodon_appname'),
+                redirect_uris: config('trwl.mastodon_redirect'),
+                scopes:        'write read'
+            );
             return MastodonServer::updateOrCreate([
                                                       'domain' => $domain,
                                                   ], [
@@ -93,7 +98,7 @@ abstract class MastodonController extends Controller
         }
     }
 
-    private static function createUser(\Laravel\Socialite\Contracts\User $socialiteUser, MastodonServer $server): User {
+    private static function createUser(SocialiteUser $socialiteUser, MastodonServer $server): User {
         //TODO: Fetch profile image (see issue #5)
         $user = User::create([
                                  'name'     => SocialController::getDisplayName($socialiteUser),
@@ -103,7 +108,7 @@ abstract class MastodonController extends Controller
         return $user;
     }
 
-    private static function updateToken(User $user, \Laravel\Socialite\Contracts\User $socialiteUser, MastodonServer $server) {
+    private static function updateToken(User $user, SocialiteUser $socialiteUser, MastodonServer $server) {
         $user->socialProfile->update([
                                          'mastodon_id'     => $socialiteUser->id,
                                          'mastodon_token'  => $socialiteUser->token,
