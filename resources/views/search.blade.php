d3ea2fdbcce2343edb1570309f5268bd22c84a1e
Kris <git@k118.de>
2021-10-13 22:52:59 +0200

    🐛 Fix distance calculation (#554)
    * Fix distance calculation
    
    * Fix distance calculation

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
-@section('title')Dashboard @endsection
+@section('title', __('search-results'))
 
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 col-lg-7">
-                @if($userSearchResponse->count() == 0)
+                @if($userSearchResponse->count() === 0)
                     <div class="col-md-8 col-lg-7">
                         <div class="alert my-3 alert-danger" role="alert">
                             {{ __('user.no-user') }}
@@ -18,27 +18,40 @@
                         <div class="card-body row">
                             <div class="col-2 image-box search-image-box d-lg-flex">
                                 <a href="{{ route('account.show', ['username' => $user->username]) }}">
-                                    <img src="{{ route('account.showProfilePicture', ['username' => $user->username]) }}"
-                                         alt="profile picture">
+                                    <img src="{{route('account.showProfilePicture', ['username' => $user->username])}}"
+                                         alt="Profile picture"/>
                                 </a>
                             </div>
 
                             <div class="col ps-0">
-                                <a href="{{ route('account.show', ['username' => $user->username]) }}">
-                                    <h4>{{ $user->name }}@if($user->private_profile)<i
-                                                class="fas fa-user-lock"></i>@endif <small
-                                                class="text-muted">{{ '@'.$user->username }}</small></h4>
+                                <span class="float-end mt-3">
+                                    @include('includes.follow-button')
+                                </span>
+                                <a href="{{ route('account.show', ['username' => $user->username]) }}"
+                                   style="font-size: calc(1.26rem + .12vw)">
+                                    {{ $user->name }}
+                                    @if($user->private_profile)
+                                        <i class="fas fa-user-lock"></i>
+                                    @endif
+                                    <small class="text-muted">{{ '@'.$user->username }}</small>
                                 </a>
-                                <h6>
-                                    <small>
-                                        <span class="font-weight-bold"><i class="fa fa-route d-inline"></i>&nbsp;{{ $user->train_distance }}</span><span
-                                                class="small font-weight-lighter">km</span>
-                                        <span class="font-weight-bold ps-sm-2"><i class="fa fa-stopwatch d-inline"></i>&nbsp;{!! durationToSpan(secondsToDuration($user->train_duration * 60)) !!}</span>
-                                        <span class="font-weight-bold ps-sm-2"><i class="fa fa-dice-d20 d-inline"></i>&nbsp;{{ $user->points }}</span><span
-                                                class="small font-weight-lighter">{{__('profile.points-abbr')}}</span>
-                                    </small>
-                                </h6>
-                                @include('includes.follow-button')
+                                <br/>
+                                <span style="font-size: 0.875em;">
+                                    <span class="font-weight-bold">
+                                        <i class="fa fa-route d-inline"></i>
+                                        {{ number($user->train_distance / 1000) }}
+                                    </span>
+                                    <span class="small font-weight-lighter">km</span>
+                                    <span class="font-weight-bold ps-sm-2">
+                                        <i class="fa fa-stopwatch d-inline"></i>
+                                        {!! durationToSpan(secondsToDuration($user->train_duration * 60)) !!}
+                                    </span>
+                                    <span class="font-weight-bold ps-sm-2">
+                                        <i class="fa fa-dice-d20 d-inline"></i>
+                                        {{ $user->points }}
+                                    </span>
+                                    <span class="small font-weight-lighter">{{__('profile.points-abbr')}}</span>
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -50,5 +63,5 @@
         </div>
         @include('includes.edit-modal')
         @include('includes.delete-modal')
-    </div><!--- /container -->
+    </div>
 @endsection
