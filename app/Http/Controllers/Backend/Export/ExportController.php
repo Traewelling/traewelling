4282d645baecb6aaae68c914f84077c8c6df13eb
Kristian Stöckel <git@k118.de>
2021-10-14 22:03:29 +0200

    <3

--- app/Http/Controllers/Backend/Export/ExportController.php
+++ app/Http/Controllers/Backend/Export/ExportController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers\Backend\Export;
 
+use App\Http\Controllers\Backend\Export\ExportController as ExportBackend;
+use App\Http\Controllers\Backend\Export\Format\CsvExportController;
+use App\Http\Controllers\Backend\Export\Format\JsonExportController;
 use App\Http\Controllers\Controller;
 use App\Models\Status;
 use App\Models\User;
+use Barryvdh\DomPDF\Facade as PDF;
 use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 abstract class ExportController extends Controller
 {
@@ -17,5 +24,67 @@ abstract class ExportController extends Controller
                      ->where('train_checkins.departure', '>=', $timestampFrom->toIso8601String())
                      ->where('train_checkins.departure', '<=', $timestampTo->toIso8601String())
                      ->get();
+    }
+
+    public static function generateExport(Carbon $from, Carbon $until, string $filetype) {
+        if ($filetype === 'json') {
+            return self::exportJson($from, $until);
+        }
+
+        if ($filetype === 'pdf') {
+            return self::exportPdf($from, $until);
+        }
+
+        if ($filetype === 'csv') {
+            return self::exportCsv($from, $until);
+        }
+    }
+
+    private static function exportJson(Carbon $begin, Carbon $end): JsonResponse {
+        $headers    = [
+            'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0',
+            'Content-type'        => 'text/json',
+            'Content-Disposition' => sprintf(
+                'attachment; filename="Traewelling_export_%s_to_%s.json"',
+                $begin->format('Y-m-d'),
+                $end->format('Y-m-d')
+            ),
+            'Expires'             => '0',
+            'Pragma'              => 'public',
+        ];
+        $exportData = JsonExportController::generateExport(auth()->user(), $begin, $end);
+        return Response::json(data: $exportData, headers: $headers);
+    }
+
+    private static function exportPdf(Carbon $begin, Carbon $end) {
+        return PDF::loadView('pdf.export-template', [
+            'statuses' => ExportBackend::getExportableStatuses(auth()->user(), $begin, $end),
+            'begin'    => $begin,
+            'end'      => $end
+        ])
+                  ->setPaper('a4', 'landscape')
+                  ->download(
+                      sprintf(
+                          'Traewelling_export_%s_to_%s.pdf',
+                          $begin->format('Y-m-d'),
+                          $end->format('Y-m-d')
+                      )
+                  );
+    }
+
+    private static function exportCsv(Carbon $begin, Carbon $end): StreamedResponse {
+        $headers      = [
+            'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0',
+            'Content-type'        => 'text/csv',
+            'Content-Disposition' => sprintf(
+                'attachment; filename="Traewelling_export_%s_to_%s.csv"',
+                $begin->format('Y-m-d'),
+                $end->format('Y-m-d')
+            ),
+            'Expires'             => '0',
+            'Pragma'              => 'public',
+        ];
+        $exportStream = CsvExportController::generateExport(auth()->user(), $begin, $end);
+        return Response::stream($exportStream, 200, $headers);
     }
 }
--- app/Http/Controllers/Frontend/Export/ExportController.php
+++ app/Http/Controllers/Frontend/Export/ExportController.php
@@ -3,17 +3,11 @@
 namespace App\Http\Controllers\Frontend\Export;
 
 use App\Http\Controllers\Backend\Export\ExportController as ExportBackend;
-use App\Http\Controllers\Backend\Export\Format\CsvExportController;
-use App\Http\Controllers\Backend\Export\Format\JsonExportController;
 use App\Http\Controllers\Controller;
-use Barryvdh\DomPDF\Facade as PDF;
 use Carbon\Carbon;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Validation\Rule;
 use Illuminate\View\View;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ExportController extends Controller
 {
@@ -26,73 +20,15 @@ class ExportController extends Controller
 
     public function renderExport(Request $request) {
         $validated = $request->validate([
-                                            'from'    => ['required', 'date', 'before_or_equal:until'],
-                                            'until'      => ['required', 'date', 'after_or_equal:from'],
+                                            'from'     => ['required', 'date', 'before_or_equal:until'],
+                                            'until'    => ['required', 'date', 'after_or_equal:from'],
                                             'filetype' => ['required', Rule::in(['json', 'csv', 'pdf'])],
                                         ]);
 
-        $from = Carbon::parse($validated['from']);
-        $until   = Carbon::parse($validated['until']);
-
-        if ($validated['filetype'] === 'json') {
-            return $this->exportJson($from, $until);
-        }
-
-        if ($validated['filetype'] === 'pdf') {
-            return $this->exportPdf($from, $until);
-        }
-
-        if ($validated['filetype'] === 'csv') {
-            return $this->exportCsv($from, $until);
-        }
-    }
-
-    private function exportJson(Carbon $begin, Carbon $end): JsonResponse {
-        $headers    = [
-            'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0',
-            'Content-type'        => 'text/json',
-            'Content-Disposition' => sprintf(
-                'attachment; filename="Traewelling_export_%s_to_%s.json"',
-                $begin->format('Y-m-d'),
-                $end->format('Y-m-d')
-            ),
-            'Expires'             => '0',
-            'Pragma'              => 'public',
-        ];
-        $exportData = JsonExportController::generateExport(auth()->user(), $begin, $end);
-        return Response::json(data: $exportData, headers: $headers);
-    }
-
-
-    private function exportPdf(Carbon $begin, Carbon $end) {
-        return PDF::loadView('pdf.export-template', [
-            'statuses' => ExportBackend::getExportableStatuses(auth()->user(), $begin, $end),
-            'begin'    => $begin,
-            'end'      => $end
-        ])
-                  ->setPaper('a4', 'landscape')
-                  ->download(
-                      sprintf(
-                          'Traewelling_export_%s_to_%s.pdf',
-                          $begin->format('Y-m-d'),
-                          $end->format('Y-m-d')
-                      )
-                  );
-    }
-
-    private function exportCsv(Carbon $begin, Carbon $end): StreamedResponse {
-        $headers      = [
-            'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0',
-            'Content-type'        => 'text/csv',
-            'Content-Disposition' => sprintf(
-                'attachment; filename="Traewelling_export_%s_to_%s.csv"',
-                $begin->format('Y-m-d'),
-                $end->format('Y-m-d')
-            ),
-            'Expires'             => '0',
-            'Pragma'              => 'public',
-        ];
-        $exportStream = CsvExportController::generateExport(auth()->user(), $begin, $end);
-        return Response::stream($exportStream, 200, $headers);
+        return ExportBackend::generateExport(
+            from:     Carbon::parse($validated['from']),
+            until:    Carbon::parse($validated['until']),
+            filetype: $validated['filetype']
+        );
     }
 }
