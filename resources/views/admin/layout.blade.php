fc77b3ce64eb9de3a98ba4838ebdbcdd31f45e80
Levin Herr <github@17d.me>
2021-10-16 00:32:54 +0200

    fixed css (https://getbootstrap.com/docs/5.1/examples/sidebars/sidebars.css) + small layouting in sidebar

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,18 +9,13 @@
         <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
         <script src="{{ asset('js/admin.js') }}"></script>
 
-        <style>
-            #wrapper {
-                display: flex;
-                width: 100%;
-                align-items: stretch;
-            }
-        </style>
     </head>
+
     <body>
-        <div id="wrapper">
-            <nav class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <main>
+            <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+                <a href="{{ route('admin.dashboard') }}"
+                   class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <img src="{{ asset('images/icons/touch-icon-vector.svg') }}" alt="{{ config('app.name') }} Logo"
                          class="brand-image me-3" style="width: 30px; opacity: 0.8">
                     <span class="fs-4">TRWL Admin</span>
@@ -28,77 +23,102 @@
                 <hr>
                 <ul class="nav nav-pills flex-column mb-auto">
                     <li class="nav-item">
-                        <a href="/" class="nav-link">
-                            <i class="bi bi-skip-backward me-2"></i>
+                        <a href="{{ route('dashboard') }}" class="nav-link">
+                            <i class="bi bi-skip-backward me-2" aria-hidden="true"></i>
                             Zurück zu Träwelling
                         </a>
                     </li>
                     <li>
                         <a href="{{ route('admin.dashboard') }}"
                            class="nav-link text-white {{ request()->is('admin') ? 'active' : '' }}">
-                            <i class="bi bi-speedometer2"></i>
+                            <i class="bi bi-speedometer2 me-2" aria-hidden="true"></i>
                             Dashboard
                         </a>
                     </li>
                     <li>
                         <a href="{{route('admin.events')}}"
                            class="nav-link text-white {{ request()->is('admin.events') ? 'active' : '' }}">
-                            <i class="bi bi-calendar2-event"></i>
+                            <i class="bi bi-calendar2-event me-2" aria-hidden="true"></i>
                             Veranstaltungen
                         </a>
                     </li>
                     <li>
                         <a href="{{ route('admin.status') }}"
                            class="nav-link text-white {{ request()->is('admin.status') ? 'active' : '' }}">
-                            <i class="bi bi-person-rolodex"></i>
+                            <i class="bi bi-person-rolodex me-2" aria-hidden="true"></i>
                             Status
                         </a>
                     </li>
                 </ul>
                 <hr>
-                <span class="d-flex align-items-center text-white text-decoration-none">
+                <div class="dropdown">
+                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                       id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="{{ route('account.showProfilePicture', ['username' => auth()->user()->username]) }}"
                              alt="" width="32" height="32" class="rounded-circle me-2">
-                    <strong>{{auth()->user()->name}}</strong>
-                </span>
-            </nav>
-            <main style="width: calc(100%  - 280px)">
-                <div class="container-fluid">
-                    @hasSection('title')
-                        <h1 class="mt-3 mb-3 text-dark">@yield('title')</h1>
-                    @endif
-                    @if ($errors->any())
+                        <strong>{{auth()->user()->name}}</strong>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                        <li>
+                            <a class="dropdown-item" href="{{ url('profile/'.Auth::user()->username) }}">
+                                <i class="fas fa-user" aria-hidden="true"></i> {{ __('menu.profile') }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('settings') }}">
+                                <i class="fas fa-cog" aria-hidden="true"></i> {{ __('menu.settings') }}
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <button class="dropdown-item" form="logout-form" type="submit">
+                                <i class="fas fa-sign-out-alt" aria-hidden="true"></i> {{ __('menu.logout') }}
+                            </button>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="container-fluid">
+                @hasSection('title')
+                    <h1 class="mt-3 mb-3 text-dark">@yield('title')</h1>
+                @endif
+                @if ($errors->any())
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="alert alert-danger">
+                                <h2 class="text-alert">Es sind Fehler aufgetreten:</h2>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                    @if(Session::has('alert-' . $msg))
                         <div class="row">
                             <div class="col-md-12">
-                                <div class="alert alert-danger">
-                                    <h2 class="text-alert">Es sind Fehler aufgetreten:</h2>
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
+                                <p class="alert alert-{{ $msg }}">
+                                    {!! Session::get('alert-' . $msg) !!}
+                                </p>
+                                <hr/>
                             </div>
                         </div>
                     @endif
-                    @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-                        @if(Session::has('alert-' . $msg))
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <p class="alert alert-{{ $msg }}">
-                                        {!! Session::get('alert-' . $msg) !!}
-                                    </p>
-                                    <hr/>
-                                </div>
-                            </div>
-                        @endif
-                        {{ Session::forget('alert-' . $msg) }}
-                    @endforeach
+                    {{ Session::forget('alert-' . $msg) }}
+                @endforeach
 
-                    @yield('content')
-                </div>
-            </main>
-        </div>
-        @yield('scripts')
+                @yield('content')
+            </div>
+        </main>
     </body>
 </html>
