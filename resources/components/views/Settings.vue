6ffd7c517997382daecc2ac17079efab6b629042
Levin Herr <github@17d.me>
2021-10-28 23:34:36 +0200

    profile & privacy

--- resources/components/views/Settings.vue
+++ resources/components/views/Settings.vue
@@ -42,172 +42,96 @@
                 role="tabpanel"
                 aria-labelledby="ex1-tab-1"
             >
-                {{ i18n.get("_.settings.heading.profile")}}
+                <h2>{{ i18n.get("_.settings.heading.profile") }}</h2>
+
+                <h6 class="text-capitalize text-muted border-bottom my-5">{{ i18n.get('_.settings.picture') }}</h6>
+                <div class="row text-start">
+                    <div class="col-3">
+                        <img :alt="i18n.get('_.settings.picture')"
+                             :src="`/profile/${$auth.user().username}/profilepicture`"
+                             class="rounded-circle w-100 d-block">
+                    </div>
+                    <div class="col-8 d-flex align-items-center">
+                        <button class="btn btn-primary me-1">
+                            {{ i18n.get("_.settings.upload-image") }}
+                        </button>
+                        <button class="btn btn-outline-danger">
+                            <i aria-hidden="true" class="fas fa-trash"></i>
+                            <span class="sr-only">{{ i18n.get("_.settings.delete-profile-picture-btn") }}</span>
+                        </button>
+                    </div>
+                </div>
+
+                <h6 class="text-capitalize text-muted border-bottom my-5">{{
+                        i18n.get('_.settings.title-profile')
+                    }}</h6>
+                <div class="form-floating mb-3">
+                    <input id="username" :value="$auth.user().username" class="form-control" placeholder="@Gertrud"
+                           type="text">
+                    <label for="username">{{ i18n.get('_.user.username') }}</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input id="displayname" :value="$auth.user().displayName" class="form-control" placeholder="Gertrud"
+                           type="text">
+                    <label for="displayname">{{ i18n.get("_.user.displayname") }}</label>
+                </div>
+
+                <h6 class="text-capitalize text-muted border-bottom my-5">{{
+                        i18n.get('_.settings.title-privacy')
+                    }}</h6>
+
+                <div class="row">
+                    <div class="col">
+                        <label aria-label="visibilityDropdown" class="form-check-label">
+                            {{ i18n.get('_.settings.visibility.default') }}
+                        </label>
+                    </div>
+                    <div class="col">
+                        <FADropdown id="visibilityDropdown" :dropdown-content="visibility" :pre-select="0"
+                                    class="float-end" showText="true"></FADropdown>
+                    </div>
+                </div>
+                <div class="row mt-3 pt-3">
+                    <div class="col-9 col-md-11">
+                        <label class="form-check-label" for="privateProfileSwitch">
+                            {{ i18n.get('_.user.private-profile') }}<br>
+                            <span class="small text-muted">{{ i18n.get("_.settings.visibility.disclaimer") }}</span>
+                        </label>
+                    </div>
+                    <div class="col form-check form-switch">
+                        <input id="privateProfileSwitch" class="form-check-input float-end" type="checkbox"/>
+                    </div>
+                </div>
+                <div class="row mt-3 pt-3">
+                    <div class="col-9 col-md-11">
+                        <label class="form-check-label" for="preventIndexSwitch">
+                            {{ i18n.get('_.settings.hide-search-engines') }}<br>
+                            <span class="text-muted small">{{
+                                    i18n.get('_.settings.search-engines.description')
+                                }}</span>
+                        </label>
+                    </div>
+                    <div class="col form-check form-switch">
+                        <input id="preventIndexSwitch" class="form-check-input float-end" type="checkbox"/>
+                    </div>
+                </div>
+                <div class="row mt-3 pt-3">
+                    <div class="col">
+                        <label class="form-check-label" for="dblSwitch">
+                            {{ i18n.get('_.user.always-dbl') }}
+                        </label>
+                    </div>
+                    <div class="col form-check form-switch">
+                        <input id="dblSwitch" class="form-check-input float-end" type="checkbox"/>
+                    </div>
+                </div>
+
             </div>
             <div class="tab-pane fade" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
                 Tab 2 content
             </div>
             <div class="tab-pane fade" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
                 Tab 3 content
-            </div>
-        </div>
-        <!-- Tabs content -->
-        <div class="row g-2">
-            <div class="col-lg-6 col-sm-12">
-                <Card :title="i18n.get('_.settings.picture')">
-                    <div class="row text-center">
-                        <img :alt="i18n.get('_.settings.picture')"
-                             :src="`/profile/${$auth.user().username}/profilepicture`"
-                             class="rounded-circle mx-auto w-25 d-block">
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col text-end">
-                            <button class="btn btn-outline-danger float-start">
-                                <i aria-hidden="true" class="fas fa-trash"></i>
-                                <span class="sr-only">{{ i18n.get("_.settings.delete-profile-picture-btn") }}</span>
-                            </button>
-                            <button class="btn btn-primary">
-                                {{ i18n.get("_.settings.upload-image") }}
-                            </button>
-                        </div>
-                    </div>
-                </Card>
-            </div>
-            <div class="col-lg-6 col-sm-12">
-                <Card :title="i18n.get('_.settings.title-profile')">
-                    <div class="row">
-                        <div class="col-4">
-                            <label for="username">
-                                {{ i18n.get('_.user.username') }}
-                            </label>
-                        </div>
-                        <div class="col-6">
-                            <div class="input-group">
-                                <span class="input-group-text">@</span>
-                                <input id="username" :value="$auth.user().username"
-                                       autofocus
-                                       class="form-control" required type="text"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-4">
-                            <label for="displayname">
-                                {{ i18n.get("_.user.displayname") }}
-                            </label>
-                        </div>
-                        <div class="col-6">
-                            <input id="displayname" :value="$auth.user().displayName" class="form-control" type="text">
-                        </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-4">
-                            <label for="mail">
-                                {{ i18n.get("_.user.email") }}
-                            </label>
-                        </div>
-                        <div class="col-6">
-                            <input id="mail" class="form-control" type="text" value="">
-                        </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col text-end">
-                            <button class="btn btn-primary">{{ i18n.get("_.settings.btn-update") }}</button>
-                        </div>
-                    </div>
-                </Card>
-            </div>
-            <div class="col-lg-6 col-sm-12">
-                <Card :title="i18n.get('_.settings.title-privacy')">
-                    <div class="row">
-                        <div class="col">
-                            <label class="form-check-label" for="privateProfileSwitch">
-                                {{ i18n.get('_.settings.visibility.default') }}
-                            </label>
-                        </div>
-                        <div class="col">
-                            <FADropdown :dropdown-content="visibility" :pre-select="0" class="float-end"
-                                        showText="true"></FADropdown>
-                        </div>
-                    </div>
-                    <div class="row border-top mt-3 pt-3">
-                        <div class="col">
-                            <label class="form-check-label" for="privateProfileSwitch">
-                                {{ i18n.get('_.user.private-profile') }}
-                            </label>
-                        </div>
-                        <div class="col form-check form-switch">
-                            <input id="privateProfileSwitch" class="form-check-input float-end" type="checkbox"/>
-                        </div>
-                    </div>
-                    <div class="row border-top mt-3 pt-3">
-                        <div class="col">
-                            <label class="form-check-label" for="preventIndexSwitch">
-                                {{ i18n.get('_.settings.hide-search-engines') }}
-                                <i :title="i18n.get('_.settings.search-engines.description')" aria-hidden="true"
-                                   class="fas fa-info-circle" data-mdb-toggle="tooltip"></i>
-                            </label>
-                        </div>
-                        <div class="col form-check form-switch">
-                            <input id="preventIndexSwitch" class="form-check-input float-end" type="checkbox"/>
-                        </div>
-                    </div>
-                    <div class="row border-top mt-3 pt-3">
-                        <div class="col">
-                            <label class="form-check-label" for="dblSwitch">
-                                {{ i18n.get('_.user.always-dbl') }}
-                            </label>
-                        </div>
-                        <div class="col form-check form-switch">
-                            <input id="dblSwitch" class="form-check-input float-end" type="checkbox"/>
-                        </div>
-                    </div>
-                </Card>
-            </div>
-            <div class="col-lg-6 col-sm-12">
-                <Card :title="i18n.get('_.settings.title-password')">
-                    <form>
-                        <input autocomplete="username" name="username" type="hidden">
-                        <div class="row">
-                            <label class="col-4" for="currentPassword">
-                                {{ i18n.get("_.settings.current-password") }}
-                            </label>
-                            <div class="col-6">
-                                <input id="currentPassword" autocomplete="current-password"
-                                       class="form-control"
-                                       type="password"/>
-                            </div>
-                        </div>
-                        <div class="row mt-4">
-                            <label class="col-4" for="password">
-                                {{ i18n.get("_.settings.new-password") }}
-                            </label>
-
-                            <div class="col-6">
-                                <input id="password" autocomplete="new-password"
-                                       class="form-control"
-                                       required type="password"/>
-                            </div>
-                        </div>
-                        <div class="row mt-4">
-                            <label class="col-4" for="password-confirm">
-                                {{ i18n.get("_.settings.confirm-password") }}
-                            </label>
-                            <div class="col-6">
-                                <input id="password-confirm" autocomplete="new-password"
-                                       class="form-control"
-                                       name="password_confirmation" required type="password"/>
-                            </div>
-                        </div>
-                        <div class="row mt-4">
-                            <div class="col text-end">
-                                <button class="btn btn-primary" type="submit">
-                                    {{ i18n.get("_.settings.btn-update") }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </Card>
             </div>
         </div>
     </LayoutBasicNoSidebar>
