92f6b7790037906ea2a768d86a7048f75aeeddf8
Jonas Möller <mail@jonasmoeller.de>
2021-10-28 22:43:31 +0200

    🐳 WIP: docker deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'apache2-foreground' ]; then
-    wait-for-it $DB_HOST:${DB_PORT:=3306}
+    wait-for-it "$DB_HOST:${DB_PORT:=3306}"
     cd /var/www/html
     runuser -u www-data -- php artisan migrate
     runuser -u www-data -- php artisan storage:link
