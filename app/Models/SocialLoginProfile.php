20efdfeaf6c468e4d9f901f61c618cf36799e788
Kristian Stöckel <git@k118.de>
2021-11-03 23:36:14 +0100

    Use laravel own encryptable casting

--- app/Models/SocialLoginProfile.php
+++ app/Models/SocialLoginProfile.php
@@ -2,23 +2,22 @@
 
 namespace App\Models;
 
-use App\Traits\Encryptable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SocialLoginProfile extends Model
 {
 
-    use Encryptable;
-
-    protected       $fillable    = [
+    protected $fillable = [
         'user_id',
         'twitter_id', 'twitter_token', 'twitter_tokenSecret',
         'mastodon_id', 'mastodon_server', 'mastodon_token'
     ];
-    protected       $hidden      = ['twitter_token', 'twitter_tokenSecret', 'mastodon_server', 'mastodon_token'];
-    protected array $encryptable = [
-        'twitter_token', 'twitter_tokenSecret', 'mastodon_token'
+    protected $hidden   = ['twitter_token', 'twitter_tokenSecret', 'mastodon_server', 'mastodon_token'];
+    protected $casts    = [
+        'twitter_token'       => 'encrypted',
+        'twitter_tokenSecret' => 'encrypted',
+        'mastodon_token'      => 'encrypted',
     ];
 
     public function user(): BelongsTo {
