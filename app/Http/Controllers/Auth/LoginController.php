125b63956bc4f3ade5f016a6acae65275ecb7ba6
Levin Herr <github@17d.me>
2021-11-03 20:05:52 +0100

    Challenge frontend auth with user or mail

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -40,5 +42,26 @@ class LoginController extends Controller
 
     protected function authenticated(Request $request, $user): void {
         $user->update(['last_login' => Carbon::now()->toIso8601String()]);
+    }
+
+    public function login(Request $request): RedirectResponse {
+        $validated = $request->validate(['login' => 'required', 'password' => 'required', 'remember' => 'nullable|in:1']);
+
+        $email       = $validated['login'];
+        $password    = $validated['password'];
+        $remember_me = $request->remember;
+
+        $login_type = filter_var($email, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        if (Auth::attempt([$login_type => $email, 'password' => $password], $remember_me)) {
+            //Auth successful here
+            return redirect()->intended($this->redirectPath());
+        }
+
+        return redirect()->back()
+                         ->withInput()
+                         ->withErrors([
+                                          'login_error' => 'These credentials do not match our records.',
+                                      ]);
     }
 }
