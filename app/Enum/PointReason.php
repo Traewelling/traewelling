8ca6cd121e3ea1c7f0b336b1797aea13f227dcf5
Kristian Stöckel <git@k118.de>
2021-11-28 17:17:57 +0100

    codacy

--- app/Enum/PointReason.php
+++ app/Enum/PointReason.php
@@ -5,8 +5,8 @@ namespace App\Enum;
 
 enum PointReason: int
 {
-    case IN_TIME        = 0;
-    case GOOD_ENOUGH    = 1;
+    case IN_TIME = 0;
+    case GOOD_ENOUGH = 1;
     case NOT_SUFFICIENT = 2;
-    case FORCED         = 3;
+    case FORCED = 3;
 }
