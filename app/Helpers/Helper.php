236e02cda4ab6e7e0ad20866ea8e252181aaa9e8
Kris <git@k118.de>
2021-11-30 13:30:59 +0100

    🚸 Improve station search (#608)
    * Improve station search
    
    * codacy
    
    * remove stationlink helper because it is no longer needed
    
    * remove stationlink helper because it is no longer needed
    
    * sorry i missed that .__.
    
    * sorry i missed that .__.
    
    * Add tests
    
    * Formatting
    
    * Fix
    
    * id -> ibnr
    
    * fix
    
    * codacy
    
    Co-authored-by: Levin Herr <github@17d.me>

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -32,6 +32,7 @@ function number($number, $decimals = 2) {
  * Calculate hours and minutes from a given duration in seconds.
  *
  * @param int $seconds How long in seconds?
+ *
  * @return array with `hours`, `minutes` and `showHours`.
  * @deprecated as soon as vue is implemented
  */
@@ -47,6 +48,7 @@ function secondsToDuration($seconds): array {
 
 /**
  * @param array $duration from the secondsToDuration
+ *
  * @return string
  * @deprecated as soon as vue is implemented
  */
@@ -58,34 +60,4 @@ function durationToSpan($duration): string {
     }
 
     return $return;
-}
-
-/**
- * @param $name
- * @param string $classes
- * @return string
- * @deprecated as soon as vue is implemented
- */
-function stationLink($name, $classes = "text-trwl clearfix"): string {
-    $urlname = $name;
-
-    switch ($name) {
-        // Those are stations that you can ride to but you can't search for them.
-        case $name == "Köln Messe/Deutz Gl. 9-10":
-        case $name == "Köln Messe/Deutz Gl.11-12":
-            $urlname = "Köln Messe/Deutz";
-            break;
-
-        // Hamburg's Landungsbrücken has three bridges [1..3], but you cannot search for them.
-        case preg_match('/Landungsbr.*cken Br.*cke \d/i', $name) > 0:
-            $urlname = "Landungsbrücken, Hamburg";
-            break;
-    }
-
-    return strtr('<a href=":href?provider=train&station=:station" class=":classes">:name</a>', [
-        ':href'    => route('trains.stationboard'),
-        ':station' => urlencode($urlname),
-        ':classes' => $classes,
-        ':name'    => $name
-    ]);
 }
