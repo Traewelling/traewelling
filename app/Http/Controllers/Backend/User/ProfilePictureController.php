a7c9df0e49c00da4853b80ac502ac8d721f916b5
Kristian Stöckel <git@k118.de>
2021-12-05 18:23:56 +0100

    Reorder structure

--- app/Http/Controllers/Backend/User/ProfilePictureController.php
+++ app/Http/Controllers/Backend/User/ProfilePictureController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Backend\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManagerStatic as Image;
 
 abstract class ProfilePictureController extends Controller
 {
@@ -11,8 +14,53 @@ abstract class ProfilePictureController extends Controller
     public static function getUrl(User $user): string {
         if ($user->avatar === null) {
             //Return default route to generate users avatar with matching color
-            return route('account.showProfilePicture', ['username' => $user->username]);
+            return route('profile.picture', ['username' => $user->username]);
         }
         return url('/uploads/avatars/' . $user->avatar);
+    }
+
+    public static function generateProfilePicture(User $user): array {
+        $publicPath = public_path('/uploads/avatars/' . $user->avatar);
+
+        if ($user->avatar === null || !file_exists($publicPath)) {
+            return [
+                'picture'   => self::generateDefaultAvatar($user),
+                'extension' => 'png'
+            ];
+        }
+
+        try {
+            $ext     = pathinfo($publicPath, PATHINFO_EXTENSION);
+            $picture = File::get($publicPath);
+            return [
+                'picture'   => $picture,
+                'extension' => $ext
+            ];
+        } catch (Exception $exception) {
+            report($exception);
+            return [
+                'picture'   => self::generateDefaultAvatar($user),
+                'extension' => 'png'
+            ];
+        }
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return string Encoded PNG Image
+     */
+    private static function generateDefaultAvatar(User $user): string {
+        $hash           = 0;
+        $usernameLength = strlen($user->username);
+        for ($i = 0; $i < $usernameLength; $i++) {
+            $hash = ord(substr($user->username, $i, 1)) + (($hash << 5) - $hash);
+        }
+
+        $hex = dechex($hash & 0x00FFFFFF);
+
+        return Image::canvas(512, 512, $hex)
+                    ->insert(public_path('/img/user.png'))
+                    ->encode('png')->getEncoded();
     }
 }
