456186f468f0c2025057c1db4cfdb10fc4284ae8
Kristian Stöckel <git@k118.de>
2022-02-05 15:34:27 +0100

    no message

--- app/Http/Resources/StatusResource.php
+++ app/Http/Resources/StatusResource.php
@@ -17,31 +17,31 @@ class StatusResource extends JsonResource
      */
     public function toArray($request): array {
         return [
-            "id"             => (int) $this->id,
-            "body"           => (string) $this->body,
-            "type"           => (string) $this->type,
-            "user"           => (int) $this->user->id,
-            "username"       => (string) $this->user->username,
+            'id'             => (int) $this->id,
+            'body'           => (string) $this->body,
+            'type'           => (string) $this->type,
+            'user'           => (int) $this->user->id,
+            'username'       => (string) $this->user->username,
             'profilePicture' => ProfilePictureController::getUrl($this->user),
-            "preventIndex"   => (bool) $this->user->prevent_index,
-            "business"       => (int) $this->business,
-            "visibility"     => (int) $this->visibility,
-            "likes"          => (int) $this->likes->count(),
-            "liked"          => (bool) $this->favorited,
-            "createdAt"      => $this->created_at->toIso8601String(),
-            "train"          => [
-                "trip"        => (int) $this->trainCheckin->HafasTrip->id,
-                "category"    => (string) $this->trainCheckin->HafasTrip->category,
-                "number"      => (string) $this->trainCheckin->HafasTrip->number,
-                "lineName"    => (string) $this->trainCheckin->HafasTrip->linename,
-                "distance"    => (int) $this->trainCheckin->distance, //TODO: Levin, das hier sind jetzt Meter (vorher km). Das muss in Vue angepasst werden.
-                "points"      => (int) $this->trainCheckin->points,
-                "duration"    => (int) $this->trainCheckin->duration,
-                "speed"       => (float) $this->trainCheckin->speed,
-                "origin"      => new StopoverResource($this->trainCheckin->origin_stopover),
-                "destination" => new StopoverResource($this->trainCheckin->destination_stopover),
+            'preventIndex'   => (bool) $this->user->prevent_index,
+            'business'       => (int) $this->business->value,
+            'visibility'     => (int) $this->visibility->value,
+            'likes'          => (int) $this->likes->count(),
+            'liked'          => (bool) $this->favorited,
+            'createdAt'      => $this->created_at->toIso8601String(),
+            'train'          => [
+                'trip'        => (int) $this->trainCheckin->HafasTrip->id,
+                'category'    => (string) $this->trainCheckin->HafasTrip->category->value,
+                'number'      => (string) $this->trainCheckin->HafasTrip->number,
+                'lineName'    => (string) $this->trainCheckin->HafasTrip->linename,
+                'distance'    => (int) $this->trainCheckin->distance, //TODO: Das hier sind jetzt Meter (vorher km). Das muss in Vue angepasst werden.
+                'points'      => (int) $this->trainCheckin->points,
+                'duration'    => (int) $this->trainCheckin->duration,
+                'speed'       => (float) $this->trainCheckin->speed,
+                'origin'      => new StopoverResource($this->trainCheckin->origin_stopover),
+                'destination' => new StopoverResource($this->trainCheckin->destination_stopover),
             ],
-            "event"          => new EventResource($this?->event)
+            'event'          => new EventResource($this?->event)
         ];
     }
 }
