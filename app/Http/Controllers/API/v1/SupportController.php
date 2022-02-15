e608d3462f7a60e9bd46c22ebd1f0a4972ecc520
Levin herr <github@17d.me>
2022-02-15 11:03:23 +0100

    add api support route

---
+++ app/Http/Controllers/API/v1/SupportController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\API\ResponseController;
+use App\Http\Controllers\Backend\Support\TicketController;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class SupportController extends ResponseController
+{
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function createTicket(Request $request): JsonResponse {
+        $validated = $request->validate([
+                                            'subject' => ['required', 'string', 'max:255'],
+                                            'message' => ['required', 'string',]
+                                        ]);
+
+        try {
+            $ticketNumber = TicketController::createTicket(
+                user:    auth()->user(),
+                subject: $validated['subject'],
+                message: $validated['message'],
+            );
+            return $this->sendv1Response(['ticket' => $ticketNumber], 201);
+        } catch (GuzzleException $exception) {
+            report($exception);
+            return $this->sendError(null, 503);
+        }
+    }
+}
