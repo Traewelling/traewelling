0a7b0e1537cb329230e1404df51d4bdac7ce08a9
Kristian Stöckel <git@k118.de>
2022-03-26 12:32:13 +0100

    fix

--- database/migrations/2022_03_11_000000_migrate_train_checkin_category.php
+++ database/migrations/2022_03_11_000000_migrate_train_checkin_category.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\HafasTravelType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
@@ -24,61 +25,61 @@ return new class extends Migration
     }
 
     private static array $migrationData = [
-        "ATR"    => "",
-        "AVE"    => "",
-        "BAT"    => "",
-        "BAV"    => "",
-        "BUS"    => "suburban",
-        "CNL"    => "national", // DB City Night Line, discontinued
-        "D"      => "",
-        "E"      => "",
-        "EC"     => "national", // EuroCity
-        "EM"     => "",
-        "EN"     => "national", // EuroNight
-        "ES"     => "",
-        "EST"    => "",
-        "EXT"    => "",
-        "FAE"    => "",
-        "FixBus" => "national",
-        "FUN"    => "",
-        "GEX"    => "",
-        "IC"     => "nationalExpress",
-        "ICB"    => "",
-        "ICE"    => "nationalExpress",
-        "ICN"    => "",
-        "IN"     => "",
-        "IR"     => "",
-        "IRE"    => "nationalExpress",
-        "KAT"    => "",
-        "M"      => "",
-        "NFB"    => "",
-        "NFO"    => "",
-        "NFT"    => "",
-        "NJ"     => "national",
-        "NZ"     => "",
-        "OIC"    => "",
-        "PE"     => "",
-        "R"      => "national",
-        "RB"     => "regional",
-        "RE"     => "regionalExp",
-        "RJ"     => "national", // OeBB RailJet
-        "RJX"    => "nationalExpress", // OeBB Railjet Express
-        "S"      => "regional",
-        "S12"    => "regional",
-        "S23"    => "regional",
-        "S27"    => "regional",
-        "S3"     => "regional",
-        "S5"     => "regional",
-        "S6"     => "regional",
-        "S8"     => "regional",
-        "T"      => "tram",
-        "TAL"    => "",
-        "TER"    => "",
-        "TGV"    => "nationalExpress", // SNCF TGV
-        "THA"    => "nationalExpress", // Thalys
-        "TLK"    => "",
-        "VAE"    => "",
-        "WB"     => "",
-        "X2"     => "",
+        "ATR"    => HafasTravelType::NATIONAL_EXPRESS, //Altaria
+        "AVE"    => HafasTravelType::NATIONAL_EXPRESS, //Alta Velocidad ES
+        "BAT"    => HafasTravelType::FERRY, //Schiff
+        "BAV"    => HafasTravelType::FERRY, //Dampfschiff
+        "BUS"    => HafasTravelType::BUS,
+        "CNL"    => HafasTravelType::NATIONAL, // DB City Night Line, discontinued
+        "D"      => HafasTravelType::NATIONAL_EXPRESS, //Schnellzug
+        "E"      => HafasTravelType::NATIONAL_EXPRESS, //Eilzug
+        "EC"     => HafasTravelType::NATIONAL, // EuroCity
+        "EM"     => HafasTravelType::NATIONAL_EXPRESS, //Euromed
+        "EN"     => HafasTravelType::NATIONAL, // EuroNight
+        "ES"     => HafasTravelType::NATIONAL_EXPRESS, //Eurostar Italia
+        "EST"    => HafasTravelType::NATIONAL_EXPRESS, //Eurostar
+        "EXT"    => HafasTravelType::NATIONAL, //Extrazug
+        "FAE"    => HafasTravelType::FERRY, //Fähre
+        "FixBus" => HafasTravelType::NATIONAL,
+        "FUN"    => HafasTravelType::REGIONAL, //Standseilbahn
+        "GEX"    => HafasTravelType::REGIONAL_EXP, //Glacier Express
+        "IC"     => HafasTravelType::NATIONAL_EXPRESS,
+        "ICB"    => HafasTravelType::NATIONAL, //InterCity-Bus
+        "ICE"    => HafasTravelType::NATIONAL_EXPRESS,
+        "ICN"    => HafasTravelType::NATIONAL, //InterCity Neigezug
+        "IN"     => HafasTravelType::NATIONAL, //InterCityNacht
+        "IR"     => HafasTravelType::REGIONAL, //InterRegio
+        "IRE"    => HafasTravelType::NATIONAL_EXPRESS,
+        "KAT"    => HafasTravelType::FERRY, //Katamaran
+        "M"      => HafasTravelType::TRAM, //Metro
+        "NFB"    => HafasTravelType::BUS, //Niederflurbus
+        "NFO"    => HafasTravelType::BUS, //Niederflur-Trolleybus
+        "NFT"    => HafasTravelType::TRAM, //Niedeflur-Tram
+        "NJ"     => HafasTravelType::NATIONAL,
+        "NZ"     => HafasTravelType::NATIONAL, //Nacht-Zug
+        "OIC"    => HafasTravelType::NATIONAL, //ÖBB InterCity
+        "PE"     => HafasTravelType::REGIONAL,
+        "R"      => HafasTravelType::NATIONAL,
+        "RB"     => HafasTravelType::REGIONAL,
+        "RE"     => HafasTravelType::REGIONAL_EXP,
+        "RJ"     => HafasTravelType::NATIONAL, // OeBB RailJet
+        "RJX"    => HafasTravelType::NATIONAL_EXPRESS, // OeBB Railjet Express
+        "S"      => HafasTravelType::REGIONAL,
+        "S12"    => HafasTravelType::REGIONAL,
+        "S23"    => HafasTravelType::REGIONAL,
+        "S27"    => HafasTravelType::REGIONAL,
+        "S3"     => HafasTravelType::REGIONAL,
+        "S5"     => HafasTravelType::REGIONAL,
+        "S6"     => HafasTravelType::REGIONAL,
+        "S8"     => HafasTravelType::REGIONAL,
+        "T"      => HafasTravelType::TRAM,
+        "TAL"    => HafasTravelType::REGIONAL, //Talgo
+        "TER"    => HafasTravelType::REGIONAL, //TER200
+        "TGV"    => HafasTravelType::NATIONAL_EXPRESS, // SNCF TGV
+        "THA"    => HafasTravelType::NATIONAL_EXPRESS, // Thalys
+        "TLK"    => HafasTravelType::NATIONAL, //Twoje Linie Kolejowe
+        "VAE"    => HafasTravelType::REGIONAL, //Voralpen-Express
+        "WB"     => HafasTravelType::REGIONAL, //Westbahn
+        "X2"     => HafasTravelType::NATIONAL_EXPRESS, //X2000 Neigezug
     ];
 };
