ce78ea0a31f1d0bbfc3af46f554f54f64c837169
Levin herr <github@17d.me>
2022-03-30 14:16:56 +0200

    Make buttons usable

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -24,42 +24,52 @@
                             <td class="text-center">Telefonpin</td>
                             <td>Registrierung</td>
                             <td>DSGVO-Accept</td>
+                            <td>Mail</td>
                             <td class="text-center">Twitter</td>
                             <td class="text-center">Mastodon</td>
                             <td class="text-center">Aktionen</td>
                         </tr>
                     </thead>
                     <tr>
+                        @foreach($users as $user)
                         <td class="text-center">
-                            <code>12345</code>
+                            <code>{{ $user->id }}</code>
                         </td>
                         <td>
-                            <a href="#">
-                                @Gertrud123
+                            <a href="{{ route('profile', ['username' => $user->username]) }}" target="_blank">
+                                {{ '@'.$user->username }}
                             </a>
                         </td>
-                        <td>Gertrud von Träwelling</td>
+                        <td>{{ $user->name }}</td>
                         <td class="text-center"><code>133756</code></td>
-                        <td>2013-01-01T17:35:02</td>
-                        <td>2013-01-01T17:36:02</td>
+                        <td>{{ $user->created_at }}</td>
+                        <td>{{ $user->privacy_ack_at }}</td>
+                        <td>{{ $user->email }}<br>{{ $user->email_verified_at }}</td>
                         <td class="text-center">
-                            <a href="#" class="btn btn-small btn-info">
+                            <a href="{{ $user->twitterUrl }}"
+                               class="btn btn-small btn-info {{($user->twitterUrl) ? '' : 'disabled'}}">
                                 <i class="fab fa-twitter"></i>
                             </a>
                         </td>
                         <td class="text-center">
-                            <a href="#" class="btn btn-small btn-info">
+                            <a href="{{ $user->mastodonUrl }}"
+                               class="btn btn-small btn-info {{($user->mastodonUrl) ? '' : 'disabled'}}">
                                 <i class="fab fa-mastodon"></i>
                             </a>
                         </td>
                         <td class="text-center">
-                            <a href="#" class="btn btn-small btn-success" title="Neuen Checkin erstellen">
+                            <a href="{{ route('admin.stationboard') }}?userId={{ $user->id }}"
+                               class="btn btn-small btn-success" title="Neuen Checkin erstellen">
                                 <i class="fas fa-plus-circle"></i>
                             </a>
                         </td>
                     </tr>
+                    @endforeach
                 </table>
             </div>
         </div>
+    </div>
+    <div class="row justify-content-center mt-2 mb-5">
+        {{ $users->withQueryString()->links() }}
     </div>
 @endsection
