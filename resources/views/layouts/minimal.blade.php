cf6017e2dbecc2997e7884dfcd5c73026a70ecb3
Kris <git@k118.de>
2022-04-03 16:04:12 +0200

    :sparkles: Add maintenance page (#804)
    closes #423

---
+++ resources/views/layouts/minimal.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>@yield('title') | Träwelling</title>
+        <link href="{{ asset('fonts/Nunito/Nunito.css') }}" rel="stylesheet"/>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet"/>
+        <link rel="shortcut icon" href="{{ asset('images/icons/touch-icon-vector.svg') }}"/>
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark bg-trwl">
+                <div class="container">
+                    <a class="navbar-brand" href="javascript:void(0)">
+                        Träwelling
+                    </a>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                <div class="container">
+                    @yield('content')
+                </div>
+            </main>
+        </div>
+    </body>
+</html>
