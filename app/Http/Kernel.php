c5e0767b3592b81ba6210b648f7e1862d46e2b46
Kris <git@k118.de>
2022-04-04 10:16:39 +0200

    :bug: Improve code (#809)
    * review
    
    * :bug: Improve code
    
    Co-authored-by: Levin Herr <github@17d.me>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,21 +58,21 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth'          => \App\Http\Middleware\Authenticate::class,
-        'api.log'       => ApiLogMiddleware::class,
-        'semiguest'     => SemiGuest::class,
-        'auth.basic'    => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings'      => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can'           => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest'         => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'privacy'       => \App\Http\Middleware\PrivacyInterceptionMiddleware::class,
-        'privacypolicy' => \App\Http\Middleware\PrivacyPolicyInterceptionMiddleware::class,
-        'signed'        => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'userrole'      => \App\Http\Middleware\UserRoleMiddleware::class,
-        'verified'      => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'return-json'   => JsonMiddleware::class,
+        'auth'           => \App\Http\Middleware\Authenticate::class,
+        'api.log'        => ApiLogMiddleware::class,
+        'semiguest'      => SemiGuest::class,
+        'auth.basic'     => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings'       => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'cache.headers'  => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can'            => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'          => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'privacy'        => \App\Http\Middleware\PrivacyInterceptionMiddleware::class,
+        'signed'         => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle'       => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'userrole'       => \App\Http\Middleware\UserRoleMiddleware::class,
+        'verified'       => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'return-json'    => JsonMiddleware::class,
+        'privacy-policy' => \App\Http\Middleware\PrivacyPolicyInterceptionMiddleware::class,
     ];
 
     /**
