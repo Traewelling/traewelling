eda624df956b9020c6de4afac423e94bdcb6bc9b
Kristian Stöckel <git@k118.de>
2022-05-20 12:45:09 +0200

    ...

--- resources/views/trip.blade.php
+++ resources/views/trip.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.app')
 
-@section('title'){{ $hafasTrip->linename }} -> {{$destination}}@endsection
+@section('title')
+    {{ $hafasTrip->linename }} -> {{$destination}}
+@endsection
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 col-lg-7">
+
+                <div class="alert alert-warning">
+                    {{__('warning-alternative-station', [
+                        'newStation' => $start->name,
+                        'searchedStation' => $searchedStation->name,
+                    ])}}
+                </div>
+
                 <div class="card">
                     <div class="card-header" data-linename="{{ $hafasTrip->linename }}"
                          data-startname="{{ $hafasTrip->originStation->name }}" data-start="{{ request()->start }}"
