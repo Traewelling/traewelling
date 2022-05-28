24ab7b28353131e46f33926ebc4829432ad71ed5
Kris <git@k118.de>
2022-05-28 14:17:51 +0200

    :bug: Fix migrations (#900)

--- database/migrations/2020_09_28_170002_add_foreign_keys_to_events.php
+++ database/migrations/2020_09_28_170002_add_foreign_keys_to_events.php
@@ -6,29 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 class AddForeignKeysToEvents extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up() {
-        Schema::table('events', function(Blueprint $table) {
-            $table->bigInteger('trainstation')->unsigned()->change();
 
-            $table->foreign('trainstation')
-                  ->references('id')
-                  ->on('train_stations')
-                  ->onDelete('restrict')
-                  ->onUpdate('cascade');
-        });
+    public function up(): void {
+        //Migration removed in v1.14 due to compatibility issues with SQLite testing.
+        //Please upgrade to v1.13.2 first, run `php artisan migrate` and THEN you can upgrade to v1.14.
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down() {
+    public function down(): void {
         Schema::table('events', function(Blueprint $table) {
             $table->dropForeign("events_trainstation_foreign");
         });
