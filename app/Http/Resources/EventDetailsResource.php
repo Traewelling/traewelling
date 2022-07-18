b9d15e275558a63fdb18f3b746846882108079d0
Levin Herr <github@17d.me>
2022-07-18 14:26:11 +0200

    ⚡ Improve access times for every status with an event (#971)
    * improve access times for every status with an event
    
    * remove unused fields

---
+++ app/Http/Resources/EventDetailsResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EventDetailsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function toArray($request): array {
+        return [
+            "id"            => $this->id,
+            "slug"          => $this->slug,
+            "trainDistance" => $this->trainDistance,
+            "trainDuration" => $this->trainDuration,
+        ];
+    }
+}
