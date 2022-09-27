a5d4f60e85d33108d5c42dfa649775066dd7085e
Jannik <jannik@outlook.com>
2022-09-27 13:08:59 +0200

    :art: Apply suggestions from code review and autoformat

--- config/queue-monitor.php
+++ config/queue-monitor.php
@@ -4,8 +4,8 @@ return [
     /*
      * Set the table to be used for monitoring data.
      */
-    'table' => 'queue_monitor',
-    'connection' => null,
+    'table'                           => 'queue_monitor',
+    'connection'                      => null,
 
     /*
      * Set the model used for monitoring.
@@ -13,22 +13,22 @@ return [
      *   romanzipp\QueueMonitor\Models\Contracts\MonitorContract
      * interface or extend the base model.
      */
-    'model' => \romanzipp\QueueMonitor\Models\Monitor::class,
+    'model'                           => \romanzipp\QueueMonitor\Models\Monitor::class,
 
     /*
      * Specify the max character length to use for storing exception backtraces.
      */
-    'db_max_length_exception' => 4294967295,
+    'db_max_length_exception'         => 4294967295,
     'db_max_length_exception_message' => 65535,
 
     /*
      * The optional UI settings.
      */
-    'ui' => [
+    'ui'                              => [
         /*
          * Set the monitored jobs count to be displayed per page.
          */
-        'per_page' => 35,
+        'per_page'         => 35,
 
         /*
          *  Show custom data stored on model
@@ -38,14 +38,14 @@ return [
         /**
          * Allow the deletion of single monitor items.
          */
-        'allow_deletion' => true,
+        'allow_deletion'   => true,
 
         /**
          * Allow purging all monitor entries.
          */
-        'allow_purge' => true,
+        'allow_purge'      => true,
 
-        'show_metrics' => true,
+        'show_metrics'       => true,
 
         /**
          * Time frame used to calculate metrics values (in days).
