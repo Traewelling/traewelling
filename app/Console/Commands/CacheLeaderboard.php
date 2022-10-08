24c9d8b189aefc77921be348121f3e51265f9df6
Jannik <jannik@outlook.com>
2022-10-09 00:25:20 +0200

    :zap: Evaluate leaderboard cache every five minutes (#1088)
    This removes the burden of filling the leaderboard cache from the first user that visits the page every five minutes.

---
+++ app/Console/Commands/CacheLeaderboard.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enum\CacheKey;
+use App\Http\Controllers\Frontend\LeaderboardController;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+
+class CacheLeaderboard extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'trwl:cache:leaderboard';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a cache of the Leaderboard so the requests';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(LeaderboardController $leaderboardController): int {
+        DB::beginTransaction();
+        Cache::forget(CacheKey::LeaderboardGlobalPoints);
+        Cache::forget(CacheKey::LeaderboardGlobalDistance);
+        $leaderboardController->renderLeaderboard();
+        DB::commit();
+
+        return Command::SUCCESS;
+    }
+}
