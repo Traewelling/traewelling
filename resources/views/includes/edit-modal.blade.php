564a01a61bfb25980e3706cd98b9a41435f4714e
Kristian Stöckel <git@k118.de>
2022-10-17 18:16:21 +0200

    :bug: Fix modal close button

--- resources/views/includes/edit-modal.blade.php
+++ resources/views/includes/edit-modal.blade.php
@@ -8,7 +8,7 @@
 
                 <div class="modal-header">
                     <h4 class="modal-title">{{__('modals.editStatus-title')}}</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div class="destination-wrapper">
