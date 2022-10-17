0378602269de612dcd07b7bbfa7e04a5ca49ee88
Kristian Stöckel <git@k118.de>
2022-10-17 18:17:31 +0200

    :bug: Fix modal close button

--- resources/views/includes/delete-modal.blade.php
+++ resources/views/includes/delete-modal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">{{__('modals.deleteStatus-title')}}</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-light" data-mdb-dismiss="modal">{{__('menu.abort')}}</button>
