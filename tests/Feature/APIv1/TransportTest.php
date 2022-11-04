6819853ff2ce43bc5a139a6c9bceca3460738e23
Kris <git@k118.de>
2022-11-04 22:01:45 +0100

    :white_check_mark: Add tests (#1162)

--- tests/Feature/APIv1/TransportTest.php
+++ tests/Feature/APIv1/TransportTest.php
@@ -174,4 +174,42 @@ class TransportTest extends ApiTestCase
         );
         $response->assertNotFound();
     }
+
+    public function testSetHome(): void {
+        $user      = User::factory()->create();
+        $userToken = $user->createToken('token')->accessToken;
+
+        $this->assertNull($user->home);
+
+        $response = $this->put(
+            uri:     '/api/v1/trains/station/Hannover Hbf/home',
+            headers: ['Authorization' => 'Bearer ' . $userToken]
+        );
+        $response->assertOk();
+        $user->refresh();
+        $this->assertEquals('Hannover Hbf', $user->home?->name);
+    }
+
+    public function testAutocompleteWithDs100(): void {
+        $user      = User::factory()->create();
+        $userToken = $user->createToken('token')->accessToken;
+
+        $response = $this->get(
+            uri:     '/api/v1/trains/station/autocomplete/HH',
+            headers: ['Authorization' => 'Bearer ' . $userToken]
+        );
+        $response->assertOk();
+        $response->assertJsonStructure([
+                                           'data' => [
+                                               '*' => [
+                                                   'ibnr',
+                                                   'rilIdentifier',
+                                                   'name',
+                                               ]
+                                           ]
+                                       ]);
+        $this->assertCount(1, $response->json('data'));
+        $this->assertEquals('HH', $response->json('data.0.rilIdentifier'));
+        $this->assertEquals('Hannover Hbf', $response->json('data.0.name'));
+    }
 }
