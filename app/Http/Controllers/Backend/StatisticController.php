5371d896f642a7212c069fa0113f257d346ec5da
Kris <git@k118.de>
2022-11-12 19:45:17 +0100

    :sparkles: Show daily journeys on map and implement station map on stats (#1176)

--- app/Http/Controllers/Backend/StatisticController.php
+++ app/Http/Controllers/Backend/StatisticController.php
@@ -215,31 +215,21 @@ abstract class StatisticController extends Controller
         return TrainStation::whereIn('ibnr', $usedStationIds)->get();
     }
 
-    public static function getPassedStations(User $user, Carbon $from, Carbon $to): Collection {
-        $checkIns = DB::table('train_checkins')
-                      ->where('user_id', '=', $user->id)
-                      ->where('departure', '>=', $from->toIso8601String())
-                      ->where('departure', '<=', $to->toIso8601String())
-                      ->select(['trip_id', 'departure', 'arrival'])
-                      ->get();
-
-        $stopoverQ = DB::table('train_stopovers')->select('train_station_id');
-        foreach ($checkIns as $checkIn) {
-            $stopoverQ->orWhere(function($q) use ($checkIn) {
-                $q->where('trip_id', '=', $checkIn->trip_id)
-                  ->where('departure_planned', '>', $checkIn->departure)
-                  ->where('departure_planned', '<', $checkIn->arrival);
-            });
-            $stopoverQ->orWhere(function($q) use ($checkIn) {
-                $q->where('trip_id', '=', $checkIn->trip_id)
-                  ->where('arrival_planned', '>', $checkIn->departure)
-                  ->where('arrival_planned', '<', $checkIn->arrival);
-            });
+    public static function getPassedStations(User $user, Carbon $from = null, Carbon $to = null): Collection {
+        $query = DB::table('train_checkins')
+                   ->join('train_stopovers', 'train_checkins.trip_id', '=', 'train_stopovers.trip_id')
+                   ->where('user_id', '=', $user->id)
+                   ->whereRaw('train_checkins.departure <= train_stopovers.departure_planned')
+                   ->whereRaw('train_checkins.arrival >= train_stopovers.arrival_planned')
+                   ->groupBy('train_stopovers.train_station_id')
+                   ->select('train_stopovers.train_station_id');
+        if ($from !== null) {
+            $query->where('train_checkins.departure', '>=', $from->toIso8601String());
+        }
+        if ($to !== null) {
+            $query->where('train_checkins.departure', '<=', $to->toIso8601String());
         }
 
-        $passedStationIds = $stopoverQ->pluck('train_station_id')
-                                      ->unique();
-
-        return TrainStation::whereIn('id', $passedStationIds)->get();
+        return TrainStation::whereIn('id', $query)->get();
     }
 }
