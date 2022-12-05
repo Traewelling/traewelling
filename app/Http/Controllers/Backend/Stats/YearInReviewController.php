7e9d46c8dd82417de5f3a4252fee68b3319fb347
Kristian Stöckel <git@k118.de>
2022-12-05 22:34:07 +0100

    :sparkles: Add Träwelling year in review
    fixes #1060

--- app/Http/Controllers/Backend/Stats/YearInReviewController.php
+++ app/Http/Controllers/Backend/Stats/YearInReviewController.php
@@ -45,7 +45,6 @@ abstract class YearInReviewController extends Controller
         $mostDelayedArrivals        = TransportStatsController::getTripsByArrivalDelay($user, $from, $to, 'desc', 1);
         $topDestinations            = TransportStatsController::getTopDestinations($user, $from, $to, 5);
         $lonelyStations             = TransportStatsController::getLonelyStations($user, $from, $to);
-        $topTravelledWith           = TransportStatsController::getTopTravellingWith($user, $from, $to);
 
         return [
             'year'                => $year,
