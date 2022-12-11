3201e43ef7db69933adac4033773814cb896f1f1
Kristian Stöckel <git@k118.de>
2022-12-11 22:33:08 +0100

    meh^2.

--- database/migrations/2022_12_11_000003_move_stopovers_at_hafas_trips.php
+++ database/migrations/2022_12_11_000003_move_stopovers_at_hafas_trips.php
@@ -13,29 +13,13 @@ return new class extends Migration
 {
     public function up(): void {
         Schema::table('hafas_trips', function(Blueprint $table) {
-            // Renaming is not possible as out tests use SQLite which doesn't support this.. So.. recreate...
-            // $table->renameColumn('stopovers', 'stopovers_legacy');
-
-            $table->json('stopovers_legacy')
-                  ->comment('Unused! Needs to be migrated to table train_stopovers and deleted afterwards.');
-        });
-
-        DB::table('hafas_trips')->update(['stopovers_legacy' => DB::raw('stopovers')]);
-
-        Schema::table('hafas_trips', function(Blueprint $table) {
-            $table->dropColumn('stopovers');
+            $table->renameColumn('stopovers', 'stopovers_legacy');
         });
     }
 
     public function down(): void {
         Schema::table('hafas_trips', function(Blueprint $table) {
-            $table->json('stopovers')->after('destination');
-        });
-
-        DB::table('hafas_trips')->update(['stopovers' => DB::raw('stopovers_legacy')]);
-
-        Schema::table('hafas_trips', function(Blueprint $table) {
-            $table->dropColumn('stopovers_legacy');
+            $table->renameColumn('stopovers_legacy', 'stopovers');
         });
     }
 };
