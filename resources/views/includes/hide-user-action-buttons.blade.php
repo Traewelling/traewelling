db7fce90b3b18b40fa282cfff0ce40b3305b2199
Kristian Stöckel <git@k118.de>
2022-12-29 18:17:09 +0100

    fix icons

--- resources/views/includes/hide-user-action-buttons.blade.php
+++ resources/views/includes/hide-user-action-buttons.blade.php
@@ -24,7 +24,7 @@
             <input type="hidden" name="user_id" value="{{$user->id}}"/>
             <button type="submit" class="btn btn-sm btn-primary" data-mdb-toggle="tooltip"
                     title="{{ __('user.unblock-tooltip') }}">
-                <i class="far fa-ban"></i> <i class="far fa-unlock"></i>
+                <i class="fa-solid fa-ban"></i> <i class="fa-solid fa-unlock"></i>
             </button>
         </form>
     @else
@@ -33,7 +33,7 @@
             <input type="hidden" name="user_id" value="{{$user->id}}"/>
             <button type="submit" class="btn btn-sm btn-primary" data-mdb-toggle="tooltip"
                     title="{{ __('user.block-tooltip') }}">
-                <i class="far fa-ban"></i>
+                <i class="fa-solid fa-ban"></i>
             </button>
         </form>
     @endif
