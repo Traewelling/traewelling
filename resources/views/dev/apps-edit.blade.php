f34950e4a4904df74fac878cd5545180f3b45e29
Marie <me@nycode.dev>
2023-01-05 15:24:00 +0100

    :technologist: Support for OAuth with PKCE (#1270)
    * Implement UI for non-confidential OAuth clients
    
    * Allow CORS access to oauth/token for access from PKCE clients
    
    Co-authored-by: Levin Herr <github@17d.me>

--- resources/views/dev/apps-edit.blade.php
+++ resources/views/dev/apps-edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="row">
         <form enctype="multipart/form-data" method="POST"
-            @if(Route::currentRouteName() === 'dev.apps.create')
+              @if(Route::currentRouteName() === 'dev.apps.create')
                   action="{{ route('dev.apps.create.post') }}">
             @else
                 action="{{ route('dev.apps.edit', ['appId' => $app->id]) }}">
@@ -15,10 +15,12 @@
                             <td>Client Id</td>
                             <td><code>{{ $app->id }}</code></td>
                         </tr>
-                        <tr>
-                            <td>Client Secret</td>
-                            <td><code>{{ $app->secret }}</code></td>
-                        </tr>
+                        @if($app->confidential())
+                            <tr>
+                                <td>Client Secret</td>
+                                <td><code>{{ $app->secret }}</code></td>
+                            </tr>
+                        @endif
                     </table>
                     <hr>
                 </div>
@@ -40,7 +42,48 @@
                     Redirect URL
                 </label>
                 <div class="col-md-6">
-                    <input id="redirect" type="text" class="form-control" name="redirect" placeholder="https://example.com/callback" required value="{{ $app ? $app->redirect : ''}}">
+                    <input id="redirect" type="text" class="form-control" name="redirect"
+                           placeholder="https://example.com/callback" required value="{{ $app ? $app->redirect : ''}}">
+                </div>
+            </div>
+
+            <div class="form-group row my-1">
+                <label for="confidential" class="col-md-4 col-form-label text-md-right">
+                    Confidential
+                </label>
+                <div class="col-md-6">
+                    <input id="confidential" type="checkbox" name="confidential"
+                           @if(!$app || $app->confidential())
+                               checked
+                           @endif
+                           @if(Route::currentRouteName() === 'dev.apps.edit')
+                               disabled
+                        @endif
+                    >
+                </div>
+                <div class="col-md-12">
+                    <div class="alert alert-warning">
+                        @if(Route::currentRouteName() === "dev.apps.create")
+
+                            <p>Make sure to check if you need a confidential client or a public client. This setting
+                                cannot be changed later, because of a technical limitation.</p>
+                            <p>This setting depends on which <a href="https://oauth.net/2/grant-types/">OAuth Grant
+                                    Type</a> you want to use.</p>
+                            <p>For a regular <a href="https://oauth.net/2/grant-types/authorization-code/">Authorization
+                                    Code Flow</a>, Confidential needs to be set to true. Use this if you can keep the
+                                secret secure. (e.g. Server Side Apps)</p>
+                            <p>If you want to use a <a href="https://oauth.net/2/pkce/">Authorization Code with Proof
+                                    Key for Code Exchange Flow</a>, Confidential needs to be set to false. Use this if
+                                you can't keep the secret secure. (e.g. SPAs, Mobile Apps)</p>
+                            <p>TLDR; Check Confidential if you need a Client Secret.</p>
+
+                        @else
+                            <p>Note: You don't have a client secret because your using a non-confidential client using
+                                the <a
+                                    href="https://oauth.net/2/pkce/">Authorization Code flow with Proof Key for Code
+                                    Exchange</a></p>
+                        @endif
+                    </div>
                 </div>
             </div>
 
