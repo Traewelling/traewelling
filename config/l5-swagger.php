b71c962b086eb3567d1408961604141fc5e8def1
Levin Herr <github@17d.me>
2023-01-27 21:10:26 +0100

    Update l5-swagger.php

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -189,7 +189,7 @@ return [
                                      ],
                                  ],
                 ],
-                'token_old' => [ // Unique name of security
+                'token' => [ // Unique name of security
                                  'type'        => 'apiKey', // Valid values are "basic", "apiKey" or "oauth2".
                                  'description' => 'Enter token in format "Bearer \<token\>"',
                                  'name'        => 'Authorization', // The name of the header or query parameter to be used.
