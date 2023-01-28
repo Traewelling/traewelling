30cc333445bef6d8165144dbe7ad3b072f23201b
Kristian Stöckel <git@k118.de>
2023-01-28 20:47:58 +0100

    no message

--- app/Notifications/BaseNotification.php
+++ app/Notifications/BaseNotification.php
@@ -7,4 +7,13 @@ use Illuminate\Notifications\Notification;
 abstract class BaseNotification extends Notification
 {
     abstract public static function render(mixed $notification): ?string;
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array
+     */
+    public function via(): array {
+        return ['database'];
+    }
 }
