809539ebb7036d60ec3233c47d90d645036a817e
Levin <git@17d.me>
2023-01-29 10:36:54 +0100

    add more scopes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -33,22 +33,27 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::tokensCan([
-                                'read'                => 'see most of your accounts data except settings',
-                                'read-statuses'       => 'see all statuses',
-                                'read-notifications'  => 'see your notifications',
-                                'read-statistics'     => 'see your statistics',
-                                'write'               => 'write statuses, follow users, etc',
-                                'write-statuses'      => 'create, edit, delete statuses',
-                                'write-notifications' => 'mark notifications as read, clear notifications',
-                                'write-exports'       => 'request data exports',
-                                'write-follows'       => 'follow and unfollow users',
-                                'write-blocks'        => 'block and unblock users, mute and unmute users',
-                                'settings-read'       => 'see your settings, email, etc.',
-                                'settings-'
-                                'settings-write-profile' => 'edit your profile',
-                                'settings-read-profile'  => 'see your profile data, e.g. email',
-                                'settings-write-mail'    => 'edit your email',
-                                'settings-'
+                                'read'                           => 'see most of your accounts data except settings',
+                                'read-statuses'                  => 'see all statuses',
+                                'read-notifications'             => 'see your notifications',
+                                'read-statistics'                => 'see your statistics',
+                                'write'                          => 'write statuses, follow users, etc',
+                                'write-statuses'                 => 'create, edit, delete statuses',
+                                'write-notifications'            => 'mark notifications as read, clear notifications',
+                                'write-exports'                  => 'request data exports',
+                                'write-follows'                  => 'follow and unfollow users',
+                                'write-blocks'                   => 'block and unblock users, mute and unmute users',
+                                'settings-read'                  => 'see your settings, email, etc.',
+                                'settings-write'                 => 'write all settings related options',
+                                'settings-write-profile'         => 'edit your profile',
+                                'settings-read-profile'          => 'see your profile data, e.g. email',
+                                'settings-write-mail'            => 'edit your email',
+                                'settings-write-profile-picture' => 'edit your profile picture',
+                                'settings-write-privacy'         => 'change your privacy settings',
+                                'settings-write-calendar'        => 'create and delete new calendar-tokens',
+                                'extra-write-password'           => 'change your password',
+                                'extra-terminate-sessions'       => 'log you out of other sessions and apps',
+                                'extra-delete'                   => 'delete your account'
                             ]);
     }
 }
