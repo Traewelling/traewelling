51b5aacb7441c80707f1a023e1cfc11b433e1e52
Levin Herr <github@17d.me>
2023-01-29 09:32:46 +0100

    prepare scopes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,24 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void {
         $this->registerPolicies();
+
+        Passport::tokensCan([
+                                'read'                => 'see most of your accounts data except settings',
+                                'read-statuses'       => 'see all statuses',
+                                'read-notifications'  => 'see your notifications',
+                                'read-statistics'     => 'see your statistics',
+                                'write'               => 'write statuses, follow users, etc',
+                                'write-statuses'      => 'create, edit, delete statuses',
+                                'write-notifications' => 'mark notifications as read, clear notifications',
+                                'write-exports'       => 'request data exports',
+                                'write-follows'       => 'follow and unfollow users',
+                                'write-blocks'        => 'block and unblock users, mute and unmute users',
+                                'settings-read'       => 'see your settings, email, etc.',
+                                'settings-'
+                                'settings-write-profile' => 'edit your profile',
+                                'settings-read-profile'  => 'see your profile data, e.g. email',
+                                'settings-write-mail'    => 'edit your email',
+                                'settings-'
+                            ]);
     }
 }
