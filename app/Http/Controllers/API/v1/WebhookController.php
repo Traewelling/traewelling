3c47f9d7ff4605d4a8fbf5cbcef437c82c12e14f
Marie Ramlow <me@nycode.dev>
2023-02-05 19:19:53 +0100

    Document webhook api endpoints

--- app/Http/Controllers/API/v1/WebhookController.php
+++ app/Http/Controllers/API/v1/WebhookController.php
@@ -14,16 +14,75 @@ use Illuminate\Http\Response;
 
 class WebhookController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/webhooks",
+     *     operationId="getWebhooks",
+     *     tags={"Webhooks"},
+     *     summary="Get webhooks for current user and current application",
+     *     description="Returns all webhooks which are created for the current user and which the current authorized applicaton has access to.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="array",
+     *                 @OA\Items(
+     *                     ref="#/components/schemas/Webhook"
+     *                 )
+     *             ),
+     *         )
+     *     ),
+     *     @OA\Response(response=401, description="Unauthorized"),
+     *     security={
+     *         {"passport": {}}, {"token": {}}
+     *     }
+     * )
+     */
     public function getWebhooks(Request $request): AnonymousResourceCollection
     {
         $clientId = $request->user()->token()->client->id;
         $webhooks = Webhook::where('oauth_client_id', '=', $clientId)
             ->where('user_id', '=', $request->user()->id)
-            ->latest()
-            ->simplePaginate(15);
+            ->get();
         return WebhookResource::collection($webhooks);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/webhooks/{id}",
+     *      operationId="getSingleWebhook",
+     *      tags={"Webhooks"},
+     *      summary="Get single webhook",
+     *      description="Returns a single webhook Object, if user and application is authorized to see it",
+     *      @OA\Parameter (
+     *          name="id",
+     *          in="path",
+     *          description="Webhook-ID",
+     *          example=1337,
+     *          @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="data",
+     *                      ref="#/components/schemas/Webhook"
+     *              ),
+     *          )
+     *       ),
+     *       @OA\Response(response=400, description="Bad request"),
+     *       @OA\Response(response=404, description="No webhook found or unauthorized for this id"),
+     *       security={
+     *           {"passport": {}}, {"token": {}}
+     *       }
+     *     )
+     *
+     * Show single webhook
+     *
+     * @param int $webhookId
+     *
+     * @return WebhookResource|JsonResponse
+     */
     public function getWebhook(Request $request, int $webhookId): WebhookResource|JsonResponse
     {
         $clientId = $request->user()->token()->client->id;
@@ -37,6 +96,39 @@ class WebhookController extends Controller
         return new WebhookResource($webhook);
     }
 
+    /**
+     * @OA\Delete(
+     *      path="/webhooks/{id}",
+     *      operationId="deleteWebhook",
+     *      tags={"Webhooks"},
+     *      summary="Delete a webhook if the user and application are authorized to do",
+     *      description="",
+     *      @OA\Parameter (
+     *          name="id",
+     *          in="path",
+     *          description="Status-ID",
+     *          example=1337,
+     *          @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\JsonContent(
+     *                      ref="#/components/schemas/SuccessResponse"
+     *          )
+     *       ),
+     *       @OA\Response(response=400, description="Bad request"),
+     *       @OA\Response(response=404, description="No webhook found for this id"),
+     *       @OA\Response(response=403, description="User or application not authorized to delete this webhook"),
+     *       security={
+     *           {"passport": {}}, {"token": {}}
+     *       }
+     *     )
+     *
+     * @param int $webhookId
+     *
+     * @return JsonResponse
+     */
     public function deleteWebhook(Request $request, int $webhookId): JsonResponse
     {
         try {
---
+++ app/Virtual/Models/Webhook.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Virtual\Models;
+
+/**
+ * @OA\Schema(
+ *     title="Webhook",
+ *     description="Webhook model",
+ *     @OA\Xml(
+ *         name="Webhook"
+ *     )
+ * )
+ */
+class Webhook
+{
+    /**
+     * @OA\Property(
+     *     title="ID",
+     *     description="ID",
+     *     format="int64",
+     *     example=12345
+     * )
+     *
+     * @var integer
+     **/
+    private $id;
+
+
+    /**
+     * @OA\Property(
+     *     title="ClientID",
+     *     description="ID of the client which created this webhook",
+     *     format="int64",
+     *     example=12345
+     * )
+     *
+     * @var integer
+     **/
+    private $clientId;
+
+
+    /**
+     * @OA\Property(
+     *     title="UserID",
+     *     description="ID of the user which created this webhook",
+     *     format="int64",
+     *     example=12345
+     * )
+     *
+     * @var integer
+     **/
+    private $userId;
+
+
+    /**
+     * @OA\Property(
+     *     title="url",
+     *     description="URL where the webhook gets sent to",
+     *     example="https://example.com/webhook"
+     * )
+     *
+     * @var string;
+     */
+    private $url;
+
+
+    /**
+     * @OA\Property(
+     *     title="createdAt",
+     *     description="creation date of this webhook",
+     *     type="string",
+     *     format="datetime",
+     *     example="2022-07-17T13:37:00+02:00"
+     * )
+     *
+     * @var DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @OA\Property(
+     *     title="events",
+     *     description="array of events this webhook receives",
+     *     type="array",
+     *     @OA\Items()
+     * )
+     *
+     */
+    private $events;
+}
