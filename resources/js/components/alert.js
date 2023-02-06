39ec6c58b42fb33823a0787d457ca3244b8c1e20
Fabian K <vainamov@users.noreply.github.com>
2023-02-06 20:50:16 +0100

    :children_crossing: Restore dismissing for alerts (#1372)

--- resources/js/components/alert.js
+++ resources/js/components/alert.js
@@ -1,17 +1,17 @@
 bootstrap_alert = function() {}
 bootstrap_alert.warning = function(message) {
     window.scrollTo(0, 0);
-    $('#alert_placeholder').append('<div class="alert alert-warning alert-block"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+    $('#alert_placeholder').append('<div class="alert alert-dismissible alert-warning alert-block"><a class="close" data-mdb-dismiss="alert">×</a><span>'+message+'</span></div>')
 }
 bootstrap_alert.danger = function(message) {
     window.scrollTo(0, 0);
-    $('#alert_placeholder').append('<div class="alert alert-danger alert-block"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+    $('#alert_placeholder').append('<div class="alert alert-dismissible alert-danger alert-block"><a class="close" data-mdb-dismiss="alert">×</a><span>'+message+'</span></div>')
 }
 bootstrap_alert.success = function(message) {
     window.scrollTo(0, 0);
-    $('#alert_placeholder').append('<div class="alert alert-success alert-block"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+    $('#alert_placeholder').append('<div class="alert alert-dismissible alert-success alert-block"><a class="close" data-mdb-dismiss="alert">×</a><span>'+message+'</span></div>')
 }
 bootstrap_alert.info = function(message) {
     window.scrollTo(0, 0);
-    $('#alert_placeholder').append('<div class="alert alert-info alert-block"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+    $('#alert_placeholder').append('<div class="alert alert-dismissible alert-info alert-block"><a class="close" data-mdb-dismiss="alert">×</a><span>'+message+'</span></div>')
 }
