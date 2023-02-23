b4f03c9fe4e320edddd67e06f78cabda77ae0bbe
Levin Herr <github@17d.me>
2023-02-23 20:38:14 +0100

    do not give access to all scopes for existing tokens

--- database/migrations/2023_01_30_173822_add_scopes_to_existing_oauth_access_tokens.php
+++ database/migrations/2023_01_30_173822_add_scopes_to_existing_oauth_access_tokens.php
@@ -10,7 +10,15 @@ return new class extends Migration
 {
     public function up(): void {
         Schema::table('oauth_access_tokens', function(Blueprint $table) {
+            $scopes = AuthServiceProvider::$scopes;
+            unset($scopes['extra-write-password'], $scopes['extra-terminate-sessions'], $scopes['extra-delete']);
             DB::table('oauth_access_tokens')->update(['scopes' => '["' . implode('","', array_keys(AuthServiceProvider::$scopes)) . '"]']);
+        });
+    }
+
+    public function down(): void {
+        Schema::table('oauth_access_tokens', function(Blueprint $table) {
+            DB::table('oauth_access_tokens')->update(['scopes' => '[]']);
         });
     }
 };
