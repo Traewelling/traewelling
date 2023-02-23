14fa5f0e8137899254b0abe75066dbc32cebe95a
Levin Herr <github@17d.me>
2023-02-23 22:11:51 +0100

    💄 Replace bootstrap alerts through notyf (#1419)
    * Replace bootstrap alerts through notyf
    
    * Cannot test js generated alerts

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,29 @@ require("leaflet/dist/leaflet.js");
 document.addEventListener("DOMContentLoaded", function () {
     window.notyf = new Notyf({
         duration: 5000,
-        position: {x: "right", y: "top"}
+        position: {x: "right", y: "top"},
+        dismissible: true,
+        ripple: true,
+        types: [
+            {
+                type: 'info',
+                background: '#0dcaf0',
+                icon: {
+                    className: 'fa-solid fa-circle-info',
+                    color: 'white',
+                    tagName: 'i',
+                }
+            },
+            {
+                type: 'warning',
+                background: '#ffc107',
+                icon: {
+                    className: 'fa-solid fa-triangle-exclamation',
+                    tagName: 'i',
+                    color: 'white',
+                }
+            },
+        ]
     });
 });
 
