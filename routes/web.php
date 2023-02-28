842c44017ec3a71e57904d49e08b9565b1cd740a
Marie Ramlow <me@nycode.dev>
2023-02-28 15:28:30 +0100

    Fix formatting in web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-use App\Http\Controllers\Backend\Auth\AuthorizationController;
+use App\Http\Controllers\Frontend\WebhookController;
 use App\Http\Controllers\Frontend\AccountController;
 use App\Http\Controllers\Frontend\DevController;
 use App\Http\Controllers\Frontend\EventController;
@@ -28,7 +28,6 @@ use App\Http\Controllers\Frontend\Stats\YearInReviewController;
 use App\Http\Controllers\Frontend\Support\SupportController;
 use App\Http\Controllers\Frontend\Transport\StatusController;
 use App\Http\Controllers\Frontend\User\ProfilePictureController;
-use App\Http\Controllers\Frontend\WebhookController;
 use App\Http\Controllers\FrontendStaticController;
 use App\Http\Controllers\FrontendStatusController;
 use App\Http\Controllers\FrontendTransportController;
@@ -39,51 +38,44 @@ use App\Http\Controllers\SitemapController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Laravel\Passport\Http\Controllers\AccessTokenController;
-use Laravel\Passport\Http\Controllers\ApproveAuthorizationController;
-use Laravel\Passport\Http\Controllers\AuthorizedAccessTokenController;
-use Laravel\Passport\Http\Controllers\ClientController;
-use Laravel\Passport\Http\Controllers\DenyAuthorizationController;
-use Laravel\Passport\Http\Controllers\PersonalAccessTokenController;
-use Laravel\Passport\Http\Controllers\ScopeController;
 
 Route::prefix('admin')->group(base_path('routes/web/admin.php'));
 
 Route::get('/@{username}/picture', [ProfilePictureController::class, 'generateProfilePicture'])
-    ->name('profile.picture');
+     ->name('profile.picture');
 
 Route::get('/', [FrontendStaticController::class, 'renderLandingPage'])
-    ->name('static.welcome');
+     ->name('static.welcome');
 
 Route::view('/about', 'about')->name('static.about');
 
 Route::permanentRedirect('/imprint', '/legal/');
 Route::permanentRedirect('/privacy', '/legal/privacy-policy');
-Route::prefix('legal')->group(function () {
+Route::prefix('legal')->group(function() {
     Route::view('/', 'legal.notice')
-        ->name('legal.notice');
+         ->name('legal.notice');
     Route::get('/privacy-policy', [PrivacyAgreementController::class, 'intercept'])
-        ->name('legal.privacy');
+         ->name('legal.privacy');
 });
 
 Route::redirect('/profile/{username}', '/@{username}');
 Route::get('/@{username}', [FrontendUserController::class, 'getProfilePage'])
-    ->name('profile');
+     ->name('profile');
 
 Route::get('/leaderboard', [LeaderboardController::class, 'renderLeaderboard'])
-    ->name('leaderboard');
+     ->name('leaderboard');
 
 Route::get('/leaderboard/{date}', [LeaderboardController::class, 'renderMonthlyLeaderboard'])
-    ->name('leaderboard.month');
+     ->name('leaderboard.month');
 
 Route::get('/statuses/active', [FrontendStatusController::class, 'getActiveStatuses'])
-    ->name('statuses.active');
+     ->name('statuses.active');
 
 Route::get('/statuses/event/{eventSlug}', [FrontendStatusController::class, 'statusesByEvent'])
-    ->name('statuses.byEvent');
+     ->name('statuses.byEvent');
 
 Route::get('/events', [EventController::class, 'renderEventOverview'])
-    ->name('events');
+     ->name('events');
 
 Auth::routes(['verify' => true]);
 
@@ -93,16 +85,16 @@ Route::get('/callback/twitter', [TwitterController::class, 'callback']);
 Route::get('/callback/mastodon', [MastodonController::class, 'callback']);
 
 Route::get('/status/{id}', [FrontendStatusController::class, 'getStatus'])
-    ->name('statuses.get');
+     ->name('statuses.get');
 
-Route::prefix('blog')->group(function () {
+Route::prefix('blog')->group(function() {
     Route::permanentRedirect('/', 'https://blog.traewelling.de')
-        ->name('blog.all');
+         ->name('blog.all');
 
     Route::permanentRedirect('/{slug}', 'https://blog.traewelling.de/posts/{slug}')
-        ->name('blog.show');
+         ->name('blog.show');
 
-    Route::get('/cat/{category}', function ($category) {
+    Route::get('/cat/{category}', function($category) {
         return redirect('https://blog.traewelling.de/categories/' . strtolower($category), 301);
     })->name('blog.category');
 });
@@ -110,54 +102,54 @@ Route::prefix('blog')->group(function () {
 /**
  * These routes can be used by logged in users although they have not signed the privacy policy yet.
  */
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function() {
 
     Route::get('/gdpr-intercept', [PrivacyAgreementController::class, 'intercept'])
-        ->name('gdpr.intercept');
+         ->name('gdpr.intercept');
 
     Route::post('/gdpr-ack', [PrivacyAgreementController::class, 'ack'])
-        ->name('gdpr.ack');
+         ->name('gdpr.ack');
 
     Route::post('/settings/destroy', [AccountController::class, 'deleteUserAccount'])
-        ->name('account.destroy');
+         ->name('account.destroy');
 });
 
 
 Route::get('/ics', [IcsController::class, 'renderIcs'])
-    ->name('ics');
+     ->name('ics');
 
 /**
  * All of these routes can only be used by fully registered users.
  */
-Route::middleware(['auth', 'privacy'])->group(function () {
+Route::middleware(['auth', 'privacy'])->group(function() {
     Route::get('year-in-review', [YearInReviewController::class, 'show']);
 
     Route::post('/ics/createToken', [IcsController::class, 'createIcsToken'])
-        ->name('ics.createToken');
+         ->name('ics.createToken');
     Route::post('/ics/revokeToken', [IcsController::class, 'revokeIcsToken'])
-        ->name('ics.revokeToken');
+         ->name('ics.revokeToken');
 
     Route::post('/destroy/provider', [SocialController::class, 'destroyProvider'])
-        ->name('provider.destroy');
+         ->name('provider.destroy');
 
-    Route::prefix('stats')->group(static function () {
+    Route::prefix('stats')->group(static function() {
         Route::get('/', [StatisticController::class, 'renderMainStats'])
-            ->name('stats');
+             ->name('stats');
         Route::get('/stations', [StatisticController::class, 'renderStations'])
-            ->name('stats.stations');
+             ->name('stats.stations');
         Route::get('/daily/{dateString}', [DailyStatsController::class, 'renderDailyStats'])
-            ->name('stats.daily');
+             ->name('stats.daily');
     });
 
     Route::get('/support', [SupportController::class, 'renderSupportPage'])->name('support');
     Route::post('/support/submit', [SupportController::class, 'submit'])->name('support.submit');
 
     Route::post('/events/suggest', [EventController::class, 'suggestEvent'])
-        ->name('events.suggest');
+         ->name('events.suggest');
 
-    Route::prefix('settings')->group(function () {
+    Route::prefix('settings')->group(function() {
 
-        Route::prefix('/applications')->group(function () {
+        Route::prefix('/applications')->group(function() {
             Route::get('/', [DevController::class, 'renderAppList'])->name('dev.apps');
             Route::get('/create', [DevController::class, 'renderCreateApp'])->name('dev.apps.create');
             Route::get('/{appId}', [DevController::class, 'renderUpdateApp'])->name('dev.apps.edit');
@@ -167,108 +159,108 @@ Route::middleware(['auth', 'privacy'])->group(function () {
         });
 
         Route::get('/', [SettingsController::class, 'renderSettings'])
-            ->name('settings');
+             ->name('settings');
         Route::post('/', [SettingsController::class, 'updateMainSettings']);
         Route::post('/update/privacy', [SettingsController::class, 'updatePrivacySettings'])
-            ->name('settings.privacy');
+             ->name('settings.privacy');
 
         Route::post('/password', [SettingsController::class, 'updatePassword'])
-            ->name('password.change');
+             ->name('password.change');
 
         Route::get('/follower', [\App\Http\Controllers\SettingsController::class, 'renderFollowerSettings'])
-            ->name('settings.follower');
+             ->name('settings.follower');
         Route::post('/follower/remove', [\App\Http\Controllers\SettingsController::class, 'removeFollower'])
-            ->name('settings.follower.remove');
+             ->name('settings.follower.remove');
         Route::post('/follower/approve', [SettingsController::class, 'approveFollower'])
-            ->name('settings.follower.approve');
+             ->name('settings.follower.approve');
         Route::post('/follower/reject', [SettingsController::class, 'rejectFollower'])
-            ->name('settings.follower.reject');
+             ->name('settings.follower.reject');
 
         Route::post('/uploadProfileImage', [FrontendUserController::class, 'updateProfilePicture'])
-            ->name('settings.upload-image');
+             ->name('settings.upload-image');
         Route::get('/deleteProfilePicture', [UserController::class, 'deleteProfilePicture'])
-            ->name('settings.delete-profile-picture');
+             ->name('settings.delete-profile-picture');
 
         Route::post('/delsession', [UserController::class, 'deleteSession'])
-            ->name('delsession');
+             ->name('delsession');
         Route::post('/deltoken', [UserController::class, 'deleteToken'])
-            ->name('deltoken');
+             ->name('deltoken');
         Route::post('/delwebhook', [WebhookController::class, 'deleteWebhook'])
-            ->name('delwebhook');
+             ->name('delwebhook');
     });
 
     Route::get('/dashboard', [FrontendStatusController::class, 'getDashboard'])
-        ->name('dashboard');
+         ->name('dashboard');
 
     Route::get('/dashboard/global', [FrontendStatusController::class, 'getGlobalDashboard'])
-        ->name('globaldashboard');
+         ->name('globaldashboard');
 
     Route::delete('/destroystatus', [FrontendStatusController::class, 'DeleteStatus'])
-        ->name('status.delete');
+         ->name('status.delete');
 
     Route::post('/status/update', [StatusController::class, 'updateStatus'])
-        ->name('status.update');
+         ->name('status.update');
 
     Route::post('/createlike', [FrontendStatusController::class, 'createLike'])
-        ->name('like.create');
+         ->name('like.create');
 
     Route::post('/destroylike', [FrontendStatusController::class, 'DestroyLike'])
-        ->name('like.destroy');
+         ->name('like.destroy');
 
-    Route::prefix('export')->group(function () {
+    Route::prefix('export')->group(function() {
         Route::get('/', [ExportController::class, 'renderForm'])
-            ->name('export.landing');
+             ->name('export.landing');
         Route::post('/generate', [ExportController::class, 'renderExport'])
-            ->name('export.generate');
+             ->name('export.generate');
     });
 
     Route::post('/createfollow', [FrontendUserController::class, 'CreateFollow'])
-        ->name('follow.create');
+         ->name('follow.create');
 
     Route::post('/requestfollow', [FrontendUserController::class, 'requestFollow'])
-        ->name('follow.request');
+         ->name('follow.request');
 
     Route::post('/destroyfollow', [FrontendUserController::class, 'destroyFollow'])
-        ->name('follow.destroy');
+         ->name('follow.destroy');
 
     Route::get('/transport/train/autocomplete/{station}', [FrontendTransportController::class, 'TrainAutocomplete'])
-        ->name('transport.train.autocomplete');
+         ->name('transport.train.autocomplete');
 
     Route::get('/trains/stationboard', [FrontendTransportController::class, 'TrainStationboard'])
-        ->name('trains.stationboard');
+         ->name('trains.stationboard');
 
     Route::get('/trains/nearby', [FrontendTransportController::class, 'StationByCoordinates'])
-        ->name('trains.nearby');
+         ->name('trains.nearby');
 
     Route::get('/trains/trip', [FrontendTransportController::class, 'TrainTrip'])
-        ->name('trains.trip');
+         ->name('trains.trip');
 
     Route::post('/trains/checkin', [FrontendTransportController::class, 'TrainCheckin'])
-        ->name('trains.checkin');
+         ->name('trains.checkin');
 
     Route::get('/trains/setHome/', [FrontendTransportController::class, 'setTrainHome'])
-        ->name('user.setHome');
+         ->name('user.setHome');
 
     Route::get('/notifications/latest', [NotificationController::class, 'renderLatest'])
-        ->name('notifications.latest');
+         ->name('notifications.latest');
 
     Route::post('/notifications/toggleReadState/{id}', [NotificationController::class, 'toggleReadState'])
-        ->name('notifications.toggleReadState');
+         ->name('notifications.toggleReadState');
 
     Route::post('/notifications/readAll', [NotificationController::class, 'readAll'])
-        ->name('notifications.readAll');
+         ->name('notifications.readAll');
 
     Route::get('/search/', [FrontendUserController::class, 'searchUser'])
-        ->name('userSearch');
+         ->name('userSearch');
 
     Route::post('/user/block', [\App\Http\Controllers\Frontend\UserController::class, 'blockUser'])
-        ->name('user.block');
+         ->name('user.block');
     Route::post('/user/unblock', [\App\Http\Controllers\Frontend\UserController::class, 'unblockUser'])
-        ->name('user.unblock');
+         ->name('user.unblock');
     Route::post('/user/mute', [\App\Http\Controllers\Frontend\UserController::class, 'muteUser'])
-        ->name('user.mute');
+         ->name('user.mute');
     Route::post('/user/unmute', [\App\Http\Controllers\Frontend\UserController::class, 'unmuteUser'])
-        ->name('user.unmute');
+         ->name('user.unmute');
 });
 
 Route::get('/sitemap.xml', [SitemapController::class, 'renderSitemap']);
