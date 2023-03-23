e71803627e4c21e0e79884f5ed1a43c63af83bc3
Kristian Stöckel <git@k118.de>
2023-03-23 20:18:43 +0100

    formatting

--- database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
+++ database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
@@ -6,9 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void
-    {
-        Schema::create('oauth_auth_codes', static function (Blueprint $table) {
+    public function up(): void {
+        Schema::create('oauth_auth_codes', static function(Blueprint $table) {
             $table->string('id', 100)->primary();
             $table->integer('user_id');
             $table->integer('client_id');
@@ -18,8 +17,7 @@ return new class extends Migration
         });
     }
 
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('oauth_auth_codes');
     }
 };
