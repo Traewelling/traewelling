335b498a5d13da9dbca6d64cefc5e1595aeebb68
Kristian Stöckel <git@k118.de>
2023-03-23 20:24:28 +0100

    no message

--- tests/Feature/Webhooks/WebhookNotificationTest.php
+++ tests/Feature/Webhooks/WebhookNotificationTest.php
@@ -7,13 +7,19 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Bus;
 use Spatie\WebhookServer\CallWebhookJob;
 use Tests\TestCase;
-
 use function PHPUnit\Framework\assertEquals;
 
-class WebhookNotificationTest extends TestCase {
+class WebhookNotificationTest extends TestCase
+{
     use RefreshDatabase;
 
-    public function testWebhookSendingOnNotification() {
+    protected function setUp(): void {
+        if (config("trwl.webhooks_active") !== "true") {
+            $this->markTestSkipped();
+        }
+    }
+
+    public function testWebhookSendingOnNotification(): void {
         Bus::fake();
 
         $alice = $this->createGDPRAckedUser();
@@ -26,7 +32,7 @@ class WebhookNotificationTest extends TestCase {
         $follow = $this->actingAs($alice)->post(route('follow.create'), ['follow_id' => $bob->id]);
         $follow->assertStatus(201);
 
-        Bus::assertDispatched(function (CallWebhookJob $job) {
+        Bus::assertDispatched(static function(CallWebhookJob $job) {
             assertEquals(WebhookEvent::NOTIFICATION->name(), $job->payload['event']);
             return true;
         });
