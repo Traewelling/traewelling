9322e9755aa736cec017e9a2ac822524a1a59336
Kristian Stöckel <git@k118.de>
2023-05-05 23:01:45 +0200

    no message

--- tests/Feature/APIv1/UserReportTest.php
+++ tests/Feature/APIv1/UserReportTest.php
@@ -2,24 +2,11 @@
 
 namespace Tests\Feature\APIv1;
 
-use App\Enum\Business;
-use App\Enum\StatusVisibility;
-use App\Http\Controllers\UserController as UserBackend;
-use App\Models\HafasTrip;
-use App\Models\Status;
-use App\Models\TrainCheckin;
-use App\Models\TrainStation;
-use App\Models\TrainStopover;
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Date;
 use Tests\ApiTestCase;
-use App\Providers\AuthServiceProvider;
-use Tests\TestCase;
 
-class UserReportTest extends TestCase
+class UserReportTest extends ApiTestCase
 {
 
     use RefreshDatabase;
