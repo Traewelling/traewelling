aefedfd0766b16c654e27159403b0858e84c3b7d
Jannik <jannik@outlook.com>
2023-05-07 14:23:16 +0200

    :card_file_box: add likes_enabled field

---
+++ database/migrations/2023_05_07_135454_add_likes_enabled_field_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('likes_enabled')
+                  ->default(true)
+                  ->after('shadow_banned')
+                  ->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("likes_enabled");
+        });
+    }
+};
