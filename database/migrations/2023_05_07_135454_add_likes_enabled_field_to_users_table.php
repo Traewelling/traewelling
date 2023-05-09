ad9cba758c85f7314dc393c796741b2ce338641f
Jannik <jannik@outlook.com>
2023-05-09 20:50:12 +0200

    👥 Apply suggestions from code review
    Co-authored-by: Kris <git@k118.de>

--- database/migrations/2023_05_07_135454_add_likes_enabled_field_to_users_table.php
+++ database/migrations/2023_05_07_135454_add_likes_enabled_field_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('likes_enabled')
@@ -26,7 +26,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn("likes_enabled");
