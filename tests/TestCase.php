cff2f0d007bc153b0aac809fa71f81f24ac489e4
Kristian Stöckel <git@k118.de>
2023-05-17 22:51:52 +0200

    :wastebasket: Cleanup old functions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,8 +7,6 @@ use App\Models\OAuthClient;
 use App\Models\User;
 use App\Models\Webhook;
 use App\Repositories\OAuthClientRepository;
-use Carbon\Carbon;
-use Exception;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -142,8 +140,7 @@ abstract class TestCase extends BaseTestCase
     }
 
     public function createWebhookClient(User $user): OAuthClient {
-        $clients = new OAuthClientRepository();
-        return $clients->create(
+        return (new OAuthClientRepository())->create(
             $user->id,
             "TRWL Webhook Testing Application",
             "https://example.com",
@@ -158,8 +155,7 @@ abstract class TestCase extends BaseTestCase
     }
 
     public function createOAuthClient(User $user, bool $confidential): OAuthClient {
-        $clients = new OAuthClientRepository();
-        return $clients->create(
+        return (new OAuthClientRepository())->create(
             $user->id,
             "TRWL OAuth Testing Application",
             "https://example.com",
@@ -180,49 +176,5 @@ abstract class TestCase extends BaseTestCase
         }
         $request = WebhookController::createWebhookRequest($user, $client, 'stub', "https://example.com", $bitflag);
         return WebhookController::createWebhook($request);
-    }
-
-    /**
-     * @var string Hafas is weird and it's trip ids are shorter the first 9 days of the month.
-     */
-    private static $HAFAS_ID_DATE = 'jmY';
-
-    /**
-     * Check if the given Hafas Trip was correct. Can be used from several test functions.
-     * Currently checking if the hafas tripId contains four pipe characters and if it contains the
-     * date of the request. If the test runs between 23:45 and midnight, the stationboard response
-     * may contain trains starting the next day. If the test runs after midnight it might contain
-     * some trains that started the day before.
-     *
-     * Trips where the first station is a day before the requestDate can be even one day more earlier.
-     * e.g. Train starts at 01.01. but out request is on the same train which departs on 02.01 at 00:01
-     * at the second station -> in the trip is is still the 01.01.
-     *
-     * @return Boolean If all checks were resolved positively. Assertions to be made on the caller
-     * side to provide a coherent amount of assertions.
-     * @throws Exception
-     */
-    public static function isCorrectHafasTrip($hafastrip, Carbon $requestDate): bool {
-        $requestDateMinusMinusOneDay = $requestDate->clone()->subDays(2)->format(self::$HAFAS_ID_DATE);
-        $requestDateMinusOneDay      = $requestDate->clone()->subDay()->format(self::$HAFAS_ID_DATE);
-        $requestDatePlusOneDay       = $requestDate->clone()->addDay()->format(self::$HAFAS_ID_DATE);
-        $requestDate                 = $requestDate->format(self::$HAFAS_ID_DATE);
-
-        // All Hafas Trips should have four pipe characters
-        $fourPipes = 4 == substr_count($hafastrip->tripId, '|');
-
-        $rightDate = in_array(1, [
-            substr_count($hafastrip->tripId, $requestDateMinusMinusOneDay),
-            substr_count($hafastrip->tripId, $requestDateMinusOneDay),
-            substr_count($hafastrip->tripId, $requestDate),
-            substr_count($hafastrip->tripId, $requestDatePlusOneDay)
-        ]);
-
-        $ret = $fourPipes && $rightDate;
-        if (!$ret) {
-            echo "The following Hafas Trip did not match our expectations:";
-            dd($hafastrip);
-        }
-        return $ret;
     }
 }
