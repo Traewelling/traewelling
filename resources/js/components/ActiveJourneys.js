022dc7685f43d6d0e4113a476c0afdb183136198
Jannik <jannik@outlook.com>
2023-06-09 18:23:31 +0200

    :sparkles: update the other maps of trwl to the new style

--- resources/js/components/ActiveJourneys.js
+++ resources/js/components/ActiveJourneys.js
@@ -6,15 +6,7 @@ window.ActiveJourneys = class ActiveJourneys {
             zoom: 5
         });
 
-        L.tileLayer(
-            "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
-            {
-                attribution:
-                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-                subdomains: "abcd",
-                maxZoom: 19
-            }
-        ).addTo(map);
+        setTilingLayer(mapprovider, map);
 
         const swapC = ([lng, lat]) => [lat, lng];
 
