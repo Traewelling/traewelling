dee6c89c1466d79289eca3e4da8947bd66b5adfc
Kris <git@k118.de>
2023-07-11 19:09:56 +0200

    :children_crossing: Cleanup leaflet copyright notices (#1725)

--- resources/js/components/maps.js
+++ resources/js/components/maps.js
@@ -5,31 +5,33 @@
 window.setTilingLayer = (mapprovider, map) => {
     switch (mapprovider) {
         case "open-railway-map":
-
             // Base map without labels
             L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png", {
-                    attribution:
-                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                    attribution: '&copy; <a href="https://carto.com/attributions" target="carto">CARTO</a>',
                     subdomains: "abcd",
                     maxZoom: 19
                 }
             ).addTo(map);
 
             // Semi-transparent Open Railway Map overlay. There are additional filters on the tiles in css.
-            new L.TileLayer('http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', {
-                attribution: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>, Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a> and OpenStreetMap',
+            new L.TileLayer('https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openrailwaymap.org/" target="orm">OpenRailwayMap</a>',
                 minZoom: 2,
                 maxZoom: 19,
                 tileSize: 256
             }).addTo(map);
+
+            //add additional copyright notice to map
             break;
         case "cargo":
         default:
             // Default voyager map
             L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                attribution: '&copy; <a href="https://carto.com/attributions" target="carto">CARTO</a>',
                 subdomains: "abcd",
                 maxZoom: 19
             }).addTo(map);
     }
+
+    map.attributionControl.addAttribution('&copy; <a href="https://www.openstreetmap.org/copyright" target="osm">OpenStreetMap</a> contributors');
 }
