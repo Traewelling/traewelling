535ba80e4b9547d05caaebd16cbb8105a48b4441
Kris <git@k118.de>
2023-07-24 17:30:38 +0200

    :white_check_mark: Remove RefreshDatabase from UnitTests (#1760)

--- tests/Unit/Transport/PointsCalculation/FactorTest.php
+++ tests/Unit/Transport/PointsCalculation/FactorTest.php
@@ -4,8 +4,9 @@ namespace Tests\Unit\Transport\PointsCalculation;
 
 use App\Enum\PointReason;
 use App\Http\Controllers\Backend\Transport\PointsCalculationController;
+use Tests\Unit\UnitTestCase;
 
-class FactorTest
+class FactorTest extends UnitTestCase
 {
     public function testInTimeFactor(): void {
         $this->assertEquals(1, PointsCalculationController::getFactorByReason(PointReason::IN_TIME));
