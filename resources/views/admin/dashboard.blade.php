c6003608fd2f00c8683aa0e3f7c98b4e877306ac
Kristian Stöckel <git@k118.de>
2023-07-26 20:10:57 +0200

    :art: Use bootstrap dark theme for admin panel

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layout')
 
 @section('content')
-    <div class="text-center">
+    <div class="text-center text-white">
         <i class="fa-solid fa-hammer fa-5x"></i>
-        <h1 class="mt-3 mb-3 text-dark fs-3" id="pageTitle">
+        <h1 class="mt-3 mb-3 fs-3" id="pageTitle">
             Welcome dear Admin!<br/>
             Don't do anything stupid!
         </h1>
