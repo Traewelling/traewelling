1a3b8e61126e6b826ef5501e7506a40154f3ebf3
Marie Ramlow <me@nycode.dev>
2023-09-01 18:02:51 +0200

    make redis scheme configurable

--- config/database.php
+++ config/database.php
@@ -127,6 +127,7 @@ return [
         ],
 
         'default' => [
+            'scheme'   => env('REDIS_SCHEME', 'tcp'),
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port'     => env('REDIS_PORT', 6379),
