ddd89bbee4446429ef4970914cb43b51ada809e0
Levin Herr <github@17d.me>
2023-10-11 10:48:04 +0200

    Update 2023_09_23_162754_add-parent-to-polylines.php
    Since this migration likely times out with a large dataset, there has to be a fallback so that it will not break in the next migration run.
    
    The Column will most likely be created, but the foreign key might not. The only practical approach in Laravel to check if a foreign key exists is, to just catch the exception if the migration fails to drop a non existing foreign

--- database/migrations/2023_09_23_162754_add-parent-to-polylines.php
+++ database/migrations/2023_09_23_162754_add-parent-to-polylines.php
@@ -7,16 +7,24 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void {
-        Schema::table('poly_lines', function(Blueprint $table) {
-            $table->unsignedBigInteger('parent_id')->nullable()->after('id');
-            $table->foreign('parent_id')->references('id')->on('poly_lines');
-        });
+        if (!Schema::hasColumn('poly_lines', 'parent_id') {
+            Schema::table('poly_lines', function(Blueprint $table) {
+                $table->unsignedBigInteger('parent_id')->nullable()->after('id');
+                $table->foreign('parent_id')->references('id')->on('poly_lines');
+            });
+        }
     }
 
     public function down(): void {
-        Schema::table('poly_lines', function(Blueprint $table) {
-            $table->dropForeign(['parent_id']);
-            $table->dropColumn('parent_id');
-        });
+        try {
+            Schema::table('poly_lines', function(Blueprint $table) {
+                $table->dropForeign(['parent_id']);
+                $table->dropColumn('parent_id');
+            });
+        } catch(\Throwable) {
+            Schema::table('poly_lines, function(Blueprint $table) {
+                $table->dropColumn('parent_id);
+            });
+        }
     }
 };
