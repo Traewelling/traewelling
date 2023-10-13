9c08a741bfc48bead542fb4f9c0c86605097bfa9
Levin <github@17d.me>
2023-10-13 11:30:59 +0200

    correct faulty code. Add console output.

--- database/migrations/2023_09_23_162754_add-parent-to-polylines.php
+++ database/migrations/2023_09_23_162754_add-parent-to-polylines.php
@@ -3,27 +3,34 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 return new class extends Migration
 {
     public function up(): void {
-        if (!Schema::hasColumn('poly_lines', 'parent_id') {
-            Schema::table('poly_lines', function(Blueprint $table) {
+        if (!Schema::hasColumn('poly_lines', 'parent_id')) {
+            Schema::table('poly_lines', function (Blueprint $table) {
                 $table->unsignedBigInteger('parent_id')->nullable()->after('id');
                 $table->foreign('parent_id')->references('id')->on('poly_lines');
             });
+        } else {
+            $output = new ConsoleOutput();
+            $output->writeln("\nColumn already exists. Skipping.");
         }
     }
 
     public function down(): void {
         try {
-            Schema::table('poly_lines', function(Blueprint $table) {
+            Schema::table('poly_lines', function (Blueprint $table) {
                 $table->dropForeign(['parent_id']);
                 $table->dropColumn('parent_id');
             });
-        } catch(\Throwable) {
-            Schema::table('poly_lines, function(Blueprint $table) {
-                $table->dropColumn('parent_id);
+        } catch (\Throwable) {
+            $output = new ConsoleOutput();
+            $output->writeln("\nForeign key didn't exist. Dropping only column.");
+
+            Schema::table('poly_lines', function (Blueprint $table) {
+                $table->dropColumn('parent_id');
             });
         }
     }
