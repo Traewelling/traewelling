8e1198703402ec405daf703e2b3ae5436de1f5f8
Marie <me@nycode.dev>
2023-10-19 19:19:24 +0200

    🐛 initialize window.notyf earlier (#2069)

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,34 @@ import "./api/api";
 import "./components/maps";
 import CheckinSuccessHelper from "../vue/components/CheckinSuccessHelper.vue";
 
-document.addEventListener("DOMContentLoaded", function () {
+window.notyf = new Notyf({
+    duration: 5000,
+    position: { x: "right", y: "top" },
+    dismissible: true,
+    ripple: true,
+    types: [
+        {
+            type: "info",
+            background: "#0dcaf0",
+            icon: {
+                className: "fa-solid fa-circle-info",
+                color: "white",
+                tagName: "i",
+            },
+        },
+        {
+            type: "warning",
+            background: "#ffc107",
+            icon: {
+                className: "fa-solid fa-triangle-exclamation",
+                tagName: "i",
+                color: "white",
+            },
+        },
+    ],
+});
+
+document.addEventListener("DOMContentLoaded", function() {
 
     const app = createApp({});
     app.component('NotificationBell', NotificationBell);
@@ -34,33 +61,6 @@ document.addEventListener("DOMContentLoaded", function () {
     app4.component('CheckinSuccessHelper', CheckinSuccessHelper);
     app4.mount('#checkin-success-helper');
 
-
-    window.notyf = new Notyf({
-        duration: 5000,
-        position: { x: "right", y: "top" },
-        dismissible: true,
-        ripple: true,
-        types: [
-            {
-                type: "info",
-                background: "#0dcaf0",
-                icon: {
-                    className: "fa-solid fa-circle-info",
-                    color: "white",
-                    tagName: "i",
-                },
-            },
-            {
-                type: "warning",
-                background: "#ffc107",
-                icon: {
-                    className: "fa-solid fa-triangle-exclamation",
-                    tagName: "i",
-                    color: "white",
-                },
-            },
-        ],
-    });
 });
 
 /**
