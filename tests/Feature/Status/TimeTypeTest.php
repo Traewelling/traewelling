06cb5b34834b0d35a3692e2073dda6460b09e4db
Jannik <jannik@outlook.com>
2023-11-06 22:17:24 +0100

    :recycle: Refactor feature test

--- tests/Feature/Status/TimeTypeTest.php
+++ tests/Feature/Status/TimeTypeTest.php
@@ -11,53 +11,80 @@ class TimeTypeTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * @dataProvider manualTimeProvider
-     */
-    public function testManualTime(bool $targetArrival, ?int $manualDeltaMins, ?bool $shouldHaveOriginal, bool $shouldBeTypeManual) {
-        $checkin       = TrainCheckin::factory()->create();
-
-        if (isset($manualDeltaMins)) {
-            if ($targetArrival) {
-                $edit_time = $checkin->destination_stopover->arrival_planned->copy();
-                $checkin->manual_arrival = $edit_time->addMinutes($manualDeltaMins);
-            } else {
-                $edit_time = $checkin->origin_stopover->departure_planned->copy();
-                $checkin->manual_departure = $edit_time->addMinutes($manualDeltaMins);
-            }
-            $checkin->update();
-        }
-
-        if ($targetArrival) {
-            $display_time = $checkin->displayArrival;
-        } else {
-            $display_time = $checkin->displayDeparture;
-        }
-
-        if (isset($shouldHaveOriginal)) {
-            $this->assertTrue($shouldHaveOriginal === isset($display_time->original));
-        }
-        $this->assertTrue($shouldBeTypeManual === ($display_time->type === TimeType::MANUAL));
+    public static function getTimeTypeFindPreference(): array {
+        return [
+            [false, false, TimeType::PLANNED],
+            [false, true, TimeType::REALTIME],
+            [true, false, TimeType::MANUAL],
+            [true, true, TimeType::MANUAL],
+        ];
     }
 
-    public static function manualTimeProvider() {
-        $testData = [];
-        foreach ([false, true] as $b) {
-            $testData = array_merge($testData, [
-                [
-                    $b, null, null, false
-                ],
-                [
-                    $b, 0, false, true
-                ],
-                [
-                    $b, 2, true, true
-                ],
-                [
-                    $b, -1, true, true
-                ],
-            ]);
+    /**
+     * @dataProvider getTimeTypeFindPreference
+     */
+    public function testTimeTypeFindPreference(bool $manual, bool $delay, TimeType $expected): void {
+        // GIVEN
+        $checkin = TrainCheckin::factory()->create();
+
+        $checkin->origin_stopover->departure_real = null;
+        $checkin->origin_stopover->update();
+
+        // WHEN
+        if ($manual) $this->setManualDeparture($checkin, 8);
+        if ($delay) $this->setDelayedTrainDeparture($checkin, 5);
+
+        // THEN
+        $this->assertEquals($expected, $checkin->displayDeparture->type);
+
+        if ($manual || $delay) {
+            $this->assertNotNull($checkin->displayDeparture->original);
+
         }
-        return $testData;
+    }
+
+    public function testSameManualDeparture(): void {
+        // GIVEN
+        $checkin = TrainCheckin::factory()->create();
+
+        $checkin->origin_stopover->departure_real = null;
+        $checkin->origin_stopover->update();
+
+        // WHEN
+        $this->setManualDeparture($checkin, 0);
+
+        // THEN
+        $this->assertEquals(TimeType::MANUAL, $checkin->displayDeparture->type);
+        $this->assertNull($checkin->displayDeparture->original);
+    }
+
+    public function testSameRealTimeDeparture(): void {
+        // GIVEN
+        $checkin = TrainCheckin::factory()->create();
+
+        $checkin->origin_stopover->departure_real = null;
+        $checkin->origin_stopover->update();
+
+        // WHEN
+        $this->setDelayedTrainDeparture($checkin, 0);
+
+        // THEN
+        $this->assertEquals(TimeType::REALTIME, $checkin->displayDeparture->type);
+        $this->assertNull($checkin->displayDeparture->original);
+    }
+
+    private function setDelayedTrainDeparture($checkin, int $min) {
+        $checkin->origin_stopover->departure_real = $checkin->origin_stopover
+            ->departure_planned
+            ->copy()
+            ->addMinutes($min);
+        $checkin->origin_stopover->update();
+    }
+
+    private function setManualDeparture($checkin, int $min) {
+        $checkin->manual_departure = $checkin->departure
+            ->copy()
+            ->addMinutes($min);
+        $checkin->update();
     }
 }
