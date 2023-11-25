b989b06142140a9fb7301e2371f394f17e44d524
Kristian Stöckel <git@k118.de>
2023-11-25 14:31:49 +0100

    :monocle_face: set last_login a little bit earlier

--- app/Http/Controllers/Backend/Auth/LoginController.php
+++ app/Http/Controllers/Backend/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend\Auth;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -11,6 +12,7 @@ class LoginController extends Controller
         $loginType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
         if (Auth::attempt([$loginType => $login, 'password' => $password], $remember)) {
+            auth()->user()->update(['last_login' => now()]);
             return true;
         }
 
