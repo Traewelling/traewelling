e31071e9b55c6aa2ed0b486c071a830223fec108
Kris <git@k118.de>
2023-12-20 19:25:55 +0100

    :card_file_box: prepare checkins table to use relation instead of duplicate data (#2222)

---
+++ app/Console/Commands/MigrateStopovers.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TrainCheckin;
+use Illuminate\Console\Command;
+
+/**
+ * @deprecated Just created and marked as deprecated, because it is only needed for migrating old checkins.
+ *             Can be deleted after migration.
+ */
+class MigrateStopovers extends Command
+{
+
+    protected $signature   = 'app:migrate-stopovers';
+    protected $description = 'Calculate missing stopover relations for train checkins. Currently only needed for migrating old checkins.';
+
+    public function handle(): int {
+        while (TrainCheckin::whereNull('origin_stopover_id')->orWhereNull('destination_stopover_id')->count() > 0) {
+            TrainCheckin::with(['HafasTrip.stopovers', 'originStation', 'destinationStation'])
+                        ->whereNull('origin_stopover_id')
+                        ->orWhereNull('destination_stopover_id')
+                        ->limit(100)
+                        ->each(function(TrainCheckin $checkin) {
+                            $originStopover      = $checkin->HafasTrip->stopovers->where('train_station_id', $checkin->originStation->id)
+                                                                                 ->where('departure_planned', $checkin->departure)
+                                                                                 ->first();
+                            $destinationStopover = $checkin->HafasTrip->stopovers->where('train_station_id', $checkin->destinationStation->id)
+                                                                                 ->where('arrival_planned', $checkin->arrival)
+                                                                                 ->first();
+                            $checkin->update([
+                                                 'origin_stopover_id'      => $originStopover->id,
+                                                 'destination_stopover_id' => $destinationStopover->id,
+                                             ]);
+
+                            $this->info("Migrated stopover ids for checkin {$checkin->id}");
+                        });
+        }
+        return Command::SUCCESS;
+    }
+}
