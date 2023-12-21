c1d9f487364da6a9a02fbc6503135cf816820c0f
Kristian Stöckel <git@k118.de>
2023-12-21 21:05:27 +0100

    :fire: remove deprecated code

--- app/Http/Resources/StatusResource.php
+++ app/Http/Resources/StatusResource.php
@@ -42,9 +42,7 @@ class StatusResource extends JsonResource
                 'points'              => (int) $this->trainCheckin->points,
                 'duration'            => (int) $this->trainCheckin->duration,
                 'speed'               => 0.0, //deprecated: TODO: remove after 2023-12-31
-                'overriddenDeparture' => $this->trainCheckin->manual_departure?->toIso8601String(), //TODO: deprecated: remove after 2023-10 (#1809)
                 'manualDeparture'     => $this->trainCheckin->manual_departure?->toIso8601String(),
-                'overriddenArrival'   => $this->trainCheckin->manual_arrival?->toIso8601String(), //TODO: deprecated: remove after 2023-10 (#1809)
                 'manualArrival'       => $this->trainCheckin->manual_arrival?->toIso8601String(),
                 'origin'              => new StopoverResource($this->trainCheckin->originStopover),
                 'destination'         => new StopoverResource($this->trainCheckin->destinationStopover),
