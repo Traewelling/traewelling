a6823cff4af59acc68aa4c5d2b4575289d7df19a
Kevin Arutyunyan <d3d9@riseup.net>
2023-12-21 19:05:54 +0100

    :monocle_face: improve caching of checkin duration

--- app/Observers/CheckinObserver.php
+++ app/Observers/CheckinObserver.php
@@ -9,7 +9,7 @@ class CheckinObserver
 {
 
     public function updated(Checkin $checkin): void {
-        if ($checkin->isDirty(['origin', 'destination', 'departure', 'arrival'])) {
+        if ($checkin->isDirty(['origin', 'destination', 'departure', 'arrival', 'manual_departure', 'manual_arrival'])) {
             //if origin, destination, departure or arrival is changed, update duration
             TrainCheckinController::calculateCheckinDuration($checkin);
         }
