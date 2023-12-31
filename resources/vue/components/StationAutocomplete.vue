a9fa44f9cc93c1ebefb13ad1ab71471d8f461331
Levin Herr <github@17d.me>
2023-12-31 22:39:27 +0100

    ✨ Add timepicker for experimental features (#2249)

--- resources/vue/components/StationAutocomplete.vue
+++ resources/vue/components/StationAutocomplete.vue
@@ -2,10 +2,13 @@
 import FullScreenModal from "./FullScreenModal.vue";
 import _ from "lodash";
 import {trans} from "laravel-vue-i18n";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import {DateTime} from "luxon";
 
 export default {
     name: "StationAutocomplete",
-    components: {FullScreenModal},
+    components: {FullScreenModal, VueDatePicker},
     props: {
         station: {
             type: Object,
@@ -15,6 +18,11 @@ export default {
             type: Boolean,
             required: false,
             default: false
+        },
+        time: {
+            type: DateTime,
+            required: false,
+            default: null
         }
     },
     data() {
@@ -23,6 +31,7 @@ export default {
             loading: false,
             autocompleteList: [],
             stationInput: "",
+            date: null
         };
     },
     methods: {
@@ -41,26 +50,50 @@ export default {
             this.loading = true;
             if (!this.stationInput || this.stationInput.length < 3) {
                 this.autocompleteList = [];
-                this.loading = false;
+                this.loading          = false;
                 return;
             }
             let query = this.stationInput.replace(/%2F/, " ").replace(/\//, " ");
             fetch(`/api/v1/trains/station/autocomplete/${query}`).then((response) => {
                 response.json().then((result) => {
                     this.autocompleteList = result.data;
-                    this.loading = false;
+                    this.loading          = false;
                 });
             });
+        },
+        showPicker() {
+            this.$refs.picker.openMenu();
+        },
+        setTime() {
+            this.$emit("update:time", DateTime.fromJSDate(this.date).setZone('UTC').toISO());
+        },
+        setStationFromText() {
+            this.setStation({name: this.stationInput});
         },
         setStation(item) {
             this.stationInput = item.name;
             this.$emit("update:station", item);
             this.$refs.modal.hide();
-            window.location="/trains/stationboard?station=" + item.name;
+            window.location = "/trains/stationboard?station=" + item.name;
+        },
+        setStationFromGps() {
+            if (!navigator.geolocation) {
+                notyf.error(trans("stationboard.position-unavailable"));
+                return;
+            }
+
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    window.location.href = `/trains/nearby?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`;
+                },
+                () => {
+                    notyf.error(trans("stationboard.position-unavailable"));
+                }
+            );
         }
     },
     watch: {
-        stationInput: _.debounce(function() {
+        stationInput: _.debounce(function () {
             this.autocomplete();
         }, 500),
         station() {
@@ -68,8 +101,17 @@ export default {
         }
     },
     mounted() {
+        this.date         = this.time;
         this.stationInput = this.station ? this.station.name : "";
         this.getRecent();
+    },
+    computed: {
+        placeholder() {
+            return `${trans('stationboard.station-placeholder')} ${trans('or-alternative')} ${trans('ril100')}`;
+        },
+        dark() {
+            return localStorage.getItem('darkMode') === 'dark';
+        }
     }
 }
 </script>
@@ -78,20 +120,27 @@ export default {
     <FullScreenModal ref="modal">
         <template #header>
             <input type="text" name="station" class="form-control"
-                   placeholder="Station or Ril 100 identifier"
+                   :placeholder="placeholder"
                    v-model="stationInput"
+                   @keyup.enter="setStationFromText"
             />
         </template>
         <template #body>
             <ul class="list-group list-group-light list-group-small">
+                <li class="list-group-item pb-3 mb-3" v-show="autocompleteList.length === 0" @click="setStationFromGps">
+                    <a href="#" class="text-trwl">
+                        <i class="fa fa-map-marker-alt"></i>
+                        {{ trans("stationboard.search-by-location") }}
+                    </a>
+                </li>
                 <li class="list-group-item" v-for="item in recent" v-show="autocompleteList.length === 0">
                     <a href="#" class="text-trwl" @click="setStation(item)">
-                        {{item.name}} <span v-if="item.rilIdentifier">({{item.rilIdentifier}})</span>
+                        {{ item.name }} <span v-if="item.rilIdentifier">({{ item.rilIdentifier }})</span>
                     </a>
                 </li>
                 <li class="list-group-item" v-for="item in autocompleteList" @click="setStation(item)">
                     <a href="#" class="text-trwl">
-                        {{item.name}} <span v-if="item.rilIdentifier">({{item.rilIdentifier}})</span>
+                        {{ item.name }} <span v-if="item.rilIdentifier">({{ item.rilIdentifier }})</span>
                     </a>
                 </li>
             </ul>
@@ -100,24 +149,33 @@ export default {
     <div class="card mb-4">
         <div class="card-header">{{ trans("stationboard.where-are-you") }}</div>
         <div class="card-body">
-                <div id="station-autocomplete-container" style="z-index: 3;">
-                    <div class="input-group mb-2 mr-sm-2">
-                        <input type="text" name="station" class="form-control"
-                               placeholder="Station or Ril 100 identifier"
-                               v-model="stationInput" @focusin="showModal"
-                        />
-                        <button type="button"
-                                class="btn btn-outline-dark stationSearchButton"
-                                id="gps-button"
-                                data-mdb-ripple-color="dark"
-                                >
-                            <i class="fa fa-map-marker-alt"></i>
-                            <div class="spinner-border d-none" role="status" style="height: 1rem; width: 1rem;">
-                                <span class="visually-hidden">Loading...</span>
-                            </div>
-                        </button>
-                    </div>
+            <div id="station-autocomplete-container" style="z-index: 3;">
+                <div class="input-group mb-2 mr-sm-2">
+                    <input type="text" name="station" class="form-control"
+                           :placeholder="placeholder"
+                           v-model="stationInput"
+                           @focusin="showModal"
+                            @keyup.enter="setStationFromText"
+                    />
+                    <button type="button" class="btn btn-outline-dark stationSearchButton" @click="showPicker">
+                        <i class="fa fa-clock" aria-hidden="true"></i>
+                    </button>
                 </div>
+                <VueDatePicker
+                        v-model="date"
+                        ref="picker"
+                        @update:model-value="setTime"
+                        time-picker-inline
+                        :dark="dark"
+                        :action-row="{ showSelect: true, showCancel: true, showNow: true, showPreview: true }"
+                >
+                    <template #trigger>
+                        <button type="button" class="btn btn-outline-dark stationSearchButton" hidden>
+                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                        </button>
+                    </template>
+                </VueDatePicker>
+            </div>
         </div>
     </div>
 
