5dcbc08f0a17fe2acfab8e1a8f457ccdb21ac539
Kris <git@k118.de>
2024-01-19 18:47:26 +0100

    :bug: fix wrong inverted polylines (#2279)

--- app/Dto/GeoJson/Feature.php
+++ app/Dto/GeoJson/Feature.php
@@ -24,14 +24,19 @@ class Feature implements \JsonSerializable
         return new self([$coordinate->longitude, $coordinate->latitude], 'Point');
     }
 
-    public function getCoordinates(): array {
-        return $this->coordinates;
+    public function getCoordinates(bool $invert = false): array {
+        if (!$invert) {
+            return $this->coordinates;
+        }
+        return array_map(static function($coordinate) {
+            return [$coordinate->latitude, $coordinate->longitude];
+        }, $this->coordinates);
     }
 
     public function toArray(): array {
         $response = [
-            'type'     => 'Feature',
-            'geometry' => [
+            'type'       => 'Feature',
+            'geometry'   => [
                 'type'        => $this->type,
                 'coordinates' => $this->coordinates
             ],
