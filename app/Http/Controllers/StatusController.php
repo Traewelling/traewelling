538a861d9c79ed63b5c2829f106a0eb4e05d905d
Kristian Stöckel <git@k118.de>
2024-01-21 15:04:16 +0100

    add log message

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -20,6 +20,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use InvalidArgumentException;
 use stdClass;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -270,6 +271,10 @@ class StatusController extends Controller
         Event            $event = null
     ): Status {
         if ($event !== null && !Carbon::now()->isBetween($event->begin, $event->end)) {
+            Log::info('Event checkin was prevented because the event is not active anymore', [
+                'event' => $event->only(['id', 'name', 'begin', 'end']),
+                'user'  => $user->only(['id', 'username']),
+            ]);
             $event = null;
         }
 
