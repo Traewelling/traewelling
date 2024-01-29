15d283042ed41f3173e47ab169560e98a83eb84d
Kristian Stöckel <git@k118.de>
2024-01-29 21:53:04 +0100

    add privacy_policy_url

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -11,8 +11,9 @@ class ClientResource extends JsonResource
 {
     public function toArray($request) {
         return [
-            'id'   => $this->id,
-            'name' => $this->name,
+            'id'               => $this->id,
+            'name'             => $this->name,
+            'privacyPolicyUrl' => $this->privacy_policy_url,
         ];
     }
 }
