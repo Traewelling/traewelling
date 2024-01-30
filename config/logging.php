3a2902ad21c8e221c9bbd3843f86a6ce8c15e123
Kristian Stöckel <git@k118.de>
2024-01-30 10:42:39 +0100

    :wrench: make log level configurable

--- config/logging.php
+++ config/logging.php
@@ -43,13 +43,13 @@ return [
         'single' => [
             'driver' => 'single',
             'path'   => storage_path('logs/laravel.log'),
-            'level'  => 'debug',
+            'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
             'path'   => storage_path('logs/laravel.log'),
-            'level'  => 'debug',
+            'level' => env('LOG_LEVEL', 'debug'),
             'days'   => 14,
         ],
 
