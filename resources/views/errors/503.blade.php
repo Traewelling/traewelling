2d9e8abc7213a32f95fe21269d7afd67121b83dc
Levin Herr <github@17d.me>
2024-02-01 20:54:42 +0100

    🚸 Change info text on maintenance page (#2310)

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -7,10 +7,11 @@
         <h1 class="display-1 mb-4">503</h1>
         <h2 class="mb-3">
             <i class="fa-solid fa-wrench"></i>
-            {{ __('maintenance') }}
+            {{ __('maintenance.title') }}
         </h2>
         <p class="lead text-muted">
-            {{__('maintenance.title')}} {{__('try-later')}}
+            {{__('maintenance.subtitle')}} {{__('maintenance.try-later')}} :)<br><br>
+            {{__('maintenance.prolonged')}}
         </p>
     </div>
     <hr/>
