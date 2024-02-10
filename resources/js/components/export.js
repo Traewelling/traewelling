9e2adc1ba184d19a47c36bb8c03d87ce5dde1655
Levin <github@17d.me>
2024-02-10 18:15:20 +0100

    Move warning script to dedicated method, expand to preselection-buttons

--- resources/js/components/export.js
+++ resources/js/components/export.js
@@ -1,3 +1,4 @@
+// Event Listener
 document.getElementById("export-nominal").addEventListener("click", function () {
     resetFields();
     selectNominal();
@@ -9,10 +10,29 @@ document.getElementById("export-all").addEventListener("click", function () {
 
 document.getElementById("export-tags").addEventListener("click", function () {
     resetFields();
-    selectNominal();
-    selectFromArray(["status_tags"]);
+    selectNominalAndTags();
 });
 
+document.querySelector('select[name="columns[]"]').addEventListener('change', function (e) {
+    if (e.target.selectedOptions.length > 8) {
+        showWarning();
+    } else {
+        showWarning(false);
+    }
+});
+
+// Warning function
+function showWarning(show=true) {
+    const classList = document.querySelector('#alert-pdf-count').classList;
+
+    if (show) {
+        classList.remove('d-none');
+    } else {
+        classList.add('d-none');
+    }
+}
+
+// Select functions
 function selectAll() {
     let select = document.getElementById("export-select");
 
@@ -21,25 +41,34 @@ function selectAll() {
     }
 }
 
+function selectNominal() {
+    const nominal = ["status_id", "line_name", "origin_name", "departure_planned", "destination_name", "arrival_planned", "distance", "points", "body"];
+    showWarning(false);
+
+    selectFromArray(nominal);
+}
+
+function selectNominalAndTags() {
+    selectNominal()
+    selectFromArray(["status_tags"]);
+    showWarning();
+}
+
+// Helper functions
 function resetFields() {
     let select = document.getElementById("export-select");
 
     for (let i = 0; i < select.options.length; i++) {
         select.options[i].selected = false;
     }
-}
-
-function selectNominal() {
-    const nominal = ["status_id", "line_name", "origin_name", "departure_planned", "destination_name", "arrival_planned", "distance", "points", "body"];
-
-    selectFromArray(nominal);
+    showWarning(false);
 }
 
 function selectFromArray(array) {
     for (let i = 0; i < array.length; i++) {
         let select = document.getElementById("export-select");
         let option = select.querySelector(`option[value="${array[i]}"]`);
-        if(option === null) {
+        if (option === null) {
             continue;
         }
 
