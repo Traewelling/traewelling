6c9d80a6cb5cd0b94b31467784bfa7aeb5b340b7
Kristian Stöckel <git@k118.de>
2024-03-10 22:41:25 +0100

    :card_file_box: add database layout for IFOPT
    see #2411

--- database/factories/StationFactory.php
+++ database/factories/StationFactory.php
@@ -9,10 +9,18 @@ class StationFactory extends Factory
     public function definition(): array {
         return [
             'ibnr'          => $this->faker->unique()->numberBetween(8000001, 8999999),
+            'wikidata_id'   => null,
+            'ifopt_a'       => $this->faker->randomElement(['de', 'at', 'ch', 'fr']),
+            'ifopt_b'       => $this->faker->numberBetween(10000, 99999),
+            'ifopt_c'       => $this->faker->numberBetween(1, 9999),
+            'ifopt_d'       => $this->faker->boolean() ? $this->faker->numberBetween(1, 9999) : null,
+            'ifopt_e'       => $this->faker->boolean() ? $this->faker->numberBetween(1, 9) : null,
+            'rilIdentifier' => substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 4),
             'name'          => $this->faker->unique()->city,
             'latitude'      => $this->faker->latitude,
             'longitude'     => $this->faker->longitude,
-            'rilIdentifier' => substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 4),
+            'time_offset'   => null,
+            'shift_time'    => null,
         ];
     }
 }
---
+++ database/migrations/2024_03_10_211526_add_ifopt_to_stations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::table('train_stations', static function(Blueprint $table) {
+            $table->unsignedInteger('ifopt_e')->nullable()->comment('Platform')->after('wikidata_id');
+            $table->unsignedInteger('ifopt_d')->nullable()->comment('Area')->after('wikidata_id');
+            $table->unsignedInteger('ifopt_c')->nullable()->comment('Station')->after('wikidata_id');
+            $table->unsignedInteger('ifopt_b')->nullable()->comment('Municipal')->after('wikidata_id');
+            $table->string('ifopt_a')->nullable()->comment('Country')->after('wikidata_id');
+
+            $table->index(['ifopt_a', 'ifopt_b', 'ifopt_c', 'ifopt_d', 'ifopt_e'], 'ifopt');
+        });
+    }
+
+    public function down(): void {
+        Schema::table('train_stations', static function(Blueprint $table) {
+            $table->dropIndex('ifopt');
+            $table->dropColumn('ifopt_e');
+            $table->dropColumn('ifopt_d');
+            $table->dropColumn('ifopt_c');
+            $table->dropColumn('ifopt_b');
+            $table->dropColumn('ifopt_a');
+        });
+    }
+};
