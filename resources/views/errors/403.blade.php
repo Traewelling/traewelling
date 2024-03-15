28b0f969069e97d0a05b490601001275d3959275
Kris <git@k118.de>
2024-03-15 11:39:00 +0100

    :technologist: fix translation output on 403 page (#2421)

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,4 +3,4 @@
 @section('image', asset('images/covers/traffic_lights.jpg'))
 @section('title', __('error.403'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'error.403'))
+@section('message', $exception->getMessage() ?: __('error.403'))
