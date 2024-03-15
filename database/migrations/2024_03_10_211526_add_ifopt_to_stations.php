e6aabf3723939f3e1d2cdf0f41165ebc51e9cb7e
Kris <git@k118.de>
2024-03-15 13:48:26 +0100

    Update database/migrations/2024_03_10_211526_add_ifopt_to_stations.php
    Co-authored-by: Levin Herr <github@17d.me>

--- database/migrations/2024_03_10_211526_add_ifopt_to_stations.php
+++ database/migrations/2024_03_10_211526_add_ifopt_to_stations.php
@@ -10,7 +10,7 @@ return new class extends Migration
         Schema::table('train_stations', static function(Blueprint $table) {
             $table->unsignedInteger('ifopt_e')->nullable()->comment('Platform')->after('wikidata_id');
             $table->unsignedInteger('ifopt_d')->nullable()->comment('Area')->after('wikidata_id');
-            $table->unsignedInteger('ifopt_c')->nullable()->comment('Station')->after('wikidata_id');
+            $table->unsignedInteger('ifopt_c')->nullable()->comment('Mode / Stop Place')->after('wikidata_id');
             $table->unsignedInteger('ifopt_b')->nullable()->comment('Administrative Area')->after('wikidata_id');
             $table->string('ifopt_a')->nullable()->comment('Country')->after('wikidata_id');
 
