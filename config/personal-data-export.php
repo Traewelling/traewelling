bf8cbe412d4cd1fc8ab85b29b1c8e60912f5a62f
Levin <github@17d.me>
2024-03-31 18:46:45 +0200

    Autoformatter

--- config/personal-data-export.php
+++ config/personal-data-export.php
@@ -4,7 +4,7 @@ return [
     /*
      * The disk where the exports will be stored by default.
      */
-    'disk' => 'personal-data-exports',
+    'disk'                     => 'personal-data-exports',
 
     /*
      * If you want to keep the original directory structure for added files,
@@ -14,26 +14,26 @@ return [
     /*
      * The amount of days the exports will be available.
      */
-    'delete_after_days' => 5,
+    'delete_after_days'        => 5,
 
     /*
      * Determines whether the user should be logged in to be able
      * to access the export.
      */
-    'authentication_required' => true,
+    'authentication_required'  => true,
 
     /*
      * The notification which will be sent to the user when the export
      * has been created.
      */
-    'notification' => \Spatie\PersonalDataExport\Notifications\PersonalDataExportedNotification::class,
+    'notification'             => \Spatie\PersonalDataExport\Notifications\PersonalDataExportedNotification::class,
 
     /*
      * Configure the queue and connection used by `CreatePersonalDataExportJob`
      * which will create the export.
      */
-    'job' => [
-        'queue' => 'export',
+    'job'                      => [
+        'queue'      => 'export',
         'connection' => null,
     ],
 ];
