65ea069db7b2fb2a0423f0d4e6d744dbdbfbc0a1
Levin Herr <github@17d.me>
2024-04-19 22:37:44 +0200

    Update database/migrations/2024_04_19_131906_add_index_to_poly_line_parent_id.php
    Co-authored-by: Kris <git@k118.de>

--- database/migrations/2024_04_19_131906_add_index_to_poly_line_parent_id.php
+++ database/migrations/2024_04_19_131906_add_index_to_poly_line_parent_id.php
@@ -6,16 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void
-    {
-        Schema::table('poly_lines', function (Blueprint $table) {
+    public function up(): void {
+        Schema::table('poly_lines', static function (Blueprint $table) {
             $table->index('parent_id');
         });
     }
 
-    public function down(): void
-    {
-        Schema::table('poly_lines', function (Blueprint $table) {
+    public function down(): void {
+        Schema::table('poly_lines', static function (Blueprint $table) {
             $table->dropIndex('parent_id');
         });
     }
