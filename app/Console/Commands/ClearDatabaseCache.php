9f13828b6f40c8e2d0cdadf4b23637407877fde2
Levin Herr <github@17d.me>
2024-04-20 18:42:30 +0200

    ⚡ Reduce time & strain on database whilst cleaning up (#2510)

--- app/Console/Commands/ClearDatabaseCache.php
+++ app/Console/Commands/ClearDatabaseCache.php
@@ -12,7 +12,19 @@ class ClearDatabaseCache extends Command
     protected $description = 'Clear the database cache and deletes expired cache keys';
 
     public function handle(): int {
-        $affectedRows = DB::table('cache')->where('expiration', '<', now()->timestamp)->delete();
+        $this->info('Clearing database cache...');
+        $this->output->writeln('');
+        $affectedRows = 0;
+        do {
+            $result = DB::table('cache')
+                        ->where('expiration', '<', now()->timestamp)
+                        ->limit(1000)
+                        ->delete();
+            if ($result > 0) {
+                $this->output->write('.');
+                $affectedRows += $result;
+            }
+        } while ($result > 0);
         $this->info("Deleted {$affectedRows} expired cache keys");
         return Command::SUCCESS;
     }
