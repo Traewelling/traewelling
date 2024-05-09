32f65af02a67d4cab00fead14bc473304a809d2d
tiuub <46517077+tiuub@users.noreply.github.com>
2024-05-09 18:25:41 +0200

    Implemented departureReal and arrivalReal support for manual trip creation in API

--- app/Http/Controllers/Backend/Transport/ManualTripCreator.php
+++ app/Http/Controllers/Backend/Transport/ManualTripCreator.php
@@ -25,8 +25,10 @@ class ManualTripCreator extends Controller
     private ?HafasOperator  $operator;
     private Station         $origin;
     private Carbon          $originDeparturePlanned;
+    private ?Carbon         $originDepartureReal;
     private Station         $destination;
     private Carbon          $destinationArrivalPlanned;
+    private ?Carbon         $destinationArrivalReal;
     private array           $stopovers = [];
 
     public function createFullTrip(): Trip {
@@ -63,6 +65,8 @@ class ManualTripCreator extends Controller
                              'train_station_id'  => $this->origin->id,
                              'arrival_planned'   => $this->originDeparturePlanned,
                              'departure_planned' => $this->originDeparturePlanned,
+                             'arrival_real'      => $this->originDepartureReal,
+                             'departure_real'    => $this->originDepartureReal,
                          ]);
     }
 
@@ -75,6 +79,8 @@ class ManualTripCreator extends Controller
                              'train_station_id'  => $this->destination->id,
                              'arrival_planned'   => $this->destinationArrivalPlanned,
                              'departure_planned' => $this->destinationArrivalPlanned,
+                             'arrival_real'      => $this->destinationArrivalReal,
+                             'departure_real'    => $this->destinationArrivalReal,
                          ]);
     }
 
@@ -102,22 +108,26 @@ class ManualTripCreator extends Controller
         return $this;
     }
 
-    public function setOrigin(Station $origin, Carbon $plannedDeparture): ManualTripCreator {
+    public function setOrigin(Station $origin, Carbon $plannedDeparture, ?Carbon $realDeparture): ManualTripCreator {
         $this->origin                 = $origin;
         $this->originDeparturePlanned = $plannedDeparture;
+        $this->originDepartureReal    = $realDeparture;
         return $this;
     }
 
-    public function setDestination(Station $destination, Carbon $plannedArrival): ManualTripCreator {
+    public function setDestination(Station $destination, Carbon $plannedArrival, ?Carbon $realArrival): ManualTripCreator {
         $this->destination               = $destination;
         $this->destinationArrivalPlanned = $plannedArrival;
+        $this->destinationArrivalReal    = $realArrival;
         return $this;
     }
 
     public function addStopover(
         Station $station,
         ?Carbon $plannedDeparture,
-        ?Carbon $plannedArrival
+        ?Carbon $plannedArrival,
+        ?Carbon $realDeparture,
+        ?Carbon $realArrival
     ): ManualTripCreator {
         if ($plannedDeparture === null && $plannedArrival === null) {
             throw new InvalidArgumentException('Either arrival or departure must be set');
@@ -125,11 +135,16 @@ class ManualTripCreator extends Controller
         if ($plannedDeparture !== null && $plannedArrival !== null && $plannedDeparture->isBefore($plannedArrival)) {
             throw new InvalidArgumentException('Departure must be after arrival');
         }
+        if ($realDeparture !== null && $realArrival !== null && $realDeparture->isBefore($realArrival)) {
+            throw new InvalidArgumentException('Real departure must be after real arrival');
+        }
 
         $this->stopovers[] = [
-            'station'   => $station,
-            'departure' => $plannedDeparture ?? $plannedArrival,
-            'arrival'   => $plannedArrival ?? $plannedDeparture,
+            'station'        => $station,
+            'departure'      => $plannedDeparture ?? $plannedArrival,
+            'arrival'        => $plannedArrival ?? $plannedDeparture,
+            'departure_real' => $realDeparture,
+            'arrival_real'   => $realArrival
         ];
         return $this;
     }
@@ -144,6 +159,8 @@ class ManualTripCreator extends Controller
                                  'train_station_id'  => $stopover['station']->id,
                                  'arrival_planned'   => $stopover['arrival'],
                                  'departure_planned' => $stopover['departure'],
+                                 'arrival_real'      => $stopover['arrival_real'],
+                                 'departure_real'    => $stopover['departure_real'],
                              ]);
         }
     }
