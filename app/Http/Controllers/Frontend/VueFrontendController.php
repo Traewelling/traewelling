85fb2fa98ccd1fc62513b8041f15fb3747f8e382
Kristian Stöckel <git@k118.de>
2024-05-20 19:27:09 +0200

    imports

--- app/Http/Controllers/Frontend/VueFrontendController.php
+++ app/Http/Controllers/Frontend/VueFrontendController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
+
+
+use Illuminate\View\View;
 
 class VueFrontendController
 {
-    public function stationboard(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application {
+    public function stationboard(): View {
         return view('vuestationboard', []);
     }
 }
