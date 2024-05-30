ed03d173d0ceba5c7cb0471c1efcb03d8afd4922
Levin Herr <github@17d.me>
2024-05-30 13:55:13 +0200

    :bug: Delete old notifications based on created_at (#2617)
    Co-authored-by: Kristian Stöckel <git@k118.de>

--- app/Console/Commands/DatabaseCleaner/Notifications.php
+++ app/Console/Commands/DatabaseCleaner/Notifications.php
@@ -15,25 +15,15 @@ class Notifications extends Command
         $this->info("Removing old notifications...");
         $affectedRows = 0;
         do {
-            $results = DatabaseNotification::where('read_at', '<', now()->subDays(30))->limit(1000)->delete();
+            $results      = DatabaseNotification::where('created_at', '<', now()->subDays(14))->limit(1000)->delete();
             $affectedRows += $results;
             if ($results > 0) {
                 $this->output->write('.');
             }
         } while ($results > 0);
         $this->output->writeln('');
-        $this->info("Removed $affectedRows old and read notifications.");
+        $this->info("Removed $affectedRows old notifications.");
 
-        $affectedRows = 0;
-        $this->output->writeln('');
-        do {
-            $results = DatabaseNotification::where('read_at', '<', now()->subMonths(6))->limit(1000)->delete();
-            if ($results > 0) {
-                $this->output->write('.');
-            }
-        } while ($results > 0);
-        $this->info("Removed $affectedRows old and unread notifications.");
-
-        return Command::SUCCESS;
+        return parent::SUCCESS;
     }
 }
