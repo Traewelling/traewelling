12f08212eb93bab13e0c50442e513b1f38ee7773
Kris <git@k118.de>
2024-06-10 23:09:06 +0200

    :memo: add todo to code (#2679)

--- app/Http/Resources/StopoverResource.php
+++ app/Http/Resources/StopoverResource.php
@@ -13,12 +13,12 @@ class StopoverResource extends JsonResource
             'name'                     => $this->station->name,
             'rilIdentifier'            => $this->station->rilIdentifier ?? null,
             'evaIdentifier'            => $this->station->ibnr,
-            'arrival'                  => $this->arrival?->toIso8601String(),
+            'arrival'                  => $this->arrival?->toIso8601String(), //TODO: not necessary if planned and real are available
             'arrivalPlanned'           => $this->arrival_planned?->toIso8601String(),
             'arrivalReal'              => $this->arrival_real?->toIso8601String(),
             'arrivalPlatformPlanned'   => $this->arrival_platform_planned ?? null,
             'arrivalPlatformReal'      => $this->arrival_platform_real ?? null,
-            'departure'                => $this->departure?->toIso8601String(),
+            'departure'                => $this->departure?->toIso8601String(), //TODO: not necessary if planned and real are available
             'departurePlanned'         => $this->departure_planned?->toIso8601String(),
             'departureReal'            => $this->departure_real?->toIso8601String(),
             'departurePlatformPlanned' => $this->departure_platform_planned ?? null,
