88aa1762ead4ebc270a0d9cad82c3b6d9a3a6cc1
Levin Herr <github@17d.me>
2024-06-11 22:10:14 +0200

    🚸 Center loading spinner (#2687)

---
+++ resources/vue/components/Spinner.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import {trans} from "laravel-vue-i18n";
+
+export default defineComponent({
+    name: "Spinner",
+    methods: {trans}
+})
+</script>
+
+<template>
+    <div class="d-flex justify-content-center p-2">
+        <div style="max-width: 200px;" class="spinner-grow text-trwl mx-auto">
+            <span class="visually-hidden">{{ trans('menu.loading') }}...</span>
+        </div>
+    </div>
+</template>
