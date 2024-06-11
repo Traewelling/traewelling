b63d9443be8967a9684ba9ab3be25f47a39196af
Levin <github@17d.me>
2024-06-11 21:56:41 +0200

    Add errors on linerun page

--- resources/vue/components/CheckinLineRun.vue
+++ resources/vue/components/CheckinLineRun.vue
@@ -1,5 +1,6 @@
 <script>
 import {DateTime} from "luxon";
+import {trans} from "laravel-vue-i18n";
 
 export default {
     name: "CheckinLineRun",
@@ -32,6 +33,8 @@ export default {
         return {
             lineRun: [],
             loading: false,
+            error: false,
+            errorMessage: ""
         };
     },
     methods: {
@@ -39,13 +42,20 @@ export default {
             this.$emit('update:destination', selected);
         },
         getLineRun() {
+            this.error   = false;
             this.loading = true;
+
             const params = new URLSearchParams({
                 hafasTripId: this.$props.selectedTrain.tripId,
                 lineName: this.$props.selectedTrain.line.name,
                 start: this.$props.selectedTrain.stop.id
             });
             fetch(`/api/v1/trains/trip?${params.toString()}`).then((response) => {
+                this.loading = false;
+                if (!response.ok) {
+                    this.error        = true;
+                    this.errorMessage = trans("messages.exception.hafas.502");
+                }
                 response.json().then((result) => {
                     this.lineRun           = result.data;
                     let remove             = true;
@@ -57,11 +67,13 @@ export default {
                         }
                         return !remove;
                     });
-                    this.loading           = false;
                     if (this.$props.fastCheckinIbnr) {
                         this.fastCheckin();
                     }
                 });
+            }).catch(() => {
+                this.error        = true;
+                this.errorMessage = trans("messages.exception.hafas.502");
             });
         },
         fastCheckin() {
@@ -90,6 +102,10 @@ export default {
 </script>
 
 <template>
+
+    <div v-if="error" class="text-trwl mx-auto p-2">
+        <p>{{ this.errorMessage }}</p>
+    </div>
     <div v-if="loading" class="spinner-grow text-trwl mx-auto p-2" style="max-width: 200px;" role="status">
         <span class="visually-hidden">Loading...</span>
     </div>
