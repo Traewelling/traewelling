53d441f707cdeb7e810659ae4a233fd7e3687a52
Kristian Stöckel <git@k118.de>
2024-07-05 12:48:31 +0200

    fix api attribute

--- app/Http/Resources/TrustedUserResource.php
+++ app/Http/Resources/TrustedUserResource.php
@@ -8,15 +8,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @OA\Schema(
  *     title="TrustedUser",
  *     @OA\Property(property="user", ref="#/components/schemas/LightUserResource"),
- *     @OA\Property(property="expires_at", type="string", format="date-time", example="2024-07-28T00:00:00Z")
+ *     @OA\Property(property="expiresAt", type="string", format="date-time", example="2024-07-28T00:00:00Z")
  * )
  */
 class TrustedUserResource extends JsonResource
 {
     public function toArray($request): array {
         return [
-            'user'       => new LightUserResource($this->trusted),
-            'expires_at' => $this->expires_at?->toIso8601String()
+            'user'      => new LightUserResource($this->trusted),
+            'expiresAt' => $this->expires_at?->toIso8601String()
         ];
     }
 }
