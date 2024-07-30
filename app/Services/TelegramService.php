9a1c57346c961ccdd8c2f23a21a9d70597739acf
Kristian Stöckel <git@k118.de>
2024-07-30 15:23:42 +0200

    use const; add doc

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Http;
 class TelegramService
 {
 
+    const TELEGRAM_API_URL = 'https://api.telegram.org/bot';
+
     private string $chatId;
     private string $token;
 
@@ -18,13 +20,16 @@ class TelegramService
     public static function isAdminActive(): bool {
         return config('services.telegram.admin.active');
     }
- 
+
     public static function admin(): self {
         return new self(config('services.telegram.admin.chat_id'), config('services.telegram.admin.token'));
     }
 
+    /**
+     * @return int Telegram Message ID
+     */
     public function sendMessage(string $text, string $parseMode = 'HTML'): int {
-        $response = Http::post('https://api.telegram.org/bot' . $this->token . '/sendMessage', [
+        $response = Http::post(self::TELEGRAM_API_URL . $this->token . '/sendMessage', [
             'chat_id'    => $this->chatId,
             'text'       => $text,
             'parse_mode' => $parseMode,
@@ -33,7 +38,7 @@ class TelegramService
     }
 
     public function deleteMessage(int $messageId): void {
-        Http::post('https://api.telegram.org/bot' . $this->token . '/deleteMessage', [
+        Http::post(self::TELEGRAM_API_URL . $this->token . '/deleteMessage', [
             'chat_id'    => $this->chatId,
             'message_id' => $messageId,
         ]);
