0e00b7db173aa61521140b9bb25544e8a1172cb7
Kris <git@k118.de>
2024-07-31 13:20:51 +0200

    :sparkles: remove closed reports from backend chat (#2802)

--- app/Http/Controllers/FrontendStatusController.php
+++ app/Http/Controllers/FrontendStatusController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Backend\EventController as EventBackend;
-use App\Http\Controllers\Backend\Support\LocationController;
 use App\Http\Controllers\Backend\Transport\StationController;
 use App\Http\Controllers\Backend\User\DashboardController;
 use App\Http\Controllers\Backend\User\ProfilePictureController;
@@ -13,7 +11,6 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
 
 /**
@@ -25,18 +22,18 @@ class FrontendStatusController extends Controller
         $statuses = DashboardController::getPrivateDashboard(auth()->user());
 
         return view('dashboard', [
-            'statuses' => $statuses,
-            'latest'   => StationController::getLatestArrivals(auth()->user()),
-            'future'   => StatusBackend::getFutureCheckins(),
+            'statuses'         => $statuses,
+            'latest'           => StationController::getLatestArrivals(auth()->user()),
+            'future'           => StatusBackend::getFutureCheckins(),
             'showGlobalButton' => auth()->user()->follows->count() === 0
         ]);
     }
 
     public function getGlobalDashboard(): Renderable {
         return view('dashboard', [
-            'statuses' => DashboardController::getGlobalDashboard(Auth::user()),
-            'latest'   => StationController::getLatestArrivals(Auth::user()),
-            'future'   => StatusBackend::getFutureCheckins(),
+            'statuses'         => DashboardController::getGlobalDashboard(Auth::user()),
+            'latest'           => StationController::getLatestArrivals(Auth::user()),
+            'future'           => StatusBackend::getFutureCheckins(),
             'showGlobalButton' => false
         ]);
     }
