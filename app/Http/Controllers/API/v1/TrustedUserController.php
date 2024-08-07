c7af134473721e2afaf507a0f35aa49c8ba7221a
Kristian Stöckel <git@k118.de>
2024-08-07 15:57:04 +0200

    no message

--- app/Http/Controllers/API/v1/TrustedUserController.php
+++ app/Http/Controllers/API/v1/TrustedUserController.php
@@ -55,7 +55,6 @@ class TrustedUserController extends Controller
      *     ),
      *     @OA\Response(response="401", description="Unauthorized"),
      *     @OA\Response(response="403", description="Forbidden"),
-     *     @OA\Response(response="404", description="User not found"),
      *     @OA\Response(response="500", description="Internal Server Error"),
      * )
      * @throws AuthorizationException
