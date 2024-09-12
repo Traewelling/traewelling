07fb9840ae34e1bd106e328628e89af2c221d5b5
Levin Herr <github@17d.me>
2024-09-12 18:42:01 +0200

    ✨ Better sorting of the stations in the trip creation interface (#2910)

--- app/Repositories/StationRepository.php
+++ app/Repositories/StationRepository.php
@@ -12,9 +12,14 @@ class StationRepository
     public function getStationByName(string $name, string $lang, bool $invertLanguage = false): Collection {
         $invertLanguage = $invertLanguage ? '!=' : '=';
         return Station::leftJoin('station_names', 'station_names.station_id', '=', 'train_stations.id')
-                      ->where('station_names.name', 'LIKE', "%$name%")
+                      ->where('station_names.name', 'LIKE', "$name%")
                       ->where('station_names.language', $invertLanguage, $lang)
-                      ->orWhere('train_stations.name', 'LIKE', "%$name%")
+                      ->orWhere('train_stations.name', 'LIKE', "$name%")
+                      ->orWhere(function($query) use ($name, $invertLanguage, $lang) {
+                          $query->where('station_names.name', 'LIKE', "%$name%")
+                                ->where('station_names.language', $invertLanguage, $lang)
+                                ->orWhere('train_stations.name', 'LIKE', "%$name%");
+                      })
                       ->select('train_stations.*')
                       ->distinct()
                       ->limit(20)
