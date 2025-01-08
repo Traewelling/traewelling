b616bbc1423dc1c26d6fb86731c9ee4dd1c94989
Kristian Stöckel <git@k118.de>
2025-01-08 19:43:46 +0100

    no message
    Co-authored-by: Levin <github@17d.me>

--- app/Http/Controllers/Backend/Transport/BahnWebApiController.php
+++ app/Http/Controllers/Backend/Transport/BahnWebApiController.php
@@ -13,16 +13,15 @@ use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 
-abstract class BahnWebApiController extends Controller
-{
+abstract class BahnWebApiController extends Controller {
 
     public static function searchStation(string $query, int $limit = 10): Collection {
         $url      = "https://www.bahn.de/web/api/reiseloesung/orte?suchbegriff=" . urlencode($query) . "&typ=ALL&limit=" . $limit;
         $response = Http::get($url);
         $json     = $response->json();
         $extIds   = [];
-        foreach ($json as $rawStation) {
-            if (!isset($rawStation['extId'])) {
+        foreach($json as $rawStation) {
+            if(!isset($rawStation['extId'])) {
                 continue;
             }
             $extIds[] = $rawStation['extId'];
@@ -30,12 +29,12 @@ abstract class BahnWebApiController extends Controller
         $stationCache = Station::whereIn('ibnr', $extIds)->get();
 
         $stations = collect();
-        foreach ($json as $rawStation) {
-            if (!isset($rawStation['extId'])) {
+        foreach($json as $rawStation) {
+            if(!isset($rawStation['extId'])) {
                 continue;
             }
             $station = $stationCache->where('ibnr', $rawStation['extId'])->first();
-            if ($station === null) {
+            if($station === null) {
                 $station = Station::create([
                                                'name'      => $rawStation['name'],
                                                'latitude'  => $rawStation['lat'],
@@ -52,7 +51,7 @@ abstract class BahnWebApiController extends Controller
 
     public static function getDepartures(Station $station, Carbon|null $timestamp = null): Collection {
         $timezone = "Europe/Berlin";
-        if ($timestamp === null) {
+        if($timestamp === null) {
             $timestamp = now();
         }
         $timestamp->tz($timezone);
@@ -62,9 +61,9 @@ abstract class BahnWebApiController extends Controller
             'zeit'     => $timestamp->format('H:i'),
         ]);
         $departures = collect();
-        foreach ($response->json('entries') as $rawDeparture) {
+        foreach($response->json('entries') as $rawDeparture) {
             $journey = Trip::where('trip_id', $rawDeparture['journeyId'])->first();
-            if ($journey) {
+            if($journey) {
                 $departures->push(new Departure(
                                       station:          $station,
                                       plannedDeparture: Carbon::parse($rawDeparture['zeit'], $timezone),
@@ -75,13 +74,14 @@ abstract class BahnWebApiController extends Controller
             }
 
             $rawJourney = self::fetchJourney($rawDeparture['journeyId']);
-            if ($rawJourney === null) {
+            if($rawJourney === null) {
                 // sorry
                 continue;
             }
+            $stopoverCacheFromDB = Station::whereIn('ibnr', collect($rawJourney['halte'])->pluck('extId'))->get();
 
-            $originStation      = self::getStationFromHalt($rawJourney['halte'][0]);
-            $destinationStation = self::getStationFromHalt($rawJourney['halte'][count($rawJourney['halte']) - 1]);
+            $originStation      = $stopoverCacheFromDB->where('ibnr', $rawJourney['halte'][0]['extId'])->first() ?? self::getStationFromHalt($rawJourney['halte'][0]);
+            $destinationStation = $stopoverCacheFromDB->where('ibnr', $rawJourney['halte'][count($rawJourney['halte']) - 1]['extId'])->first() ?? self::getStationFromHalt($rawJourney['halte'][count($rawJourney['halte']) - 1]);
             $departure          = isset($rawJourney['halte'][0]['abfahrtsZeitpunkt']) ? Carbon::parse($rawJourney['halte'][0]['abfahrtsZeitpunkt'], $timezone) : null;
             $arrival            = isset($rawJourney['halte'][count($rawJourney['halte']) - 1]['ankunftsZeitpunkt']) ? Carbon::parse($rawJourney['halte'][count($rawJourney['halte']) - 1]['ankunftsZeitpunkt'], $timezone) : null;
             $category           = isset($rawDeparture['verkehrmittel']['produktGattung']) ? ReiseloesungCategory::tryFrom($rawDeparture['verkehrmittel']['produktGattung']) : ReiseloesungCategory::UNKNOWN;
@@ -107,9 +107,10 @@ abstract class BahnWebApiController extends Controller
                                         'source'         => TripSource::BAHN_WEB_API,
                                     ]);
 
+
             $stopovers = collect();
             foreach($rawJourney['halte'] as $rawHalt) {
-                $station = self::getStationFromHalt($rawHalt);
+                $station = $stopoverCacheFromDB->where('ibnr', $rawHalt['extId'])->first() ?? self::getStationFromHalt($rawHalt);
 
                 $departurePlanned = isset($rawHalt['abfahrtsZeitpunkt']) ? Carbon::parse($rawHalt['abfahrtsZeitpunkt'], $timezone) : null;
                 $departureReal    = isset($rawHalt['ezAbfahrtsZeitpunkt']) ? Carbon::parse($rawHalt['ezAbfahrtsZeitpunkt'], $timezone) : null;
@@ -138,10 +139,10 @@ abstract class BahnWebApiController extends Controller
     }
 
     private static function getStationFromHalt(array $rawHalt) {
-        $station = Station::where('ibnr', $rawHalt['extId'])->first();
-        if ($station !== null) {
-            return $station;
-        }
+        //$station = Station::where('ibnr', $rawHalt['extId'])->first();
+        //if($station !== null) {
+        //    return $station;
+        // }
 
         //urgh, there is no lat/lon - extract it from id
         // example id: A=1@O=Druseltal, Kassel@X=9414484@Y=51301106@U=81@L=714800@
@@ -150,13 +151,14 @@ abstract class BahnWebApiController extends Controller
         $latitude  = $matches[2] / 1000000;
         $longitude = $matches[1] / 1000000;
 
-        return Station::create([
-                                   'name'      => $rawHalt['name'],
-                                   'latitude'  => $latitude ?? 0, // Hello Null-Island
-                                   'longitude' => $longitude ?? 0, // Hello Null-Island
-                                   'ibnr'      => $rawHalt['extId'],
-                                   'source'    => TripSource::BAHN_WEB_API->value,
-                               ]);
+        return Station::updateOrCreate([
+                                           'ibnr' => $rawHalt['extId'],
+                                       ], [
+                                           'name'      => $rawHalt['name'],
+                                           'latitude'  => $latitude ?? 0, // Hello Null-Island
+                                           'longitude' => $longitude ?? 0, // Hello Null-Island
+                                           'source'    => TripSource::BAHN_WEB_API->value,
+                                       ]);
     }
 
     public static function fetchJourney(string $journeyId, bool $poly = false): array|null {
