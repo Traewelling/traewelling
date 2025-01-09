31ddfb59c744fece478e655368ba58e232991165
Gregor Wehrle <171450664+gwehrle@users.noreply.github.com>
2025-01-09 15:17:04 +0100

    Add minlength to frontend validation in register form for password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -77,7 +77,7 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                            class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="new-password">
+                                           required autocomplete="new-password" minlength="8">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -93,7 +93,7 @@
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
-                                           name="password_confirmation" required autocomplete="new-password">
+                                           name="password_confirmation" required autocomplete="new-password" minlength="8">
                                 </div>
                             </div>
 
