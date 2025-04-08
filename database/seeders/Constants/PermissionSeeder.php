20dc618445b62e19388e06fc94cdeac4e6ce8a1d
Kristian Stöckel <git@k118.de>
2025-04-08 14:23:02 +0200

    cleanup seeder

--- database/seeders/Constants/PermissionSeeder.php
+++ database/seeders/Constants/PermissionSeeder.php
@@ -20,7 +20,7 @@ class PermissionSeeder extends Seeder
         $roleClosedBeta             = Role::updateOrCreate(['name' => 'closed-beta']);
         $roleDisallowManualTrips    = Role::updateOrCreate(['name' => 'disallow-manual-trips']);
         $roleDeactivateAccountUsage = Role::updateOrCreate(['name' => 'deactivate-account-usage']);
-        $roleTestGdprExport         = Role::updateOrCreate(['name' => 'test-gdpr-export']);                      //TODO: remove this permission when GDPR export is no longer in testing
+        Role::updateOrCreate(['name' => 'test-gdpr-export']);                                                    //TODO: remove this permission when GDPR export is no longer in testing
 
         //Create permissions
         $permissionViewBackend                    = Permission::updateOrCreate(['name' => 'view-backend']);
