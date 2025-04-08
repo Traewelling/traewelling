f9ce343953bdef063eaf33c9847b027387414339
Kristian Stöckel <git@k118.de>
2025-04-08 14:34:04 +0200

    no message

--- app/Services/PersonalDataSelection/UserGdprDataService.php
+++ app/Services/PersonalDataSelection/UserGdprDataService.php
@@ -41,47 +41,47 @@ class UserGdprDataService
 
         $personalDataSelection
             ->add('user.json', $userData)
-            ->add('notifications.json', $userModel->notifications()->get()->toJson()) //TODO: columns definieren
-            ->add('likes.json', $userModel->likes()->get()->toJson()) //TODO: columns definieren
-            ->add('social_profile.json', $userModel->socialProfile()->with('mastodonserver')->get()) //TODO: columns definieren
-            ->add('event_suggestions.json', EventSuggestion::where('user_id', $userModel->id)->get()->toJson()) //TODO: columns definieren
-            ->add('events.json', Event::where('approved_by', $userModel->id)->get()->toJson()) //TODO: columns definieren
+            ->add('notifications.json', $userModel->notifications()->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('likes.json', $userModel->likes()->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('social_profile.json', $userModel->socialProfile()->with('mastodonserver')->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('event_suggestions.json', EventSuggestion::where('user_id', $userModel->id)->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('events.json', Event::where('approved_by', $userModel->id)->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
             ->add('webhooks.json', $webhooks)
             ->add(
                 'webhook_creation_requests.json',
-                WebhookCreationRequest::where('user_id', $userModel->id)->get()->toJson() //TODO: columns definieren
+                WebhookCreationRequest::where('user_id', $userModel->id)->get()->toJson() //TODO before leaving test-phase: columns definieren per resource
             )
-            ->add('tokens.json', TokenController::index($userModel)->toJson()) //TODO: columns definieren
-            ->add('ics_tokens.json', $userModel->icsTokens()->get()->toJson())                                          //TODO: columns definieren
+            ->add('tokens.json', TokenController::index($userModel)->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('ics_tokens.json', $userModel->icsTokens()->get()->toJson())                                          //TODO before leaving test-phase: columns definieren per resource
             ->add(
                 'password_resets.json',
-                DB::table('password_resets')->select(['email', 'created_at'])->where('email', $userModel->email)->get() //TODO: columns definieren
+                DB::table('password_resets')->select(['email', 'created_at'])->where('email', $userModel->email)->get() //TODO before leaving test-phase: columns definieren per resource
             )
-            ->add('apps.json', $userModel->oAuthClients()->get()->toJson()) //TODO: columns definieren
-            ->add('follows.json', DB::table('follows')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('followings.json', DB::table('follows')->where('follow_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('blocks.json', DB::table('user_blocks')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('mutes.json', DB::table('user_mutes')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('follow_requests.json', DB::table('follow_requests')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('follows_requests.json', DB::table('follow_requests')->where('follow_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('sessions.json', $userModel->sessions()->get()->toJson()) //TODO: columns definieren
-            ->add('home.json', $userModel->home()->get()->toJson()) //TODO: columns definieren
-            ->add('hafas_trips.json', DB::table('hafas_trips')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
-            ->add('mentions.json', Mention::where('mentioned_id', $userModel->id)->get()->toJson()) //TODO: columns definieren
-            ->add('roles.json', $userModel->roles()->get()->toJson())                                                             //TODO: columns definieren
+            ->add('apps.json', $userModel->oAuthClients()->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('follows.json', DB::table('follows')->where('user_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('followings.json', DB::table('follows')->where('follow_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('blocks.json', DB::table('user_blocks')->where('user_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('mutes.json', DB::table('user_mutes')->where('user_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('follow_requests.json', DB::table('follow_requests')->where('user_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('follows_requests.json', DB::table('follow_requests')->where('follow_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('sessions.json', $userModel->sessions()->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('home.json', $userModel->home()->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('hafas_trips.json', DB::table('hafas_trips')->where('user_id', $userModel->id)->get()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('mentions.json', Mention::where('mentioned_id', $userModel->id)->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('roles.json', $userModel->roles()->get()->toJson())                                                             //TODO before leaving test-phase: columns definieren per resource
             ->add(
                 'activity_log.json',
-                DB::table('activity_log')->where('causer_type', get_class($userModel))->where('causer_id', $userModel->id)->get() //TODO: columns definieren
+                DB::table('activity_log')->where('causer_type', get_class($userModel))->where('causer_id', $userModel->id)->get() //TODO before leaving test-phase: columns definieren per resource
             )
-            ->add('permissions.json', $userModel->permissions()->get()->toJson()) //TODO: columns definieren
-            ->add('statuses.json', $userModel->statuses()->with('tags')->get()) //TODO: columns definieren
+            ->add('permissions.json', $userModel->permissions()->get()->toJson()) //TODO before leaving test-phase: columns definieren per resource
+            ->add('statuses.json', $userModel->statuses()->with('tags')->get()) //TODO before leaving test-phase: columns definieren per resource
             ->add(
                 'reports.json',
                 DB::table('reports')
                   ->select('subject_type', 'subject_id', 'reason', 'description', 'reporter_id')
                   ->where('reporter_id', $userModel->id)
-                  ->get()                                                       //TODO: columns definieren
+                  ->get()                                                       //TODO before leaving test-phase: columns definieren per resource
             )
-            ->add('trusted_users.json', DB::table('trusted_users')->where('user_id', $userModel->id)->get()); //TODO: columns definieren
+            ->add('trusted_users.json', DB::table('trusted_users')->where('user_id', $userModel->id)->get()); //TODO before leaving test-phase: columns definieren per resource
     }
 }
