9dccb5bed570f69639b3608d9ed38a213606cb60
Levin <github@17d.me>
2025-04-11 18:10:42 +0200

    Fetch license from transitous directly

--- app/Console/Commands/FetchTransitousLicenses.php
+++ app/Console/Commands/FetchTransitousLicenses.php
@@ -3,139 +3,73 @@
 namespace App\Console\Commands;
 
 use App\Models\MotisSourceLicense;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Storage;
-use ZipArchive;
 
 class FetchTransitousLicenses extends Command
 {
-    private const string REPO = 'https://github.com/public-transport/transitous/archive/refs/heads/main.zip';
+    private const string LICENSE_URL = 'https://api.transitous.org/gtfs/license.json';
     private const string PATH = 'tmp/transitous';
 
-    protected $signature   = 'trwl:fetch-transitous-licenses';
+    protected $signature = 'trwl:fetch-transitous-licenses';
     protected $description = 'Fetch License data from transitous repository';
 
-    public function handle(): int {
-        // fetch the git repository https://github.com/public-transport/transitous.git
-        // and import all licenses from the folder feeds into the database
-        $return = self::SUCCESS;
-        if ($this->cloneRepo() !== self::SUCCESS || $this->parseLicenses() !== self::SUCCESS) {
-            $this->error('Failed to parse licenses');
-            $return = self::FAILURE;
-        }
-
-        // Delete the repository
-        if ($this->deleteData() !== self::SUCCESS) {
-            $this->error('Failed to delete repository');
-            $return = self::FAILURE;
-        }
-
-        $this->info('Done');
-        return $return;
+    public function handle(): int
+    {
+        return $this->parseLicenses();
     }
 
-    private function cloneRepo(): int {
-        $this->info('Cloning repository...');
-
-        // Download the zip file
-        $this->info('Downloading repository...');
-        $response = Http::get(self::REPO);
+    private function getLicenseJson(): array
+    {
+        $this->info('Downloading licenses...');
+        $response = Http::get(self::LICENSE_URL);
         if (empty($response) || $response->status() !== 200) {
-            $this->error('Failed to download repository');
-            return self::FAILURE;
+            $this->error('Failed to download licenses');
+            return [];
         }
-        // Save the zip file
-        Storage::disk('local')->put(self::PATH . '/transitous.zip', $response->body());
-        $zipFile = $this->getStoragePath('transitous.zip');
-        $zipper  = new ZipArchive();
-        if ($zipper->open($zipFile) === true) {
-            $zipper->extractTo($this->getStoragePath());
-            $zipper->close();
-        } else {
-            $this->error('Failed to unzip repository');
-            return self::FAILURE;
+        try {
+            return json_decode($response->body(), true);
+        } catch (Exception $e) {
+            $this->error('Failed to decode licenses: ' . $e->getMessage());
+            return [];
         }
-
-        // Delete the zip file
-        Storage::disk('local')->delete(self::PATH . '/transitous.zip');
-
-        $this->info('Repository cloned successfully');
-        return self::SUCCESS;
     }
 
-    private function getStoragePath(?string $path = null): string {
-        $storage = Storage::disk('local')->path(self::PATH);
-
-        return $path ? $storage . '/' . $path : $storage;
-    }
-
-    private function parseLicenses(): int {
+    private function parseLicenses(): int
+    {
         $this->info('Reading licenses...');
-        $files = glob($this->getStoragePath('transitous-main/feeds/*'));
+        $licenses = $this->getLicenseJson();
 
-        $errors = 0;
-        foreach ($files as $file) {
-            $content = file_get_contents($file);
-            $content = $content ? json_decode($content, true) : [];
-            $country = basename($file, '.json');
-            if (empty($content)) {
-                $this->error('Failed to read file: ' . $file);
-                $errors++;
-                continue;
-            }
+        foreach ($licenses as $license) {
+            $country = $license['region_code'];
+            $spdx = $license['spdx_license_identifier'] ?? $license['rt_spdx_license_identifier'] ?? '';
+            $licenseUrl = $license['license_url'] ?? '';
+            $name = $license['filename'];
+            $humanName = $license['human_name'] ?? null;
+            $source = $license['source_url'] ?? '';
+            $active = array_key_exists($spdx, MotisSourceLicense::SPDX);
 
-            $this->info('Reading licenses for country: ' . $country);
-            foreach ($content['sources'] as $license) {
-                $tmp = [
-                    'name'        => $license['name'],
-                    'url'         => $license['url'] ?? '',
-                    'license_url' => $license['license']['url'] ?? '',
-                    'version'     => $license['version'] ?? '',
-                    'type'        => $license['type'] ?? '',
-                    'spdx'        => $license['license']['spdx-identifier'] ?? '',
-                    'active'      => array_key_exists($license['license']['spdx-identifier'] ?? '', MotisSourceLicense::SPDX),
-                ];
-                $this->info(
-                    sprintf('[%s] Found license: %s (%s) %s', $country, $tmp['name'], $tmp['spdx'], $tmp['active'] ? 'active' : 'inactive')
-                );
+            $this->info(
+                sprintf('[%s] Found license: %s (%s) %s', $country, $name, $spdx, $active ? 'active' : 'inactive')
+            );
 
-                MotisSourceLicense::updateOrCreate(
-                    [
-                        'provider' => 'transitous',
-                        'country'  => $country,
-                        'name'     => $tmp['name'],
-                    ],
-                    [
-                        'license_url' => $tmp['license_url'],
-                        'source_url'  => $tmp['url'],
-                        'spdx'        => $tmp['spdx'],
-                        'license'     => $tmp['type'],
-                        'active'      => $tmp['active'],
-                    ]
-                );
-            }
+            MotisSourceLicense::updateOrCreate(
+                [
+                    'provider' => 'transitous',
+                    'country' => $country,
+                    'name' => $name,
+                ],
+                [
+                    'license_url' => $licenseUrl,
+                    'source_url' => $source,
+                    'spdx' => $spdx,
+                    'human_name' => $humanName,
+                    'active' => $active,
+                ]
+            );
         }
-
-        if ($errors == count($files)) {
-            $this->error('Failed to read some files');
-            return self::FAILURE;
-        } elseif ($errors > 0) {
-            $this->info('Some licenses read successfully');
-        }
-
 
         return self::SUCCESS;
-    }
-
-    private function deleteData(): int {
-        $this->info('Deleting repository...');
-        if (Storage::disk('local')->deleteDirectory(self::PATH)) {
-            $this->info('Repository deleted successfully');
-            return self::SUCCESS;
-        }
-
-        $this->error('Failed to delete repository');
-        return self::FAILURE;
     }
 }
---
+++ database/migrations/2025_04_11_155304_add_human_name_to_motis_licenses.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void {
+        Schema::table('motis_source_licenses', function(Blueprint $table) {
+            $table->string('human_name')->nullable()->after('name');
+        });
+    }
+
+    public function down(): void {
+        Schema::table('motis_source_licenses', function(Blueprint $table) {
+            $table->dropColumn('human_name');
+        });
+    }
+};
