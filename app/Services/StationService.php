2f626761b44c1fb635447b107b7100182b965121
Kris <git@k118.de>
2025-04-15 16:23:21 +0200

    :arrow_up: show nearby stations in admin panel (#3389)

---
+++ app/Services/StationService.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Models\Station;
+use Illuminate\Support\Collection;
+
+abstract class StationService
+{
+
+    public static function getNearbyStations(Station $station, $maxDistanceKilometers = 10, int $limit = 15): Collection {
+        $latitude  = $station->latitude;
+        $longitude = $station->longitude;
+
+        $latDelta = $maxDistanceKilometers / 111;  // ca. 1° ≈ 111 km
+        $lngDelta = $maxDistanceKilometers / abs(cos(deg2rad($latitude)) * 111);
+
+        $minLat = $latitude - $latDelta;
+        $maxLat = $latitude + $latDelta;
+        $minLng = $longitude - $lngDelta;
+        $maxLng = $longitude + $lngDelta;
+
+        $earthRadius = 6371;
+        return Station::selectRaw("*, (
+    $earthRadius * acos(
+        cos(radians(?)) * cos(radians(latitude)) *
+        cos(radians(longitude) - radians(?)) +
+        sin(radians(?)) * sin(radians(latitude))
+    )
+) AS distance", [$latitude, $longitude, $latitude])
+                      ->whereBetween('latitude', [$minLat, $maxLat])
+                      ->whereBetween('longitude', [$minLng, $maxLng])
+                      ->where('id', '!=', $station->id)
+                      ->orderBy('distance')
+                      ->limit($limit)
+                      ->get();
+    }
+
+}
