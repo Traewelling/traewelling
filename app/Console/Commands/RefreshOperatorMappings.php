ff5245c65a69d3d161a92925202a871594d42766
Kris <git@k118.de>
2025-04-15 23:17:19 +0200

    :bug: compare by id and name (#3401)

--- app/Console/Commands/RefreshOperatorMappings.php
+++ app/Console/Commands/RefreshOperatorMappings.php
@@ -5,11 +5,11 @@ namespace App\Console\Commands;
 use App\Services\OperatorService;
 use Illuminate\Console\Command;
 
-class RefreshOperatorMappings extends Command
-{
+class RefreshOperatorMappings extends Command {
     protected $signature = 'app:refresh-operator-mappings';
 
     public function handle(): void {
-        OperatorService::refreshFiles();
+        $operatorService = new OperatorService();
+        $operatorService->refreshFiles();
     }
 }
