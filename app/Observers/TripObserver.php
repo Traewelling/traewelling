4b3c3749b5c6224b70efc0acc322b6db62a0dbba
Levin <github@17d.me>
2025-05-01 16:10:34 +0200

    only report user trips

--- app/Observers/TripObserver.php
+++ app/Observers/TripObserver.php
@@ -3,20 +3,20 @@
 namespace App\Observers;
 
 use App\Enum\Report\ReportableSubject;
-use App\Enum\Report\ReportReason;
+use App\Enum\TripSource;
 use App\Models\Trip;
-use App\Repositories\ReportRepository;
 use App\Services\ReportService;
-use Illuminate\Support\Facades\Log;
 
 class TripObserver
 {
     public function created(Trip $trip): void {
         // check if trip is out of allowed types and create an admin report if so
-        (new ReportService())->checkAndReport(
-            $trip->linename,
-            ReportableSubject::TRIP,
-            $trip->id
-        );
+        if ($trip->source === TripSource::USER) {
+            (new ReportService())->checkAndReport(
+                $trip->linename,
+                ReportableSubject::TRIP,
+                $trip->id
+            );
+        }
     }
 }
