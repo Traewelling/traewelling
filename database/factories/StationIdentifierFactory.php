158ea195c4b551bf0b647603d29ea687d8950f17
Levin Herr <github@17d.me>
2025-05-03 13:39:05 +0200

    ✅ Add MotisHydratorTest (#3481)

---
+++ database/factories/StationIdentifierFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enum\TripSource;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StationIdentifierFactory extends Factory
+{
+    public function definition(): array {
+        return [
+            'relevance'  => $this->faker->numberBetween(0, 100),
+            'station_id' => $this->faker->numberBetween(1, 100),
+            'identifier' => $this->faker->uuid,
+            'type'       => $this->faker->randomElement(['motis']),
+            'origin'     => $this->faker->randomElement([TripSource::TRANSITOUS]),
+            'name'       => $this->faker->city,
+        ];
+    }
+}
