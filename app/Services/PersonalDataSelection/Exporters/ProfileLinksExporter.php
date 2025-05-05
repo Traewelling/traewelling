ff8d5feedac4394493c21ae2f8cb1e4912b95f09
Levin <github@17d.me>
2025-05-06 00:56:35 +0200

    gdpr export

---
+++ app/Services/PersonalDataSelection/Exporters/ProfileLinksExporter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\PersonalDataSelection\Exporters;
+
+use App\Services\PersonalDataSelection\Exporters\Base\AbstractExporter;
+use App\Services\PersonalDataSelection\Exporters\Base\RelationExportable;
+
+class ProfileLinksExporter extends AbstractExporter
+{
+    use RelationExportable;
+
+    protected string $fileName = 'profile_links.json';
+    protected string $relation = 'profileLinks';
+    // todo: columns
+}
