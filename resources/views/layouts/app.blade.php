46aea5780db347d26d21fd53aa9d80d69ece9c12
Kristian Stöckel <git@k118.de>
2025-05-25 20:06:06 +0200

    :recycle: remove duplicate viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark" data-bs-theme="dark">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>@yield('title') - {{ config('app.name', 'Träwelling') }}</title>
 
     @include('layouts.includes.meta')
