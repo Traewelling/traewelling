702c6aab50f23419bc8dcb8365b79a030814c744
Levin <github@17d.me>
2025-07-18 13:13:12 +0200

    Fix factory

--- database/factories/OperatorFactory.php
+++ database/factories/OperatorFactory.php
@@ -3,15 +3,13 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class OperatorFactory extends Factory
 {
     public function definition(): array {
         $companyName = $this->faker->company;
         return [
-            'hafas_id' => Str::slug($companyName . $this->faker->hexColor, '_'),
-            'name'     => $companyName,
+            'name' => $companyName,
         ];
     }
 }
