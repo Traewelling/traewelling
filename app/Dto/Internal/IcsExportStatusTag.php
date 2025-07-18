010d0eb580822cd0e84a6b4b50b5bf251f04e8b8
Levin <github@17d.me>
2025-07-18 15:18:12 +0200

    Cache ics export

---
+++ app/Dto/Internal/IcsExportStatusTag.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Dto\Internal;
+
+readonly class IcsExportStatusTag
+{
+    public function __construct(
+        public string $key,
+        public string $value,
+    ) {
+    }
+}
