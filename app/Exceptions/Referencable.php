bff2247928d3321e6277b1cd618e586b9046cc75
Kristian Stöckel <git@k118.de>
2025-11-06 14:13:01 +0100

    :adhesive_bandage: fix constructor type hint for reference parameter

--- app/Exceptions/Referencable.php
+++ app/Exceptions/Referencable.php
@@ -9,7 +9,7 @@ class Referencable extends Exception
     public readonly string $reference;
     protected array        $context = [];
 
-    public function __construct(string $message = "", string $reference = null) {
+    public function __construct(string $message = "", ?string $reference = null) {
         $this->reference = $reference ?? uniqid();
         parent::__construct($message);
     }
