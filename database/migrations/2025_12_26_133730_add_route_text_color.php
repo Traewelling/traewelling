7f8f7b57af56d576cde86db195feb22ae2290c45
wittighausen <ichhabdiamanten@gmail.com>
2025-12-26 13:38:42 +0100

    add migration file

---
+++ database/migrations/2025_12_26_133730_add_route_text_color.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void	{
+        Schema::table('hafas_trips', function (Blueprint $table) {
+            $table->string('route_text_color', 6)->nullable()->after('color')->comment('Hex text color code of the route, without #');
+        });
+    }
+
+    public function down(): void	{
+        Schema::table('hafas_trips', function (Blueprint $table) {
+            $table->dropColumn('route_text_color');
+        });
+    }
+};
