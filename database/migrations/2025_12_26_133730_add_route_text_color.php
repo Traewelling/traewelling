a2238e75367f242a7f29517055389263457d014a
Kristian Stöckel <git@k118.de>
2025-12-26 21:27:29 +0100

    :adhesive_bandage: fix creation of `route_text_color` column

--- database/migrations/2025_12_26_133730_add_route_text_color.php
+++ database/migrations/2025_12_26_133730_add_route_text_color.php
@@ -6,14 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void	{
-        Schema::table('hafas_trips', function (Blueprint $table) {
-            $table->string('route_text_color', 6)->nullable()->after('color')->comment('Hex text color code of the route, without #');
+    public function up(): void {
+        Schema::table('hafas_trips', function(Blueprint $table) {
+            $table->string('route_text_color', 6)
+                  ->nullable()
+                  ->after('route_color')
+                  ->comment('Hex text color code of the route, without #');
         });
     }
 
-    public function down(): void	{
-        Schema::table('hafas_trips', function (Blueprint $table) {
+    public function down(): void {
+        Schema::table('hafas_trips', function(Blueprint $table) {
             $table->dropColumn('route_text_color');
         });
     }
