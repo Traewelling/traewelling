1a8530461f2b391c503d7647427eb6952211241a
Varox <c.e.w658@gmail.com>
2025-12-27 01:03:14 +0100

    Fixed id/uuid error

--- database/migrations/2025_12_21_000000_create_status_hidden_users_table.php
+++ database/migrations/2025_12_21_000000_create_status_hidden_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('status_hidden_users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignId('status_id')->constrained('statuses')->cascadeOnDelete();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
