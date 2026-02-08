bba41b8f31ad6fe4925040fd430db95e9dfe51cc
Kristian Stöckel <git@k118.de>
2026-02-08 11:42:24 +0100

    :zap: Improve CheckinSeeder
    838ms instead of 433300ms

--- database/seeders/CheckinSeeder.php
+++ database/seeders/CheckinSeeder.php
@@ -2,39 +2,47 @@
 
 namespace Database\Seeders;
 
-use App\Dto\Internal\CheckInRequestDto;
-use App\Http\Controllers\Backend\Transport\TrainCheckinController;
+use App\Models\Checkin;
 use App\Models\Event;
 use App\Models\StatusTag;
 use App\Models\Trip;
 use App\Models\User;
-use Exception;
 use Illuminate\Database\Seeder;
 
 class CheckinSeeder extends Seeder
 {
     public function run(): void
     {
+        $users = User::all();
+        $trips = Trip::with('stopovers')->get();
+        $events = Event::all();
+
         for ($i = 0; $i < 10; $i++) {
-            $user = User::inRandomOrder()->first();
-            $trip = Trip::inRandomOrder()->first();
+            $user = $users->random();
+            $trip = $trips->random();
 
-            $dto = new CheckInRequestDto();
-            $dto->setUser($user)
-                ->setTrip($trip)
-                ->setOrigin($trip->originStation)// Checkin from the first station...
-                ->setDeparture($trip->departure)
-                ->setDestination($trip->destinationStation)// ...to the last station
-                ->setArrival($trip->arrival)
-                ->setEvent(random_int(0, 1) ? Event::inRandomOrder()->first() : null);
+            $originStopover = $trip->stopovers->where('train_station_id', $trip->originStation->id)->first();
+            $destinationStopover = $trip->stopovers->where('train_station_id', $trip->destinationStation->id)->first();
 
-            try {
-                $checkinResponse = TrainCheckinController::checkin($dto);
-                $status = $checkinResponse->status;
-                StatusTag::factory(['status_id' => $status->id])->create();
-            } catch (Exception) {
+            if ($originStopover === null || $destinationStopover === null) {
                 continue;
             }
+
+            $checkin = Checkin::factory()
+                ->for($user)
+                ->create([
+                    'trip_id'                  => $trip->trip_id,
+                    'origin_stopover_id'       => $originStopover->id,
+                    'destination_stopover_id'  => $destinationStopover->id,
+                    'departure'                => $trip->departure,
+                    'arrival'                  => $trip->arrival,
+                ]);
+
+            $checkin->status->update([
+                'event_id' => random_int(0, 1) ? $events->random()->id : null,
+            ]);
+
+            StatusTag::factory(['status_id' => $checkin->status_id])->create();
         }
     }
 }
