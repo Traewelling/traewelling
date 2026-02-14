b4c337c47e18efecd365c0572224a2925575ffcf
Kristian Stöckel <git@k118.de>
2026-02-14 15:13:14 +0100

    fix title

--- resources/views/contribute.blade.php
+++ resources/views/contribute.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ trans('contribute.title') }} - Träwelling</title>
+    <title>{{ trans('contribute') }} - Träwelling</title>
 
     <!-- Dark Mode Script -->
     <script>
