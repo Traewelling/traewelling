2ffc914db664ac02bc75161f0b7500e84463464b
Kristian Stöckel <git@k118.de>
2026-02-15 12:42:16 +0100

    Remove role middleware from contribute route group

--- routes/web.php
+++ routes/web.php
@@ -147,7 +147,7 @@ Route::middleware(['auth', 'privacy'])->group(function () {
             ->name('open-data.wikidata');
     });
 
-    Route::prefix('contribute')->middleware(['role:open-beta|admin'])->group(function () {
+    Route::prefix('contribute')->group(function () {
         Route::get('/{any?}', function () {
             return view('contribute');
         })->where('any', '.*')->name('contribute');
