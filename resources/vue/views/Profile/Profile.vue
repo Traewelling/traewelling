06ab150b41a63e278ef4a6b7ed5efa2926685944
Levin <github@17d.me>
2026-02-19 14:00:52 +0100

    Move statuses to partial
    Signed-off-by: Levin <github@17d.me>

--- resources/vue/views/Profile/Profile.vue
+++ resources/vue/views/Profile/Profile.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { trans } from 'laravel-vue-i18n';
-import { DateTime, Duration } from 'luxon';
+import { Duration } from 'luxon';
 import { Notyf } from 'notyf';
 import { computed, ref } from 'vue';
-import { Api, StatusResource, StopoverResource, UserResource } from '../../types/Api.gen';
-import LoadingSkeletonRows from '../components/Loader/LoadingSkeletonRows.vue';
-import StatusCard from '../components/Status/StatusCard.vue';
-import { getDepartureForStatus } from '../helpers/DateTimeHelper';
-import { IconHelper } from '../helpers/IconHelper';
-import { useUserStore } from '../stores/user';
+import { Api, StatusResource, StopoverResource, UserResource } from '../../../types/Api.gen';
+import LoadingSkeletonRows from '../../components/Loader/LoadingSkeletonRows.vue';
+import { IconHelper } from '../../helpers/IconHelper';
+import { useUserStore } from '../../stores/user';
+import Statuses from './partials/Statuses.vue';
 
 const props = defineProps<{ username: string }>();
 
@@ -94,30 +93,6 @@ async function fetchStopovers() {
     } catch (err) {
         console.error('Stopovers error', err);
     }
-}
-
-function getStopoverForTrip(tripId: string) {
-    return stopovers.value[tripId];
-}
-
-const isOwnProfile = computed(() => {
-    return authUser && authUser.getId === userData.value?.id;
-});
-
-function isNewDay(index: number): boolean {
-    if (index === 0) return true;
-    const prevDt = getDepartureForStatus(statuses.value[index - 1]).dateTime;
-    const currDt = getDepartureForStatus(statuses.value[index]).dateTime;
-    return !currDt.hasSame(prevDt, 'day');
-}
-
-function dateTitleFor(s: StatusResource): string {
-    return getDepartureForStatus(s).dateTime.toLocaleString(DateTime.DATE_HUGE);
-}
-
-function statsDailyHref(s: StatusResource): string {
-    const dt = getDepartureForStatus(s).dateTime;
-    return `/statistics/daily/${dt.toISODate()}`;
 }
 
 // Metrics
@@ -207,52 +182,17 @@ fetchStatuses(false);
         </div>
 
         <div class="col-md-8 col-lg-7">
-            <h1 v-if="statuses.length" class="fs-3">
-                {{ trans('profile.last-journeys-of') }} {{ userData?.displayName || props.username }}:
-            </h1>
-
-            <template v-for="(s, i) in statuses" :key="s.id">
-                <h2 v-if="isNewDay(i)" class="mb-2 fs-5">
-                    {{ dateTitleFor(s) }}
-                    <a v-if="isOwnProfile" :href="statsDailyHref(s)" class="text-trwl" aria-label="Tägliche Fahrten">
-                        <i class="fa-solid fa-map-location-dot" aria-hidden="true" />
-                    </a>
-                </h2>
-
-                <StatusCard
-                    :status="s"
-                    :authenticated-user="authUser.user"
-                    :stopovers="getStopoverForTrip(s.checkin.trip.toString())"
-                />
-            </template>
-
-            <template v-if="loadingStatuses">
-                <LoadingSkeletonRows class="text-center" :row-height="30" :columns="1" :rows="1" />
-                <LoadingSkeletonRows class="text-center" :row-height="15" :columns="1" :rows="1" />
-                <LoadingSkeletonRows class="text-center mb-4" :row-height="206" :columns="1" :rows="5" />
-            </template>
-
-            <div v-if="!loadingStatuses && showMore" class="text-center my-4">
-                <button class="btn btn-primary" :disabled="loadingStatuses" @click="fetchStatuses(true)">
-                    <i class="fa-solid fa-arrow-down" />
-                </button>
-                <div v-if="lastPage !== null" class="small text-muted mt-2">{{ currentPage }} / {{ lastPage }}</div>
-            </div>
-
-            <div v-if="!loadingStatuses && !showMore && statuses.length" class="text-center my-4">
-                <p class="text-muted">Final stop. All change, please!</p>
-            </div>
-
-            <div v-if="!loadingStatuses && !statuses.length && userData" class="text-center my-4">
-                <span class="text-danger fs-3">
-                    <template v-if="(userData?.trainDistance ?? 0) > 0">
-                        {{ trans('profile.no-visible-statuses', { username: userData.displayName }) }}
-                    </template>
-                    <template v-else>
-                        {{ trans('profile.no-statuses', { username: userData.displayName }) }}
-                    </template>
-                </span>
-            </div>
+            <Statuses
+                v-if="userData"
+                :statuses="statuses"
+                :stopovers="stopovers"
+                :user-data="userData"
+                :loading-statuses="loadingStatuses"
+                :show-more="showMore"
+                :last-page="lastPage"
+                :current-page="currentPage"
+                @fetch-more-statuses="fetchStatuses(true)"
+            />
         </div>
     </div>
 </template>
