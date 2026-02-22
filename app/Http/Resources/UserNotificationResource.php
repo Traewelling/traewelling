388840030f69b76e9c776862ef363a7a7dd71c5a
Kristian Stöckel <git@k118.de>
2026-02-22 23:00:37 +0100

    :bug: set recipients locale in UserNotificationResource
    fixes #4202

--- app/Http/Resources/UserNotificationResource.php
+++ app/Http/Resources/UserNotificationResource.php
@@ -8,18 +8,26 @@ class UserNotificationResource extends JsonResource
 {
     public function toArray($request): array
     {
-        return [
-            'id' => (string) $this->id,
-            'type' => (string) str_replace('App\\Notifications\\', '', $this->type),
-            'leadFormatted' => $this->resource->type::getLead($this->resource->data),
-            'lead' => strip_tags($this->resource->type::getLead($this->resource->data)),
-            'noticeFormatted' => $this->resource->type::getNotice($this->resource->data),
-            'notice' => strip_tags($this->resource->type::getNotice($this->resource->data)),
-            'link' => $this->resource->type::getLink($this->resource->data),
-            'data' => $this->data,
-            'readAt' => $this->read_at?->toIso8601String(),
-            'createdAt' => $this->created_at->toIso8601String(),
-            'createdAtForHumans' => $this->created_at->diffForHumans(),
-        ];
+        $originalLocale = app()->getLocale();
+        $recipientLocale = $this->resource->notifiable?->language ?? $originalLocale;
+        app()->setLocale($recipientLocale);
+
+        try {
+            return [
+                'id' => (string) $this->id,
+                'type' => (string) str_replace('App\\Notifications\\', '', $this->type),
+                'leadFormatted' => $this->resource->type::getLead($this->resource->data),
+                'lead' => strip_tags($this->resource->type::getLead($this->resource->data)),
+                'noticeFormatted' => $this->resource->type::getNotice($this->resource->data),
+                'notice' => strip_tags($this->resource->type::getNotice($this->resource->data)),
+                'link' => $this->resource->type::getLink($this->resource->data),
+                'data' => $this->data,
+                'readAt' => $this->read_at?->toIso8601String(),
+                'createdAt' => $this->created_at->toIso8601String(),
+                'createdAtForHumans' => $this->created_at->diffForHumans(),
+            ];
+        } finally {
+            app()->setLocale($originalLocale);
+        }
     }
 }
