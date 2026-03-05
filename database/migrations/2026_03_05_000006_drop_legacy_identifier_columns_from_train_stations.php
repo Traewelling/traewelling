b6e4a6a90227bf64fd5adc5ece1878a066289610
Kristian Stöckel <git@k118.de>
2026-03-05 22:21:03 +0100

    SQLite² ┌(ಠ_ಠ)┐

--- database/migrations/2026_03_05_000006_drop_legacy_identifier_columns_from_train_stations.php
+++ database/migrations/2026_03_05_000006_drop_legacy_identifier_columns_from_train_stations.php
@@ -11,12 +11,17 @@ return new class() extends Migration
     public function up(): void
     {
         Schema::table('train_stations', function (Blueprint $table): void {
-            $table->dropUnique('train_stations_ibnr_unique');
-            if (Schema::hasIndex('train_stations', 'train_stations_wikidata_id_foreign')) {
-                $table->dropIndex('train_stations_wikidata_id_foreign');
+            foreach ([
+                'train_stations_ibnr_unique',
+                'train_stations_wikidata_id_foreign',
+                'train_stations_wikidata_id_index',
+                'train_stations_rilidentifier_index',
+                'ifopt',
+            ] as $index) {
+                if (Schema::hasIndex('train_stations', $index)) {
+                    $table->dropIndex($index);
+                }
             }
-            $table->dropIndex('train_stations_rilidentifier_index');
-            $table->dropIndex('ifopt');
             $table->dropColumn([
                 'ibnr',
                 'wikidata_id',
