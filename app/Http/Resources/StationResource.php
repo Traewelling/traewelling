c310c8d6c8515088b7b477f82e9043359b748870
Kris <git@k118.de>
2026-03-05 12:46:14 +0100

    :wastebasket: deprecate ibnr and rilIdentifier fields in StationResource (#4500)

--- app/Http/Resources/StationResource.php
+++ app/Http/Resources/StationResource.php
@@ -15,6 +15,22 @@ use OpenApi\Attributes as OA;
         new OA\Property(property: 'latitude', type: 'number', example: '48.993207'),
         new OA\Property(property: 'longitude', type: 'number', example: '8.400977'),
         new OA\Property(
+            property: 'ibnr',
+            description: 'Deprecated. Always null. Use identifiers with type "de_db_ibnr" instead.',
+            type: 'integer',
+            example: null,
+            nullable: true,
+            deprecated: true,
+        ),
+        new OA\Property(
+            property: 'rilIdentifier',
+            description: 'Deprecated. Always null. Use identifiers with type "de_db_ril100" instead.',
+            type: 'string',
+            example: null,
+            nullable: true,
+            deprecated: true,
+        ),
+        new OA\Property(
             property: 'areas',
             type: 'array',
             items: new OA\Items(ref: '#/components/schemas/AreaResource'),
@@ -36,8 +52,8 @@ class StationResource extends JsonResource
             'name' => $this->name,
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
-            'ibnr' => $this->ibnr, // @deprecated - see identifiers
-            'rilIdentifier' => $this->rilIdentifier, // @deprecated - see identifiers
+            'ibnr' => null, // @deprecated - see identifiers
+            'rilIdentifier' => null, // @deprecated - see identifiers
             'areas' => AreaResource::collection($this->whenLoaded('areas')),
             'identifiers' => StationIdentifierResource::collection($this->whenLoaded('stationIdentifiers')),
         ];
