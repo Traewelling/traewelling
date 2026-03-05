d28142d826c2e593fc6a7e52f296adecc4ed23fd
Kris <git@k118.de>
2026-03-05 10:52:45 +0100

    :card_file_box:️ migrate `ifopt_x` to `station_identifiers` (#4494)

--- app/StationIdentifierType.php
+++ app/StationIdentifierType.php
@@ -6,6 +6,7 @@ enum StationIdentifierType: string
 {
     case MOTIS = 'motis';
     case WIKIDATA_ID = 'wikidata_id';
+    case IFOPT = 'ifopt';              // International: Identification of Fixed Objects in Public Transport
     case DE_DB_RIL100 = 'de_db_ril100'; // Germany: Deutsche Bahn Richtline 100 identifier
     case DE_DB_IBNR = 'de_db_ibnr';   // Germany: internal train station ID
 }
