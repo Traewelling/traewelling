60ccec0cf8594ebbacb92ca9a3c59f896fb6338b
Kris <git@k118.de>
2026-03-06 21:46:26 +0100

    :bug: fix casing of `alsoOnThisConnection` property (#4529)

--- resources/vue/components/CheckinSuccessHelper.vue
+++ resources/vue/components/CheckinSuccessHelper.vue
@@ -37,7 +37,7 @@ export default {
             }
 
             this.points = success?.points ?? null;
-            this.alsoOnThisConnection = success?.alsoOnThisconnection ?? [];
+            this.alsoOnThisConnection = success?.alsoOnThisConnection ?? [];
             this.status = success?.status ?? null;
             this.$refs.modal.show();
             this.checkinSuccess.reset();
