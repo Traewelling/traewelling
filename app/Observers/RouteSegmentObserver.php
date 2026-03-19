05b8482eb4d88eb1b190051f425d08f77111762a
Kristian Stöckel <git@k118.de>
2026-03-19 10:52:51 +0100

    :recycle: trigger distance recalculation for related trips on route segment update

--- app/Observers/RouteSegmentObserver.php
+++ app/Observers/RouteSegmentObserver.php
@@ -5,12 +5,26 @@ declare(strict_types=1);
 namespace App\Observers;
 
 use App\Jobs\AssignRouteSegmentToStopovers;
+use App\Jobs\RecalculateStatusesDistanceForTrip;
 use App\Models\RouteSegment;
+use App\Models\Stopover;
 
 class RouteSegmentObserver
 {
     public function created(RouteSegment $routeSegment): void
     {
         AssignRouteSegmentToStopovers::dispatch($routeSegment);
+    }
+
+    public function updated(RouteSegment $routeSegment): void
+    {
+        if (!$routeSegment->wasChanged(['polyline', 'distance'])) {
+            return;
+        }
+
+        Stopover::where('route_segment_id', $routeSegment->id)
+            ->distinct()
+            ->pluck('trip_id')
+            ->each(fn (string $tripId) => RecalculateStatusesDistanceForTrip::dispatch($tripId));
     }
 }
