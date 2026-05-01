5ff5277c5d3950e4e2cdf5335a0d62dde9f64f2a
Kristian Stöckel <git@k118.de>
2026-05-01 10:07:52 +0200

    move bound calculation to GenericMap

--- resources/vue/components/Map/Map.vue
+++ resources/vue/components/Map/Map.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { GeoJSONFeature, LngLat, LngLatBounds } from 'maplibre-gl';
+import { GeoJSONFeature } from 'maplibre-gl';
 import { computed, PropType, ref } from 'vue';
 import { Api, EventResource, LivePointDto, MapProvider, StatusResource } from '../../../types/Api.gen';
 import { useUserStore } from '../../stores/user';
@@ -17,8 +17,6 @@ const props = defineProps({
 const api = new Api({ baseUrl: window.location.origin + '/api/v1' });
 const userStore = useUserStore();
 
-const center = ref(new LngLat(9.902056, 49.843));
-const bounds = ref<LngLatBounds>(LngLatBounds.fromLngLat(center.value, 100000));
 const lineColor = ref<string>('#c72730');
 const polylines = ref<GeoJSONFeature[]>([]);
 const livePositions = ref<LivePointDto[]>([]);
@@ -30,32 +28,13 @@ if (props.statuses.length === 1) {
     api.polyline
         .getPolylines(props.statuses.map((s) => s.id.toString()).join(','))
         .then((response) => {
-            const newBounds = new LngLatBounds();
-
             for (const feature of response.data.data?.features || []) {
                 polylines.value.push(feature as GeoJSONFeature);
-                for (const point of feature.geometry?.coordinates || []) {
-                    newBounds.extend([point[0], point[1]]);
-                }
             }
-            setNewBounds(newBounds);
         })
         .catch((error) => {
             console.error('Error fetching polylines:', error);
         });
-}
-
-function setNewBounds(newBounds: LngLatBounds): void {
-    const sw = newBounds.getSouthWest();
-    const ne = newBounds.getNorthEast();
-    if (!sw || !ne) return;
-    const latDiff = ne.lat - sw.lat;
-    const lngDiff = ne.lng - sw.lng;
-    newBounds = new LngLatBounds(
-        [sw.lng - lngDiff * 0.1, sw.lat - latDiff * 0.1],
-        [ne.lng + lngDiff * 0.1, ne.lat + latDiff * 0.1],
-    );
-    bounds.value = newBounds;
 }
 
 api.positions
@@ -80,7 +59,6 @@ api.events
 <template>
     <GenericMap
         v-if="polylines.length || livePositions.length"
-        :bounds="bounds"
         :poly-lines="polylines"
         :line-color="lineColor"
         :live-positions="livePositions"
