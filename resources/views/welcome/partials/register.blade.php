f7bfb42553143bc51f70718b813c006a7eb536f5
Levin Herr <github@17d.me>
2026-06-09 19:15:02 +0200

    ✨ Replace FontAwesome icons with SVG for Mastodon login buttons (#4864)
    Signed-off-by: Levin <github@17d.me>

--- resources/views/welcome/partials/register.blade.php
+++ resources/views/welcome/partials/register.blade.php
@@ -8,7 +8,9 @@
         </span>
 
     <button class="btn btn-neutral" onclick="mastodon_modal.showModal()" type="button">
-        <i class="fa-brands fa-mastodon text-primary"></i>
+        <figure class="w-4 fill-white">
+            @include('welcome.partials.mastodon-icon')
+        </figure>
         {{ __('user.login.mastodon') }}
     </button>
 
@@ -37,7 +39,8 @@
             <span class="label-text">{{ __('user.displayname') }}</span>
         </div>
 
-        <input type="text" class="input validator w-full @error('name') input-error @enderror" id="name" name="name" required
+        <input type="text" class="input validator w-full @error('name') input-error @enderror" id="name" name="name"
+               required
                autocomplete="name"/>
         @error('name')
         <p class="validator-hint text-error visible">
@@ -51,7 +54,8 @@
             <span class="label-text">{{ __('user.email') }}</span>
         </div>
 
-        <input type="text" class="input validator w-full @error('email') input-error @enderror" id="email" name="email" required
+        <input type="text" class="input validator w-full @error('email') input-error @enderror" id="email" name="email"
+               required
                value="{{ old('email') }}" autocomplete="email"/>
         @error('email')
         <p class="validator-hint text-error visible">
@@ -65,7 +69,8 @@
             <span class="label-text">{{ __('user.password') }}</span>
         </div>
 
-        <input type="password" class="input validator w-full @error('password') input-error @enderror" id="password" name="password" required
+        <input type="password" class="input validator w-full @error('password') input-error @enderror" id="password"
+               name="password" required
                value="{{ old('email') }}" autocomplete="new-password" minlength="8"/>
         @error('password')
         <p class="validator-hint text-error visible">
