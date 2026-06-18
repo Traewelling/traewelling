52d459a5bf2eb1220bf0096776bf11ba8eab556e
skloobi <michael@rittmeister.in>
2026-06-19 00:50:11 +0200

    Fix HTTP 500 error when sending webhooks that have intentionally missing fields

--- app/Http/Controllers/Backend/WebhookController.php
+++ app/Http/Controllers/Backend/WebhookController.php
@@ -16,6 +16,8 @@ use Carbon\Carbon;
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\MissingValue;
 use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -23,8 +25,7 @@ use Spatie\WebhookServer\WebhookCall;
 
 abstract class WebhookController extends Controller
 {
-    public static function index(User $user): object
-    {
+    public static function index(User $user): object {
         return $user->webhooks;
     }
 
@@ -35,20 +36,20 @@ abstract class WebhookController extends Controller
         $request->delete();
         $secret = bin2hex(random_bytes(32));
         $client = $request->client()->first();
-        $user = $request->user()->first();
+        $user   = $request->user()->first();
 
         $webhook = Webhook::create([
-            'oauth_client_id' => $client->id,
-            'url' => $request->url,
-            'secret' => $secret,
-            'user_id' => $user->id,
-        ]);
+                                       'oauth_client_id' => $client->id,
+                                       'url'             => $request->url,
+                                       'secret'          => $secret,
+                                       'user_id'         => $user->id,
+                                   ]);
 
         foreach (explode(',', $request->events) as $event) {
             WebhookEvent::create([
-                'webhook_id' => $webhook->id,
-                'event' => $event,
-            ]);
+                                     'webhook_id' => $webhook->id,
+                                     'event'      => $event,
+                                 ]);
         }
 
         DB::commit();
@@ -58,52 +59,49 @@ abstract class WebhookController extends Controller
         return $webhook;
     }
 
-    public static function sendStatusWebhook(Status $status, WebhookEventEnum $event): void
-    {
+    public static function sendStatusWebhook(Status $status, WebhookEventEnum $event): void {
         self::dispatchWebhook($status->user, $event, [
             'status' => new StatusResource($status),
         ]);
     }
 
-    public static function sendNotificationWebhook(User $user, DatabaseNotification $notification): void
-    {
+    public static function sendNotificationWebhook(User $user, DatabaseNotification $notification): void {
         self::dispatchWebhook($user, WebhookEventEnum::NOTIFICATION, [
             'notification' => new UserNotificationResource($notification),
         ]);
     }
 
-    public static function dispatchWebhook(User $user, WebhookEventEnum $event, array $data): void
-    {
+    public static function dispatchWebhook(User $user, WebhookEventEnum $event, array $data): void {
         if (!config('trwl.webhooks_active')) {
             return;
         }
 
         $webhooks = $user->webhooks()
-            ->withWhereHas('events', function ($builder) use ($event) {
-                $builder->where('event', '=', $event);
-            })
-            ->where('user_id', $user->id)
-            ->whereNull('disabled_at')
-            ->get();
+                         ->withWhereHas('events', function($builder) use ($event) {
+                             $builder->where('event', '=', $event);
+                         })
+                         ->where('user_id', $user->id)
+                         ->whereNull('disabled_at')
+                         ->get();
 
         foreach ($webhooks as $webhook) {
             Log::debug('Sending webhook', [
                 'webhook_id' => $webhook->id,
-                'user_id' => $webhook->user->id,
+                'user_id'    => $webhook->user->id,
             ]);
             WebhookCall::create()
-                ->url($webhook->url)
-                ->withHeaders([
-                    'X-Trwl-User-Id' => $user->id,
-                    'X-Trwl-Webhook-Id' => $webhook->id,
-                    'X-Trwl-OAuth-Client-Id' => $webhook->oauth_client_id,
-                ])
-                ->payload([
-                    'event' => $event->value,
-                    ...self::freezeJsonObjects($data),
-                ])
-                ->useSecret($webhook->secret)
-                ->dispatch();
+                       ->url($webhook->url)
+                       ->withHeaders([
+                                         'X-Trwl-User-Id'         => $user->id,
+                                         'X-Trwl-Webhook-Id'      => $webhook->id,
+                                         'X-Trwl-OAuth-Client-Id' => $webhook->oauth_client_id,
+                                     ])
+                       ->payload([
+                                     'event' => $event->value,
+                                     ...self::freezeJsonObjects($data),
+                                 ])
+                       ->useSecret($webhook->secret)
+                       ->dispatch();
         }
     }
 
@@ -111,20 +109,20 @@ abstract class WebhookController extends Controller
      * Creates a new webhook creation request
      */
     public static function createWebhookRequest(
-        User $user,
+        User        $user,
         OAuthClient $client,
-        string $oauthCode,
-        string $url,
-        array $events,
+        string      $oauthCode,
+        string      $url,
+        array       $events,
     ): WebhookCreationRequest {
         return WebhookCreationRequest::create([
-            'id' => hash('sha256', $oauthCode),
-            'user_id' => $user->id,
-            'oauth_client_id' => $client->id,
-            'expires_at' => Carbon::now()->addHour(),
-            'events' => implode(',', $events),
-            'url' => $url,
-        ]);
+                                                  'id'              => hash('sha256', $oauthCode),
+                                                  'user_id'         => $user->id,
+                                                  'oauth_client_id' => $client->id,
+                                                  'expires_at'      => Carbon::now()->addHour(),
+                                                  'events'          => implode(',', $events),
+                                                  'url'             => $url,
+                                              ]);
     }
 
     public static function findWebhookRequest(
@@ -137,14 +135,20 @@ abstract class WebhookController extends Controller
      * Calls custom serializers of all objects in this array, to avoid dynamic values to change
      * when serializing again.
      *
-     * @param  array  $data  the object to freeze
+     * @param array $data the object to freeze
+     *
      * @return array|null the new object
      *
      * @throws BindingResolutionException if not called from a request
      */
-    private static function freezeJsonObjects(?array $data): ?array
-    {
-        return array_map(function ($value) {
+    private static function freezeJsonObjects(?array $data): ?array {
+        // Missing values actually cannot be frozen, because toArray fails on them
+        // Sidenote: Signer fails on them as well
+        $relevant = array_filter($data, function($value) {
+            return !($value instanceof JsonResource && $value->resource instanceof MissingValue);
+        });
+
+        return array_map(function($value) {
             $request = Container::getInstance()->make('request');
 
             if (is_null($value)) {
@@ -155,6 +159,7 @@ abstract class WebhookController extends Controller
                 if (is_null($value->resource)) {
                     return null;
                 }
+
                 $result = $value->toArray($request);
                 if (is_null($result)) {
                     return null;
@@ -164,6 +169,6 @@ abstract class WebhookController extends Controller
             }
 
             return $value;
-        }, $data);
+        }, $relevant);
     }
 }
