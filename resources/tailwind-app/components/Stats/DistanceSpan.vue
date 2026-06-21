62102afdf5be3381f962399eb00f011024bfba3c
Kristian Stöckel <git@k118.de>
2026-06-21 14:23:25 +0200

    :fire: remove mega- and gigameter

--- resources/tailwind-app/components/Stats/DistanceSpan.vue
+++ resources/tailwind-app/components/Stats/DistanceSpan.vue
@@ -8,41 +8,20 @@ const props = defineProps({
         type: Number,
         required: true,
     },
-    class: {
-        type: String,
-        default: '',
-    },
 });
 
 const split = ref<SplitDistance>(metersToDistance(props.distance));
 </script>
 
 <template>
-    <!-- eslint-disable-next-line vue/valid-v-bind -->
-    <span v-if="split" :class>
-        <span v-if="split.gigameters > 0" class="tooltip" :data-tip="trans('distance.gigameters')">
-            {{ split.gigameters }}
-            <small class="opacity-65 me-1">
-                {{ trans('distance.gigameters.short') }}
-            </small>
-        </span>
-        <span v-if="split.megameters > 0" class="tooltip" :data-tip="trans('distance.megameters')">
-            {{ split.megameters }}
-            <small class="opacity-65 me-1">
-                {{ trans('distance.megameters.short') }}
-            </small>
-        </span>
+    <span v-if="split">
         <span v-if="split.kilometers > 0" class="tooltip cursor-default" :data-tip="`${props.distance} m`">
             {{ split.kilometers }}
             <small class="opacity-65 me-1">
                 {{ trans('distance.kilometers.short') }}
             </small>
         </span>
-        <span
-            v-if="!(split.gigameters || split.megameters || split.kilometers)"
-            class="tooltip"
-            :data-tip="trans('distance.meters')"
-        >
+        <span v-if="!split.kilometers" class="tooltip" :data-tip="trans('distance.meters')">
             {{ split.meters }}
             <small class="opacity-65">
                 {{ trans('distance.meters.short') }}
