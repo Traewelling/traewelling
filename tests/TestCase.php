337154e67b4ad4c1d326b8ad9bcc5d5c935a2a59
Levin <github@17d.me>
2026-06-25 15:23:59 +0200

    fix tests concerning performance stats
    Signed-off-by: Levin <github@17d.me>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUpTheTestEnvironment(): void
+    {
+        parent::setUpTheTestEnvironment();
+        if (!defined('LARAVEL_START')) {
+            define('LARAVEL_START', microtime(true));
+        }
+    }
 }
