5f7826825b8535829e3523da304d7e54ba6feabd
Kris <git@k118.de>
2026-07-26 10:58:05 +0200

    :bug: allow creating local apps with unsecure schemes (#4971)

--- app/Rules/SecureUrl.php
+++ app/Rules/SecureUrl.php
@@ -18,8 +18,45 @@ class SecureUrl implements InvokableRule
      */
     public function __invoke($attribute, $value, $fail)
     {
-        if (!str_starts_with($value, 'https://') && !App::environment('local')) {
-            $fail(':attribute must be a secure URL.');
+        if (str_starts_with($value, 'https://')) {
+            return;
         }
+
+        if (App::environment('local') || $this->isLoopbackUrl($value)) {
+            return;
+        }
+
+        $fail(':attribute must be a secure URL.');
+    }
+
+    /**
+     * RFC 8252 Nr. 8.3
+     */
+    private function isLoopbackUrl(string $value): bool
+    {
+        if (!str_starts_with($value, 'http://')) {
+            return false;
+        }
+
+        $host = parse_url($value, PHP_URL_HOST);
+        if ($host === null || $host === false) {
+            return false;
+        }
+
+        $host = trim($host, '[]');
+
+        if ($host === 'localhost' || str_ends_with($host, '.localhost')) {
+            return true;
+        }
+
+        if ($host === '::1') {
+            return true;
+        }
+
+        if (filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            return str_starts_with($host, '127.');
+        }
+
+        return false;
     }
 }
