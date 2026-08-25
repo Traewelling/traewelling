6b57580879fe40930fe86f2e4dceb5875d8029f1
Kris <git@k118.de>
2026-08-25 20:06:49 +0200

    :bug: fix icons (#5039)

--- resources/tailwind-app/components/TransportIcon.vue
+++ resources/tailwind-app/components/TransportIcon.vue
@@ -1,57 +1,73 @@
 <script setup lang="ts">
-import { CarTaxiFront, Container, Moon, Plane, Ship, Train } from '@lucide/vue';
-import { computed } from 'vue';
+import { CableCar, CarTaxiFront, Container, Moon, Plane, Ship, Train, TrainFront } from '@lucide/vue';
+import { computed, type Component } from 'vue';
+import { HafasTravelType, MotisCategory } from '../../types/Api.gen';
 
 const props = defineProps<{
     product?: string | null;
     mode?: string | null;
 }>();
 
-const svgMap: Record<string, string> = {
-    BUS: '/img/bus.svg',
-    COACH: '/img/bus.svg',
-    FLEX: '/img/bus.svg',
-    bus: '/img/bus.svg',
-    bus_pill: '/img/bus.svg',
-    TRAM: '/img/tram.svg',
-    CABLE_CAR: '/img/tram.svg',
-    FUNICULAR: '/img/tram.svg',
-    AERIAL_LIFT: '/img/tram.svg',
-    AERAL_LIFT: '/img/tram.svg',
-    tram: '/img/tram.svg',
-    SUBWAY: '/img/subway.svg',
-    METRO: '/img/subway.svg',
-    subway: '/img/subway.svg',
-    SUBURBAN: '/img/suburban.svg',
-    suburban: '/img/suburban.svg',
+type TransportIconDefinition = { svg: string } | { icon: Component };
+
+const svg = (name: string): TransportIconDefinition => ({ svg: `/img/${name}.svg` });
+const icon = (component: Component): TransportIconDefinition => ({ icon: component });
+
+const modeIcons: Partial<Record<MotisCategory, TransportIconDefinition>> = {
+    [MotisCategory.ODM]: icon(CarTaxiFront),
+    [MotisCategory.FLEX]: svg('bus'),
+    [MotisCategory.TRAM]: svg('tram'),
+    [MotisCategory.SUBWAY]: svg('subway'),
+    [MotisCategory.FERRY]: icon(Ship),
+    [MotisCategory.AIRPLANE]: icon(Plane),
+    [MotisCategory.SUBURBAN]: svg('suburban'),
+    [MotisCategory.BUS]: svg('bus'),
+    [MotisCategory.COACH]: svg('bus'),
+    [MotisCategory.RAIL]: icon(Train),
+    [MotisCategory.HIGHSPEED_RAIL]: icon(TrainFront),
+    [MotisCategory.LONG_DISTANCE]: icon(TrainFront),
+    [MotisCategory.NIGHT_RAIL]: icon(Moon),
+    [MotisCategory.REGIONAL_FAST_RAIL]: icon(Train),
+    [MotisCategory.REGIONAL_RAIL]: icon(Train),
+    [MotisCategory.CABLE_CAR]: icon(CableCar),
+    [MotisCategory.FUNICULAR]: icon(CableCar),
+    [MotisCategory.AERIAL_LIFT]: icon(CableCar),
+    [MotisCategory.AERAL_LIFT]: icon(CableCar),
+    [MotisCategory.METRO]: svg('suburban'),
 };
 
-type LucideComponent = typeof Train | typeof Ship | typeof Plane | typeof Moon | typeof CarTaxiFront | typeof Container;
-const lucideMap: Record<string, LucideComponent> = {
-    FERRY: Ship,
-    ferry: Ship,
-    AIRPLANE: Plane,
-    plane: Plane,
-    NIGHT_RAIL: Moon,
-    taxi: CarTaxiFront,
-    freightTrain: Container,
+/** Icon per (legacy named) category. Every category must resolve to something visible. */
+const categoryIcons: Record<HafasTravelType, TransportIconDefinition> = {
+    [HafasTravelType.NationalExpress]: icon(TrainFront),
+    [HafasTravelType.National]: icon(TrainFront),
+    [HafasTravelType.RegionalExp]: icon(Train),
+    [HafasTravelType.Regional]: icon(Train),
+    [HafasTravelType.Suburban]: svg('suburban'),
+    [HafasTravelType.Bus]: svg('bus'),
+    [HafasTravelType.Ferry]: icon(Ship),
+    [HafasTravelType.Subway]: svg('subway'),
+    [HafasTravelType.Tram]: svg('tram'),
+    [HafasTravelType.Taxi]: icon(CarTaxiFront),
+    [HafasTravelType.Plane]: icon(Plane),
+    [HafasTravelType.FreightTrain]: icon(Container),
 };
 
-const svgSrc = computed((): string | null => {
-    if (props.mode && svgMap[props.mode]) return svgMap[props.mode];
-    if (props.product && svgMap[props.product]) return svgMap[props.product];
-    return null;
+const lookup = (
+    map: Partial<Record<string, TransportIconDefinition>>,
+    key?: string | null,
+): TransportIconDefinition | undefined => {
+    return key ? map[key] : undefined;
+};
+
+const definition = computed((): TransportIconDefinition => {
+    return lookup(modeIcons, props.mode) ?? lookup(categoryIcons, props.product) ?? icon(Train);
 });
 
-const lucideIcon = computed((): LucideComponent | null => {
-    if (props.mode && lucideMap[props.mode]) return lucideMap[props.mode];
-    if (props.product && lucideMap[props.product]) return lucideMap[props.product];
-    return null;
-});
+const svgSrc = computed((): string | null => ('svg' in definition.value ? definition.value.svg : null));
+const iconComponent = computed((): Component | null => ('icon' in definition.value ? definition.value.icon : null));
 </script>
 
 <template>
     <img v-if="svgSrc" :src="svgSrc" class="w-4 h-4" :alt="mode ?? product ?? ''" />
-    <component :is="lucideIcon" v-else-if="lucideIcon" class="w-4 h-4" />
-    <Train v-else class="w-4 h-4" />
+    <component :is="iconComponent" v-else class="w-4 h-4" />
 </template>
